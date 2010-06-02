1ccf0ac10da1554f2fe2be132bc8acf0349b0c80
cekees <cekees@gmail.com>
2010-06-02 00:57:23 +0000

    A    proteusExtensions/proteusGraphical A    proteusExtensions/proteusGraphical/build A    proteusExtensions/proteusGraphical/build/temp.macosx-10.6-x86_64-2.6 A    proteusExtensions/proteusGraphical/build/temp.macosx-10.6-x86_64-2.6/Users A    proteusExtensions/proteusGraphical/build/temp.macosx-10.6-x86_64-2.6/Users/cekees A    proteusExtensions/proteusGraphical/build/temp.macosx-10.6-x86_64-2.6/Users/cekees/src A    proteusExtensions/proteusGraphical/build/temp.macosx-10.6-x86_64-2.6/Users/cekees/src/proteus A    proteusExtensions/proteusGraphical/build/temp.macosx-10.6-x86_64-2.6/Users/cekees/src/proteus/proteusModule A    proteusExtensions/proteusGraphical/build/temp.macosx-10.6-x86_64-2.6/Users/cekees/src/proteus/proteusModule/proteus A    proteusExtensions/proteusGraphical/build/temp.macosx-10.6-x86_64-2.6/proteusGraphical A    proteusExtensions/proteusGraphical/build/lib.macosx-10.6-x86_64-2.6 A    proteusExtensions/proteusGraphical/build/lib.macosx-10.6-x86_64-2.6/proteusGraphical A    proteusExtensions/proteusGraphical/build/lib.macosx-10.6-x86_64-2.6/proteusGraphical/vtkViewers.py A    proteusExtensions/proteusGraphical/build/lib.macosx-10.6-x86_64-2.6/proteusGraphical/__init__.py A    proteusExtensions/proteusGraphical/defaultConfig.py A    proteusExtensions/proteusGraphical/setup.py A    proteusExtensions/proteusGraphical/README.txt A    proteusExtensions/proteusGraphical/proteusGraphical A    proteusExtensions/proteusGraphical/proteusGraphical/vtkviewers.h A    proteusExtensions/proteusGraphical/proteusGraphical/vtkViewers.py A    proteusExtensions/proteusGraphical/proteusGraphical/cvtkviewersModule.cpp A    proteusExtensions/proteusGraphical/proteusGraphical/__init__.py A    proteusExtensions/proteusGraphical/proteusGraphical/vtkviewers.cpp

---
+++ proteusExtensions/proteusGraphical/defaultConfig.py
@@ -0,0 +1,16 @@
+import os
+
+PROTEUS = os.getenv('PROTEUS',os.getenv('HOME')+'/src/proteus')
+PROTEUS_PREFIX = os.getenv('PROTEUS_PREFIX',PROTEUS+os.getenv('PROTEUS_ARCH'))
+PROTEUS_GRAPHICAL = os.getenv('PROTEUS_GRAPHICAL',os.getenv('PROTEUS')+'/proteusExtensions/proteusGraphical')
+PROTEUS_GRAPHICAL_EXTRA_COMPILE_ARGS= ['-Wall']
+PROTEUS_GRAPHICAL_EXTRA_LINK_ARGS=['-lblas','-lstdc++']
+
+PROTEUS_GRAPHICAL_VTK_INCLUDE_DIR = os.getenv('PROTEUS_PREFIX')+'/include/vtk-5.7'
+PROTEUS_GRAPHICAL_VTK_LIB_DIR = os.getenv('PROTEUS_PREFIX')+'/lib/vtk-5.7'
+PROTEUS_GRAPHICAL_VTK_LIBS = ['vtkCommon','vtkCommonPythonD','vtksys']
+
+PROTEUS_TRIANGLE_INCLUDE_DIR = PROTEUS_PREFIX+'/include'
+PROTEUS_TRIANGLE_H = r'"triangle.h"'
+PROTEUS_TRIANGLE_LIB_DIR = PROTEUS_PREFIX+'/lib'
+PROTEUS_TRIANGLE_LIB ='tri'
