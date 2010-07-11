9e89c05c472af1cb4f4ab1e5d53c61d3d1971115
cekees <cekees@gmail.com>
2010-07-11 20:55:08 +0000

    M    demo/stokes_pome_2d_homog_c0p1c0p1_n.py M    proteusModule/include/numericalFlux.h AM   proteusModule/scripts/replacePYADH.py M    proteusModule/proteus/cnumericalFluxModule.c M    proteusModule/proteus/numericalFlux.c M    proteusModule/proteus/NumericalFlux.py

---
+++ proteusModule/scripts/replacePYADH.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import glob
+files = glob.glob('*.py') + glob.glob('*.cpp') + glob.glob('*.c') + glob.glob('*.h')  + glob.glob('*.pxd')
+print files
+for f in files:
+	if f != "replacePYADH.py":
+		fh = open(f,'r')
+		fstr = fh.read()
+		fh.close()
+		fstr = fstr.replace("pyadh",
+				    "proteus")
+		fh = open(f,'w')
+		fh.write(fstr)
+		fh.close()
