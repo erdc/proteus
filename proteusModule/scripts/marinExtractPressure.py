1d1a6e4a638d27e6431ea85f3d085a99018edb00
ido <idoakkerman@gmail.com>
2010-12-07 01:01:44 +0000

    Marin pressure and height extraction script: requires paraview

---
+++ proteusModule/scripts/marinExtractPressure.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env pvpython
+
+from paraview import servermanager
+from paraview.simple import *
+from optparse import OptionParser
+
+usage = ""
+parser = OptionParser(usage=usage)
+parser.add_option("-f","--filebase",
+                    help="filename",
+                    action="store",
+                    type="string",
+                    dest="filename",
+                    default="simulation")
+		    
+(opts,args) = parser.parse_args()	
+
+if not paraview.servermanager.ActiveConnection:
+	connection = paraview.servermanager.Connect()
+
+reader = servermanager.sources.XdmfReader(FileName=opts.filename)
+reader.UpdatePipeline()
+timesteps = reader.TimestepValues
+
+points=[]
+points.append(PointSource(Center=[2.3950,0.4745,0.020],NumberOfPoints=1))
+points.append(PointSource(Center=[2.3950,0.4745,0.100],NumberOfPoints=1))
+points.append(PointSource(Center=[2.4195,0.5255,0.161],NumberOfPoints=1))
+points.append(PointSource(Center=[2.4995,0.5255,0.161],NumberOfPoints=1))
+
+probes=[]
+for point in points:
+  probes.append(ProbePoint(Source=point,Input=reader))
+
+outfile = open("pressure.txt",'w')
+for time in timesteps:
+  outfile.write(str(time))
+  for probe in probes:
+     probe.UpdatePipeline (time)
+
+     fp = servermanager.Fetch(probe)
+     pdata= fp.GetPointData()
+     pressure = pdata.GetArray("p").GetTuple1(0)
+
+     outfile.write("  " + str(pressure))
+  outfile.write("\n")
+outfile.close()
