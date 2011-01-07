d94de0d0dde203cc75af0275ff37762d1d472ff5
cekees <cekees@gmail.com>
2011-01-07 23:38:38 +0000

    added fill pivoting prototypes M    proteus_lapack.h

--- proteusModule/include/proteus_lapack.h
+++ proteusModule/include/proteus_lapack.h
@@ -8,6 +8,8 @@ extern "C"
 
 extern int dgetrf_(int *m, int *n, double *a, int *lda, int *ipiv, int *info);
 extern int dgetrs_(char *trans, int *n, int *nrhs, double *a, int *lda, int *ipiv, double *b, int *ldb, int *info);
+extern int dgetc2_(int *n, double *a, int *lda, int *ipiv, int *jpiv, int *info);
+extern int dgesc2_(int *n, double *a, int *lda, double* rhs, int *ipiv, int *jpiv, double* scale);
 extern int dgeev_(char* jobvl, char* jobvr, int* n, double* a, int* lda, double* wr, double* wi, double* vl, int* ldvl, double* vr, int* ldvr, double* work, int* lwork,int* info);
 extern int dgetri_(int* N,double* A,int* LDA,int* IPIV,double* WORK,int* LWORK,int* INFO );
 
