176deabd33f32c4fb263ce7c1f6c9dce5a680361
Chris Kees <cekees@gmail.com>
2011-09-02 22:25:00 -0500

    removed strings containing "/" from library list. Fixes incorrect parsing of rpath statements

--- proteusModule/setPetscLib.py
+++ proteusModule/setPetscLib.py
@@ -8,7 +8,8 @@ for s in sys.argv:
     if '-L' in s:
         lib_dirs.append(s[2:])
     if '-l' in s:
-        libs.append(s[2:])
+        if '/' not in s:
+            libs.append(s[2:])
     if '-I' in s:
         include_dirs.append(s[2:])
 f = open('config.py','r')
