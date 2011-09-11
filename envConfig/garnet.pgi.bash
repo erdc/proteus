58bd4ef438381a61d49e0d615d76bd32a086e8c6
Chris Kees <cekees@gmail.com>
2011-09-11 01:06:23 -0500

    cleaned up envConfig and added some new ones for garnet
    These should be sourced from the proper shell, for example ${PROTEUS}/envConfig/garnet.bash)

---
+++ envConfig/garnet.pgi.bash
@@ -0,0 +1,15 @@
+#
+#garent.pgi
+#
+module load acml
+export PROTEUS=${HOME}/proteus
+export PROTEUS_ARCH=garnet.pgi
+export PROTEUS_PREFIX=${PROTEUS}/${PROTEUS_ARCH}
+export PROTEUS_PYTHON=${PROTEUS_PREFIX}/bin/python
+export PATH=.:${PROTEUS_PREFIX}/bin:${HOME}/src/proteus/garnet/bin:${PATH}
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PROTEUS_PREFIX}/lib:${ACML_DIR}/pgi64/lib
+export CC=pgcc
+export CXX=pgCC
+export FC=pgf90
+export F77=pgf77
+export F90=pgf90
