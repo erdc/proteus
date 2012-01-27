dad537b3994e9a8199f6395e4fc74e0c16db35c4
Chris Kees <cekees@gmail.com>
2012-01-27 11:56:56 -0600

    updated externalPackages

---
+++ externalPackages/clawpack/setenv.bash
@@ -0,0 +1,8 @@
+# Clawpack environment settings
+export CLAW="/Users/cekees/proteus/externalPackages/clawpack"
+export CLAWUTIL="/Users/cekees/proteus/externalPackages/clawpack/clawutil"
+export PYCLAW="/Users/cekees/proteus/externalPackages/clawpack/pyclaw"
+export RIEMANN="/Users/cekees/proteus/externalPackages/clawpack/riemann"
+export VISCLAW="/Users/cekees/proteus/externalPackages/clawpack/visclaw"
+export PYTHONPATH="/Users/cekees/proteus/externalPackages/clawpack/visclaw/src/python:/Users/cekees/proteus/externalPackages/clawpack/riemann/src/python:/Users/cekees/proteus/externalPackages/clawpack/pyclaw/src:/Users/cekees/proteus/externalPackages/clawpack/clawutil/src/python:${PYTHONPATH}"
+export MATLABPATH="/Users/cekees/proteus/externalPackages/clawpack/visclaw/src/matlab:${MATLABPATH}"
