5b57eef933dac5b1f50c2fcf1b2d9f2b67d4f5c0
Chris Kees <cekees@gmail.com>
2012-02-19 09:23:35 -0600

    fixed configuration on garnet--changed versions of several modules and put PETSC_ARCH etc. as petsc configure.py options

--- envConfig/garnet.gnu.bash
+++ envConfig/garnet.gnu.bash
@@ -3,6 +3,10 @@
 #
 module swap PrgEnv-pgi PrgEnv-gnu
 module load acml
+module swap xt-mpich2 xt-mpich2/5.3.1
+module swap gcc gcc/4.6.0
+module swap xt-libsci xt-libsci/11.0.00
+module swap xt-asyncpe xt-asyncpe/5.00
 export PROTEUS_ARCH=garnet.gnu
 export PROTEUS_PREFIX=${PROTEUS}/${PROTEUS_ARCH}
 export PROTEUS_PYTHON=${PROTEUS_PREFIX}/bin/python
