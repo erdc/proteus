4470601a15261f1a11a44ac23a9ba1e8d693f902
Chris Kees <cekees@gmail.com>
2012-03-01 11:48:01 -0600

    updated petsc with Mac OSX build fixes, add favicon to docs

--- doc/conf.py
+++ doc/conf.py
@@ -51,7 +51,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Proteus'
-#copyright = u'2010, U.S. Government'
+copyright = u'2012, U.S. Government'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -116,19 +116,19 @@ html_theme = 'sphinxdoc'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = u"Proteus Computational Methods and Simulation Toolkit"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+html_short_title = u"Proteus"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = "ProteusLogo.png"
+#html_logo = "proteusSimple.ico"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = "proteusSimple.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -144,7 +144,7 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = {'index':'indexsidebar.html',}
+html_sidebars = {'index':'indexsidebar.html'}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
