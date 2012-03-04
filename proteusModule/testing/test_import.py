4e7826d30a38c19bb933789720387540f5e7e3fa
Chris Kees <cekees@gmail.com>
2012-03-04 15:41:57 -0600

    working on test cleanup and docs

--- proteusModule/testing/test_import.py
+++ proteusModule/testing/test_import.py
@@ -1,7 +1,17 @@
-from proteus import *
-
 def test_import():
-    pass
+    print "hello"
+    import proteus
+    successful_import=True
+    for m in proteus.__all__:
+        print m
+        if m not in ['cMeshTools','TimeIntegration','StepControl','PostProcessing','MeshTools','Norms','FemTools','iproteus','LinearSolvers','Transport','LinearAlgebraTools','Comm']:
+            if m[0] != 'c':
+                try:
+                    module = __import__('proteus.'+m,fromlist=['proteus'])
+                except:
+                    print "Failed to import ",m
+                    successful_import=False
+    assert(successful_import)
 
 if __name__ == '__main__':
     test_import()
