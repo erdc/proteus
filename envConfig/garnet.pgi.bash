2bb00502105a6c90454c4862c21a0c144fdecbdd
Chris Kees <cekees@gmail.com>
2012-03-07 01:32:37 -0600

    changed garnet.pgi to use gcc for python

--- envConfig/garnet.pgi.bash
+++ envConfig/garnet.pgi.bash
@@ -6,6 +6,7 @@ module load acml
 module swap xt-mpich2 xt-mpich2/5.3.1
 module swap xt-libsci xt-libsci/11.0.00
 module swap xt-asyncpe xt-asyncpe/5.00
+module load gcc/4.6.0
 export PROTEUS_ARCH=garnet.pgi
 export PROTEUS_PREFIX=${PROTEUS}/${PROTEUS_ARCH}
 export PROTEUS_PYTHON=${PROTEUS_PREFIX}/bin/python
