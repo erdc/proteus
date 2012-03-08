4771e9fe36844f5208187994d3f540643e8a1720
Chris Kees <cekees@gmail.com>
2012-03-08 15:09:51 -0600

    trying to fix the tests and update to newer dev version of petsc

--- proteusModule/src/Comm.py
+++ proteusModule/src/Comm.py
@@ -4,24 +4,27 @@ Module for controlling MPI
 import sys
 import flcbdfWrappers
 initial_communicator = None
-
+isInitialized = 0
 def init(petscDatabaseFilename=None,argv=sys.argv):
-    global initial_communicator
+    global initial_communicator,isInitialized
     if initial_communicator == None:
+        #pass
         try:
             import petsc4py
             petsc4py.init(argv)
+            isInitialized=1
         except:
             print "WARNING petsc4py import failed!!!"
     if isinstance(petscDatabaseFilename,str):
-        comm = flcbdfWrappers.DaetkPetscSys(argv,petscDatabaseFilename)
+        comm = flcbdfWrappers.DaetkPetscSys(isInitialized,argv,petscDatabaseFilename)
+        isInitialized=1
     else:
-        comm = flcbdfWrappers.DaetkPetscSys(argv)
+        comm = flcbdfWrappers.DaetkPetscSys(isInitialized,argv)
+        isInitialized=1
     if initial_communicator == None:
         initial_communicator = comm
     return comm
 
 def get():
-    if initial_communicator != None:
-        return initial_communicator
-    return flcbdfWrappers.getDaetkPetscSys()
+    assert initial_communicator != None, "Call Comm.init() once before calling Comm.get()"
+    return initial_communicator
