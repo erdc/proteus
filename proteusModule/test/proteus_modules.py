2c662b0e6d6752b64bf88c798f7031328c218a9b
Chris Kees <cekees@gmail.com>
2012-03-08 15:09:51 -0600

    trying to fix the tests and update to newer dev version of petsc

--- proteusModule/test/proteus_modules.py
+++ proteusModule/test/proteus_modules.py
@@ -1,13 +1,17 @@
+import sys
+
 def test_import():
     import proteus
     successful_import=True
     for m in proteus.__all__:
-        print m
         try:
+            print m
             module = __import__('proteus.'+m,fromlist=['proteus'])
+            del module
         except:
             print "Failed to import ",m
             successful_import=False
+    del proteus
     assert(successful_import)
 
 if __name__ == '__main__':
