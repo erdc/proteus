a7438dc912abecad91aaa6e8b920b898c92e2463
Chris Kees <cekees@gmail.com>
2012-03-13 16:04:27 -0500

    fixed LD_LIBRARY_PATH

--- envConfig/garnet.gnu.bash
+++ envConfig/garnet.gnu.bash
@@ -2,7 +2,7 @@
 #garnet.gnu
 #
 module swap PrgEnv-pgi PrgEnv-gnu
-module load acml
+#module load acml
 #module swap xt-mpich2 xt-mpich2/5.3.1
 #module swap gcc gcc/4.6.0
 #module swap xt-libsci xt-libsci/11.0.00
@@ -13,4 +13,4 @@ export PROTEUS_PYTHON=${PROTEUS_PREFIX}/bin/python
 export PATH=.:${PROTEUS_PREFIX}/bin:${HOME}/src/proteus/garnet/bin:${PATH}
 #export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PROTEUS_PREFIX}/lib:${ACML_DIR}/gnu64/lib
 #export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PROTEUS_PREFIX}/lib:${ACML_DIR}/gnu64/lib:/opt/xt-libsci/11.0.0/gnu/46/mc8/lib
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PROTEUS_PREFIX}/lib:/opt/xt-libsci/10.4.9/gnu/46/mc8/lib 
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PROTEUS_PREFIX}/lib:/opt/xt-libsci/10.4.9/gnu/lib
