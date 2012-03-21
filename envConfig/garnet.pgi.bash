6a18dc92be92352f98aa7f7998256dde654f6ddc
Chris Kees <cekees@gmail.com>
2012-03-20 20:11:38 -0500

    tweaks to garnet

--- envConfig/garnet.pgi.bash
+++ envConfig/garnet.pgi.bash
@@ -2,13 +2,9 @@
 #garent.pgi
 #
 module swap PrgEnv-gnu PrgEnv-pgi
-module load acml
-module swap xt-mpich2 xt-mpich2/5.3.1
-module swap xt-libsci xt-libsci/11.0.00
-module swap xt-asyncpe xt-asyncpe/5.00
-module load gcc/4.6.0
+module load gcc
 export PROTEUS_ARCH=garnet.pgi
 export PROTEUS_PREFIX=${PROTEUS}/${PROTEUS_ARCH}
 export PROTEUS_PYTHON=${PROTEUS_PREFIX}/bin/python
 export PATH=.:${PROTEUS_PREFIX}/bin:${HOME}/src/proteus/garnet/bin:${PATH}
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PROTEUS_PREFIX}/lib:${ACML_DIR}/pgi64/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PROTEUS_PREFIX}/lib:/opt/xt-libsci/10.4.9/pgi/lib
