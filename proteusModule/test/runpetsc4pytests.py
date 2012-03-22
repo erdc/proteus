02c863e27742f4567c16599881a280221a84dba8
Chris Kees <cekees@gmail.com>
2012-03-22 15:55:45 -0500

    made tests run in verbose mode

--- proteusModule/test/runpetsc4pytests.py
+++ proteusModule/test/runpetsc4pytests.py
@@ -1,4 +1,4 @@
 import nose,os
 proteus=os.getenv('PROTEUS')
-nose.run(defaultTest=os.path.join(proteus,'externalPackages','petsc4py','test'),argv=['runtests.py','--exclude','test_ts'])
+nose.run(defaultTest=os.path.join(proteus,'externalPackages','petsc4py','test'),argv=['runtests.py','-v','--exclude','test_ts','--exclude','test_da'])
 print("PEXPECT_EXIT")
