a5242f44e0c47fa02726096884a7dd6389cf78a2
Matthew Farthing <mwfarthing@gmail.com>
2013-05-27 09:43:40 -0500

    got ncp1 and dgp1 examples to run except for issues as noted in issues.txt

--- proteusModule/test/poisson_2d/poisson_het_2d_dgp1_n.py
+++ proteusModule/test/poisson_2d/poisson_het_2d_dgp1_n.py
@@ -2,28 +2,40 @@ from proteus import *
 from proteus.default_n import *
 from poisson_het_2d_p import *
 
-parallel = False
+parallel = True
+numerical_flux_flag = 'SIPG'
+
 
 timeIntegration = NoIntegration
 nDTout = 1
 
-femSpaces = dict((i,NC_AffineLinearOnSimplexWithNodalBasis) for i in range(nc))
+femSpaces = dict((i,DG_AffineLinearOnSimplexWithNodalBasis) for i in range(nc))
 
 elementQuadrature = SimplexGaussQuadrature(nd,4)
 elementBoundaryQuadrature = SimplexGaussQuadrature(nd-1,4)
 
-nn = 21
-nLevels = 2
+nn = 11
 if parallel:
     nLevels = 1
+else:
+    nLevels = 4
 
 subgridError = None
 
 shockCapturing = None
 
-multilevelNonlinearSolver  = Newton
+if numerical_flux_flag == 'SIPG':
+    numericalFluxType = Advection_DiagonalUpwind_Diffusion_SIPG
+elif numerical_flux_flag == 'NIPG':
+    numericalFluxType = Advection_DiagonalUpwind_Diffusion_NIPG
+elif numerical_flux_flag == 'LDG':
+    numericalFluxType = Diffusion_LDG
+else:
+    numericalFluxType = Advection_DiagonalUpwind_Diffusion_IIPG
+
+multilevelNonlinearSolver  = NLNI
+
 levelNonlinearSolver = Newton
-maxNonlinearIts = 1
 
 fullNewtonFlag = True
 
@@ -40,23 +52,21 @@ if parallel:
     #-pc_type lu -pc_factor_mat_solver_package
     #can also set -pc_asm_overlap 2 with default asm type (restrict)
     levelLinearSolver = KSP_petsc4py
-    #pick number of layers to use in overlap 
-    nLayersOfOverlapForParallel = 1
-    #type of partition
-    parallelPartitioningType = MeshParallelPartitioningTypes.node
-    numericalFluxType = Advection_DiagonalUpwind_Diffusion_SIPG_exterior
+    #pick number of layers to use in overlap and type of partition
+    if numerical_flux_flag == 'LDG':
+        nLayersOfOverlapForParallel = 2
+    else:
+        nLayersOfOverlapForParallel = 1
+    parallelPartitioningType = MeshParallelPartitioningTypes.element
+    
     #to allow multiple models to set different ksp options
     #linear_solver_options_prefix = 'poisson_'
-    linearSmoother = None
 else:
     multilevelLinearSolver = LU
     levelLinearSolver = LU
     linearSolverConvergenceTest= 'r'
 
-
 linTolFac = 0.0
-l_atol_res = 1.0e-10
 
-cfluxtag  = 'p1-nc'
-conservativeFlux =  dict((i,cfluxtag) for i in range(nc))
-
+cfluxtag  = 'dg' #'dg-point-eval','dg'
+conservativeFlux = dict((i,cfluxtag) for i in range(nc))
