eef3a6dde560eefd866d750e5c00c997e3a48240
Chris Kees <cekees@gmail.com>
2013-06-25 10:42:51 -0500

    removing submodules for proteus 1.0

--- externalPackages/clawpack/setenv.bash
+++
@@ -1,8 +0,0 @@
-# Clawpack environment settings
-export CLAW=${PROTEUS}"/externalPackages/clawpack"
-export CLAWUTIL=${PROTEUS}"/externalPackages/clawpack/clawutil"
-export PYCLAW=${PROTEUS}"/externalPackages/clawpack/pyclaw"
-export RIEMANN=${PROTEUS}"/externalPackages/clawpack/riemann"
-export VISCLAW=${PROTEUS}"/externalPackages/clawpack/visclaw"
-export PYTHONPATH="${PROTEUS}/externalPackages/clawpack/visclaw/src/python:${PROTEUS}/externalPackages/clawpack/riemann/src/python:${PROTEUS}/externalPackages/clawpack/pyclaw/src:${PROTEUS}/externalPackages/clawpack/clawutil/src/python:${PYTHONPATH}"
-export MATLABPATH=${PROTEUS}"/externalPackages/clawpack/visclaw/src/matlab:${MATLABPATH}"
