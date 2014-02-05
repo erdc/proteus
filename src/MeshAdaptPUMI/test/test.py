d7bd0f1c3923c9096cd5a9a08dc6374e4d352bb3
Kedar Chitale <chitak@piglet.scorec.rpi.edu>
2014-02-05 00:34:57 -0500

    minor

--- src/MeshAdaptPUMI/test/test.py
+++ src/MeshAdaptPUMI/test/test.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
+#from pyadh import *
 #from proteus.iproteus import *
 from ctypes import *
 from proteus import MeshTools
 from proteus import cmeshTools
 from proteus import MeshAdaptPUMI
+from proteus import Archiver
+from tables import *
 
 #import mpi4py
-#mpi = CDLL('libmpi.so.0', RTLD_GLOBAL)
+mpi = CDLL('libmpi.so.0', RTLD_GLOBAL)
 #print dir(MeshAdaptPUMI)
 
 MeshAdaptPUMI = MeshAdaptPUMI.MeshAdaptPUMI()
@@ -15,17 +18,31 @@ MeshAdaptPUMI.helloworld('hello: Entering MeshAdaptPUMI')
 MeshAdaptPUMI.readGeomModel('yy')
 MeshAdaptPUMI.readPUMIMesh('geom.sms')
 
-mesh2 = MeshTools.Mesh()
-mesh2.cmesh = cmeshTools.CMesh()
+#mesh = MeshTools.Mesh()
+mesh = MeshTools.TetrahedralMesh()
+mesh.cmesh = cmeshTools.CMesh()
 
 #mesh2.buildFromC(mesh2.cmesh)
 
 #mesh = MeshAdaptPUMIDrvr.mesh_proteus
-MeshAdaptPUMI.initProteusMesh(mesh2)
-#MeshAdaptPUMIDrvr.ConstructFromPUMIMesh(mesh2.cmesh) #not working
+MeshAdaptPUMI.initProteusMesh(mesh)
+MeshAdaptPUMI.ConstructFromSerialPUMIMesh(mesh) #not working
 #cmeshTools.CMesh_FromMesh(mesh)
 
-#cmeshTools.writeTetgenFiles(cmesh,"tetgen",1)
-#MeshAdaptPUMIDrvr.deleteProteusMesh(mesh2)
+print "Done reading in mesh" 
 
+#cmeshTools.write3dmFiles(mesh,"mesh",1)
+#mesh.printMesh(mesh)
+mesh.writeMeshEnsight("mesh","n")
+#mesh.meshInfo()
+#mesh.writeMeshXdmf(cmesh,ar,"mesh",0.0,False,False,0,False)
+
+#cmeshTools.writeTetgenFiles(mesh,"tetgen",1)
+#cmeshTools.writeTetgenFiles(mesh,"tetgen",1)
+#mesh.writeTetgenFiles(mesh,"tetgen",1)
+MeshAdaptPUMI.helloworld('Done MeshAdaptPUMI')
+
+#cmeshTools.cmeshToolsDeleteMeshDataStructures(mesh2.cmesh)
+#from pyadh import vtkViewers as vtkViewers
+#vtkViewers.viewMesh(mesh2)
 
