6e0f2a413fe85bc51029d85c652e72a9b30c4531
Dan Ibanez <dan.a.ibanez@gmail.com>
2014-06-12 15:13:50 -0400

    got mesh fields to compile

--- src/MeshAdaptPUMI/MeshFields.cpp
+++ src/MeshAdaptPUMI/MeshFields.cpp
@@ -21,10 +21,8 @@ void MeshAdaptPUMIDrvr::freeNumbering(apf::Numbering*& n)
 int MeshAdaptPUMIDrvr::TransferSolutionToPUMI(double* inArray, int nVar, int nN)
 {
   assert(nN == mesh->count(0));
-
   numVar = nVar;
-  solution = apf::createPackedField(mesh, "proteus_solution", nVar);
-
+  solution = apf::createPackedField(m, "proteus_solution", nVar);
   apf::NewArray<double> tmp(nVar);
   apf::MeshEntity* v;
   apf::MeshIterator* it = m->begin(0);
@@ -32,10 +30,11 @@ int MeshAdaptPUMIDrvr::TransferSolutionToPUMI(double* inArray, int nVar, int nN)
   while ((v = m->iterate(it))) {
     for(int j = 0; j < nVar; j++)
       tmp[j] = inArray[j * nN + i];
-    apf::setComponents(solution, v, 0, tmp); 
+    apf::setComponents(solution, v, 0, &tmp[0]); 
     ++i;
   }
   m->end(it);
+  return 0;
 }
 
 int MeshAdaptPUMIDrvr::TransferSolutionToProteus(double* outArray, int nVar, int nN)
@@ -46,9 +45,9 @@ int MeshAdaptPUMIDrvr::TransferSolutionToProteus(double* outArray, int nVar, int
   apf::MeshIterator* it = m->begin(0);
   size_t i = 0;
   while ((v = m->iterate(it))) {
-    apf::getComponents(solution, v, 0, tmp); 
+    apf::getComponents(solution, v, 0, &tmp[0]); 
     for(int j = 0; j < nVar; j++)
-      inArray[j * nN + i] = tmp[j];
+      outArray[j * nN + i] = tmp[j];
     ++i;
   }
   m->end(it);
