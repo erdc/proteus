2f0948973cc2f472688b04791a0427ba5d5c65a2
Matthew Farthing <mwfarthing@gmail.com>
2014-12-26 15:28:27 -0600

    adds archiving option for spatial residual if simFlagsList[i]['storeQuantities'] contains 'pod_residuals'. See test_DEIM for another smoke test

--- test/POD/test_DEIM.py
+++ test/POD/test_DEIM.py
@@ -8,7 +8,7 @@ import numpy.testing as npt
 from nose.tools import ok_ as ok
 from nose.tools import eq_ as eq
 
-def get_burgers_ns(name,T=0.1,nDTout=10):
+def get_burgers_ns(name,T=0.1,nDTout=10,archive_space_res=False):
     import burgers_init as bu
     bu.physics.name=name
     bu.so.name = bu.physics.name
@@ -17,15 +17,27 @@ def get_burgers_ns(name,T=0.1,nDTout=10):
     bu.nDTout=nDTout
     bu.DT=bu.T/float(bu.nDTout)
     bu.so.tnList = [i*bu.DT for i in range(bu.nDTout+1)]
-    ns = bu.NumericalSolution.NS_base(bu.so,[bu.physics],[bu.numerics],bu.so.sList,bu.opts)
+    #request archiving of spatial residuals ...
+    if archive_space_res:
+        simFlagsList=[{}]
+        simFlagsList[0]['storeQuantities']=['pod_residuals']
+    ns = bu.NumericalSolution.NS_base(bu.so,[bu.physics],[bu.numerics],bu.so.sList,bu.opts,simFlagsList=simFlagsList)
     return ns
 
 def test_burger_run():
+    """
+    Aron's favority smoke test to see if burgers runs
+    """
     ns = get_burgers_ns("test_run",T=0.1,nDTout=10)
     failed = ns.calculateSolution("run_smoke_test")
     assert not failed
 
 def test_residual_split():
+    """
+    just tests that R=R_s+R_t for random dof vector in [0,1]
+
+    Here R_s and R_t are the spatial and mass residuals
+    """
     ns = get_burgers_ns("test_res_split",T=0.05,nDTout=5)
     failed = ns.calculateSolution("run_res_test")
     assert not failed
@@ -39,11 +51,19 @@ def test_residual_split():
     model.getMassResidual(u_tmp,res_t)
     
     res_t += res_s
-
     npt.assert_almost_equal(res,res_t)
+
+def test_res_archive():
+    """
+    smoke test if numerical solution can archive 'spatial residuals' to xdmf  
+    """
+    ns = get_burgers_ns("test_space_res_archive",T=0.1,nDTout=10,archive_space_res=True)
+    
+    failed = ns.calculateSolution("run_space_res_smoke_test")
+    assert not failed
 
 if __name__ == "__main__":
     from proteus import Comm
     comm = Comm.init()
     import nose
-    nose.main(defaultTest='test_DEIM:test_residual_split')
+    nose.main(defaultTest='test_DEIM:test_res_archive')
