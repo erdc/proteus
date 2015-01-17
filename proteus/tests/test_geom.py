03eabb418fa62ccadc4cbd8bcbd79ba05351dd36
Aron Ahmadia <aron@ahmadia.net>
2015-01-17 00:41:02 -0500

    Add working/tested intersection of line/polyhedron

--- proteus/tests/test_geom.py
+++ proteus/tests/test_geom.py
@@ -19,6 +19,21 @@ def test_intersect_edges():
                      [[3, 1, 2]])
 
     # check a proper 3D intersection
-
     npt.assert_equal(Geom.intersectEdges([(1, 1, 0), (1, 1, 2)], [[(5, 5, 5), (-1, -1, -1)],]),
                      [[1, 1, 1]])
+
+def test_intersect_polyhedron():
+    # check 2-D triangle intersection
+    l = ((-1, 0.5), (1, 0.5))
+    p = (((-1, 0), (0, 0)), ((0, -1), (0, 0)), ((1, 1), (0.5, 0.5)))
+    npt.assert_equal(Geom.intersectPolyhedron(l, p),
+                     ((0., 0.5), ((0.5, 0.5))))
+
+    # check 2-D square intersection in 3 space
+    l = ((0, 0, 0), (2, 2, 0))
+    p = (((-1, 0, 0), (0, 0, 0)),
+         ((0, -1, 0), (0, 0, 0)),
+         ((1, 0, 0), (1, 1, 0)),
+         ((0, 1, 0), (1, 1, 0)))
+    npt.assert_equal(Geom.intersectPolyhedron(l, p),
+                     ((0., 0., 0.), ((1., 1., 0.))))
