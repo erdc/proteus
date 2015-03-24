d34068375648e860f3cf8c0aa99f49a1f5c97874
cekees <cekees@gmail.com>
2015-03-24 13:20:49 -0500

    added unit tests and cleaned up

--- proteus/Context.py
+++ proteus/Context.py
@@ -1,13 +1,40 @@
-"""A global user context object"""
+"""Support for a global user context
+
+Sub-models in multi-physics simulations sometimes need to be consistent
+with other sub-models through certain "global" information. This model
+provides methods for setting and getting such a global user context.
+It also alows declaring input options to the user context that can be
+set from the command line.
+
+Example (set the global context from an object):
+
+#create a simple context class
+globalSettings = {"nnx":11, "T":10.0, "g"=9.8}
+MyContext = namedtuple("MyContext",globalSettings.keys())
+#set the global  context  object
+proteus.Context.set(MyContext._make(globalSettings.values())
+
+Example (set the global context from a module):
+
+import globalSettingsModule
+proteus.Context.setFromModule(globalSettingsModule)
+
+Example (use the global context):
+
+ct = proteus.Context.get()
+nnx = ct.nnx
+"""
 from collections import  namedtuple
 from .Profiling import logEvent
 
 """The global context"""
 context = None
 
+"""A string containing Python setting input options"""
 contextOptionsString = None
 
 def get():
+    """Get the global context object"""
     global context
     return context
 
@@ -17,23 +44,36 @@ def set(contextIn):
     context = contextIn
 
 def setFromModule(moduleIn):
-    """Construct the context object from a module"""
+    """Construct the global context object from a module"""
     global context
     fields = {}
-    for key,value in moduleIn.__dict__.iteritems():
+    for key, value in moduleIn.__dict__.iteritems():
         if  key[:2] != "__":
             fields[key]=value
-    Context = namedtuple(moduleIn.__name__,fields.keys(),verbose=False)
+    Context = namedtuple(moduleIn.__name__.split('.')[-1], fields.keys(), verbose=False)
     context = Context._make(fields.values())
 
-def declareAndGetInputOptions(optionsList=None):
-    """Return a named tuple of the context options"""
-    global contextOptionsString,globalContextOptions
+def Options(optionsList=None):
+    """Construct an o
+    from proteus.LinearAlgebraToptions object (named tuple)
+    
+    :param optionsList: An iteratble of options tuples. Each option is a 3-tuple,
+    with the first entry the option name string, the second entry the default value,
+    and the third entry a help string
+
+    Example:
+
+    opts=Context.Options([("nnx", 11, "number of mesh nodes in x-direction"),
+                          ("nny", 21, "number of mesh nodes in y-direction"])
+    nnx = opts.nnx
+    nny = opts.nny
+    """
+    import ast, sys
+    global contextOptionsString
     contextOptionsDict = {}
     help="Context input options:\n"
     for optTuple  in optionsList:
-        print optTuple
-        help += """{0}[{1}] {2}\n\n""".format(optTuple[0],optTuple[1],optTuple[2])
+        help += """{0}[{1}] {2}\n\n""".format(optTuple[0], optTuple[1], optTuple[2])
         contextOptionsDict[optTuple[0]] = optTuple[1]
     logEvent(help)
     if contextOptionsString=="?":
@@ -42,17 +82,17 @@ def declareAndGetInputOptions(optionsList=None):
     if contextOptionsString != None:
         option_overides=contextOptionsString.split(" ")
         for option in option_overides:
-            lvalue,rvalue = option.split("=")
+            lvalue, rvalue = option.split("=")
             if contextOptionsDict.has_key(lvalue):
                 logEvent("Processing context input options from commandline")
                 try:
-                    contextOptionsDict[lvalue] = eval(rvalue)
+                    contextOptionsDict[lvalue] = ast.literal_eval(rvalue)
                     logEvent(lvalue+" = "+rvalue)
                 except:
-                    print "Failed setting context options from command line string."
+                    sys.stderr.write("Failed setting context options from command line string.")
                     raise 
             else:
                 logEvent("IGNORING CONTEXT OPTION; DECLARE "+lvalue+" IF YOU WANT TO SET IT")
     #now set named tuple merging optionsList and opts_cli_dihelpct
-    ContextOptions = namedtuple("ContextOptions",contextOptionsDict.keys(),verbose=False)
+    ContextOptions = namedtuple("ContextOptions", contextOptionsDict.keys(), verbose=False)
     return ContextOptions._make(contextOptionsDict.values())
