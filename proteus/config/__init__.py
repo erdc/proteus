5be0009c8258003c30f4ccc7db221d81045ee4b0
cekees <cekees@gmail.com>
2015-03-25 00:50:04 -0400

    cekees/lightning_afrl_testing

--- proteus/config/__init__.py
+++ proteus/config/__init__.py
@@ -2,7 +2,9 @@ import os
 
 if 'HOSTNAME' in os.environ and (os.environ['HOSTNAME'].startswith('garnet')
                                  or
-                                 os.environ['HOSTNAME'].startswith('copper')):
+                                 os.environ['HOSTNAME'].startswith('copper')
+                                 or
+                                 os.environ['HOSTNAME'].startswith('lightning')):
     from garnet import *
 else:
     from default import *
