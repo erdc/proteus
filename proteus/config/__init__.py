9b874996eb159685c802c24911cb97750d95ca42
cekees <cekees@gmail.com>
2015-03-24 20:59:59 -0500

    removed nested if  with no else to load default

--- proteus/config/__init__.py
+++ proteus/config/__init__.py
@@ -1,7 +1,8 @@
 import os
 
-if 'HOSTNAME' in os.environ:
-    if os.environ['HOSTNAME'].startswith('garnet') or os.environ['HOSTNAME'].startswith('copper'):
-        from garnet import *
+if 'HOSTNAME' in os.environ and (os.environ['HOSTNAME'].startswith('garnet')
+                                 or
+                                 os.environ['HOSTNAME'].startswith('copper')):
+    from garnet import *
 else:
     from default import *
