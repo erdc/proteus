dfea8057e2b0ec714136a92edaf18e88b36ed856
cekees <cekees@gmail.com>
2015-04-02 13:10:31 -0500

    fixed a few references, verified that heat and burgers run

--- proteus/tests/POD/svd_burgers.py
+++ proteus/tests/POD/svd_burgers.py
@@ -13,14 +13,14 @@ archive = Archiver.XdmfArchive(".","burgers_3d",readOnly=True)
 import numpy as np
 
 #generate snapshots for solution
-S = deim_utils.read_snapshots(archive,nDTout+1,'u',)
+S = read_snapshots(archive,nDTout+1,'u',)
 U, s, V = np.linalg.svd(S, full_matrices=False)
 print 'SVD for solution done!'
 np.savetxt('SVD_basis', U, delimiter=' ')
 np.savetxt('Singular_values', s, delimiter=' ')
 
-if burgers_init.use_deim:
-    Sf = deim_utils.read_snapshots(archive,nDTout+1,'spatial_residual0')
+if use_deim:
+    Sf = read_snapshots(archive,nDTout+1,'spatial_residual0')
     Uf,sf,Vf = np.linalg.svd(Sf,full_matrices=False)
     print 'SVD for spatial residual done!'
     np.savetxt('Fs_SVD_basis', Uf, delimiter=' ')
