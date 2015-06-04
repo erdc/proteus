c1eede6faf10ffe7342b51e93b2011ff82200b67
cekees <cekees@gmail.com>
2015-06-04 17:55:12 -0500

    turned  of  forceStrongConditions

--- proteus/Transport.py
+++ proteus/Transport.py
@@ -859,7 +859,7 @@ class OneLevelTransport(NonlinearEquation):
                                        max(1,self.nSpace_global-1)),
                                       'd')
         log(memory("global exterior element boundary quadrature","OneLevelTransport"),level=4)
-        self.forceStrongConditions=True
+        self.forceStrongConditions=False
         self.dirichletConditionsForceDOF = {}
         if self.forceStrongConditions:
             for cj in range(self.nc):
