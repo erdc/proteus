abf714f09c8625bb96f31795e9b28e96285ccc46
zhang-alvin <zhanga@portal2.scorec.rpi.edu>
2015-06-26 01:13:18 -0400

    Added capability of computing viscosity field using hardcoded viscosities and phi field.

--- src/MeshAdaptPUMI/ERM.cpp
+++ src/MeshAdaptPUMI/ERM.cpp
@@ -7,13 +7,55 @@
 #include <apfShape.h>
 #include <apfDynamicMatrix.h>
 
-void averageToEntity(apf::Field* ef, apf::Field* vf, apf::MeshEntity* ent);
+#include <iostream>
+
+enum {
+  PHI_IDX = 5
+};
+
+static void averageToEntity(apf::Field* ef, apf::Field* vf, apf::MeshEntity* ent);
+static apf::Field* extractPhi(apf::Field* solution);
 
 void MeshAdaptPUMIDrvr::get_local_error() 
 //This function aims to compute error at each element via ERM.
 //First get the mesh and impose a 2nd order field
 //Then get the desired quadrature points
 {
+
+  //***** Viscosity hard-coded hook *****//
+  double nu_0 = 1.004e-6; //water 
+  double nu_1 = 1.5e-5; //air
+
+  //***** Get Phi First *****//
+  apf::Field* phif = extractPhi(solution);
+  apf::Field* visc = apf::createLagrangeField(m,"viscosity",apf::SCALAR,1);
+  //*****               *****//
+  
+  //***** Compute the viscosity field *****//
+
+  apf::Mesh*m = apf::getMesh(solution); 
+  apf::MeshEntity* ent;
+  apf::MeshIterator* iter = m->begin(0);
+  double phi_val, H, visc_val;
+  while(ent = m->iterate(iter)){ //loop through all elements
+    //std::cout<<"What is the value for phif? "<<apf::getScalar(phif,ent,0)<<std::endl; 
+    phi_val=apf::getScalar(phif,ent,0);
+    if(phi_val < 0){ //gas
+      H = 0;
+    }
+    else if(phi_val > 0){ //liquid
+      H = 1;
+    }
+    else{ //at interface
+      H = 0.5;
+    }
+    visc_val = nu_1 + (nu_0-nu_1)*H;
+    apf::setScalar(visc, ent, 0,visc_val);
+  }
+  m->end(iter);
+  apf::writeVtkFiles("viscosity", m);
+/*
+  //Start computing element quantities
   int numqpt; //number of quadrature points
   int nshl; //number of local shape functions
   int elem_type; //what type of topology
@@ -64,11 +106,11 @@ void MeshAdaptPUMIDrvr::get_local_error()
       elem_shape->getValues(NULL,NULL,qpt,shpval);
       elem_shape->getLocalGradients(NULL,NULL,qpt,shgval); 
       invJ_copy = apf::fromMatrix(invJ);
-/*
+
       std::cout<<"Jdet "<<Jdet<<std::endl;
       std::cout<<"invJ "<<invJ_copy<<std::endl;
       std::cout<<"What is qpt and weight? "<<qpt<<" "<<weight<<std::endl;
-*/
+
       for(int i =0;i<nshl;i++){ //get the true derivative
         shgval_copy[i] = apf::fromVector(shgval[i]);
         //std::cout<<"What is shgval? "<<shgval_copy[i]<<std::endl;
@@ -76,7 +118,7 @@ void MeshAdaptPUMIDrvr::get_local_error()
         //std::cout<<"What is shdrv? "<<shdrv[i]<<std::endl;
       }
 
-/*
+
     VecCreate(PETSC_COMM_SELF,&F); 
     VecSetSizes(F,5,5);
     VecSetFromOptions(F); 
@@ -86,14 +128,18 @@ void MeshAdaptPUMIDrvr::get_local_error()
     }
     VecView(F,PETSC_VIEWER_STDOUT_SELF);
     VecDestroy(&F);
-*/
+
     }
   }
   m->end(iter);
+*/
+  apf::destroyField(phif);
+  apf::destroyField(visc);
   printf("It cleared the function.\n");
 }
 
-void averageToEntity(apf::Field* ef, apf::Field* vf, apf::MeshEntity* ent) //taken from Dan's superconvergent patch recovery code
+//used to attach error estimates to nodes
+static void averageToEntity(apf::Field* ef, apf::Field* vf, apf::MeshEntity* ent) //taken from Dan's superconvergent patch recovery code
 {
   apf::Mesh* m = apf::getMesh(ef);
   apf::Adjacent elements;
@@ -103,5 +149,21 @@ void averageToEntity(apf::Field* ef, apf::Field* vf, apf::MeshEntity* ent) //tak
     s += apf::getScalar(ef, elements[i], 0);
   s /= elements.getSize();
   apf::setScalar(vf, ent, 0, s);
+  return;
 }
 
+static apf::Field* extractPhi(apf::Field* solution)
+{
+  apf::Mesh* m = apf::getMesh(solution);
+  apf::Field* phif = apf::createLagrangeField(m,"proteus_phi",apf::SCALAR,1);
+  apf::MeshIterator* it = m->begin(0);
+  apf::MeshEntity* v;
+  apf::NewArray<double> tmp(apf::countComponents(solution));
+  while ((v = m->iterate(it))) {
+    apf::getComponents(solution, v, 0, &tmp[0]);
+    double phi = tmp[PHI_IDX];
+    apf::setScalar(phif, v, 0, phi);
+  }
+  m->end(it);
+  return phif;
+}
