108aed902a1c99064a08b16bc6752caba64e3f6c
zhang-alvin <zhanga@mastermind.scorec.rpi.edu>
2015-08-12 14:44:21 -0400

    modified boundary conditions for couette flow case

--- src/MeshAdaptPUMI/test/splash-cube/test-1-Procs/twp_navier_stokes_p.py
+++ src/MeshAdaptPUMI/test/splash-cube/test-1-Procs/twp_navier_stokes_p.py
@@ -29,7 +29,6 @@ coefficients = RANS2P.Coefficients(epsFact=epsFact_viscosity,
                                    turbulenceClosureModel=ns_closure)
 
 Uinf = 1.0
-
 '''
 def getDBC_p(x,flag):
     if flag == boundaryTags['top'] or x[2] >= L[2] - 1.0e-12:
@@ -40,20 +39,21 @@ def getDBC_u(x,flag):
         return lambda x,t: 0.0 
 
 def getDBC_v(x,flag):
-    if flag == boundaryTags['top'] or x[2] >= L[2] - 1.0e-12:
-    #if flag == boundaryTags['top'] or x[2] >= L[2] - 1.0e-12 or flag==boundaryTags['bottom'] or x[2] <= 1.0e-12 or flag==boundaryTags['right'] or x[0] >= L[0] - 1.0e-12 or flag==boundaryTags['left'] or x[0] <= 1.0e-12 or flag==boundaryTags['front'] or x[1]<=1.0e-12 or flag==boundaryTags['back'] or x[1]>L[1]-1.0e-12 : 
-        return lambda x,t: 1.0#Uinf*x[2]/L[2]
+    #if flag == boundaryTags['top'] or x[2] >= L[2] - 1.0e-12:
+    if flag == boundaryTags['top'] or x[2] >= L[2] - 1.0e-12 or flag==boundaryTags['bottom'] or x[2] <= 1.0e-12 or flag==boundaryTags['right'] or x[0] >= L[0] - 1.0e-12 or flag==boundaryTags['left'] or x[0] <= 1.0e-12 or flag==boundaryTags['front'] or x[1]<=1.0e-12 or flag==boundaryTags['back'] or x[1]>L[1]-1.0e-12 : 
+        return lambda x,t: Uinf*x[2]/L[2]
 
 def getDBC_w(x,flag):
     #if flag == boundaryTags['top'] or x[2] >= L[2] - 1.0e-12:
     if flag == boundaryTags['top'] or x[2] >= L[2] - 1.0e-12 or flag==boundaryTags['bottom'] or x[2] <= 1.0e-12 or flag==boundaryTags['right'] or x[0] >= L[0] - 1.0e-12 or flag==boundaryTags['left'] or x[0] <= 1.0e-12 or flag==boundaryTags['front'] or x[1]<=1.0e-12 or flag==boundaryTags['back'] or x[1]>L[1]-1.0e-12 : 
         return lambda x,t: 0.0
+
 '''
 def getDBC_p(x,flag):
-    if flag == boundaryTags['back']:
+    if flag == boundaryTags['top']:
     #if flag == boundaryTags['top'] or flag==boundaryTags['bottom'] or flag==boundaryTags['front'] or flag==boundaryTags['left'] or flag==boundaryTags['right']:
     #if flag==boundaryTags['top'] or flag==boundaryTags['bottom'] or flag==boundaryTags['front'] or flag==boundaryTags['left'] or flag==boundaryTags['right'] or flag==boundaryTags['back']:
-        return lambda x,t: 0.0
+        return lambda x,t: 1.0
     
 def getDBC_u(x,flag):
     if flag == boundaryTags['top'] or flag==boundaryTags['bottom'] or flag==boundaryTags['front'] or flag==boundaryTags['left'] or flag==boundaryTags['right']:
@@ -62,24 +62,16 @@ def getDBC_u(x,flag):
 
 def getDBC_v(x,flag):
     #if flag == boundaryTags['top']:
-    if flag==boundaryTags['top'] or flag==boundaryTags['bottom'] or flag==boundaryTags['front'] or flag==boundaryTags['left'] or flag==boundaryTags['right']:# or flag==boundaryTags['back']:
-        #print x[2], L[2]
-        #return lambda x,t: x[2]*1.0/L[2]
-        #return lambda x,t: Decimal(x[2])/Decimal(L[2])
-        if x[2]>L[2]:
-          return lambda x,t: 1.0
-        else:
-          return lambda x,t: x[2]/L[2]   
-        #return lambda x,t: 1.0
-    #elif flag ==boundaryTags['top'] or x[2] > L[2]-1.0e-12:
-    #    return lambda x,t: 1.0
+    if flag==boundaryTags['top'] or x[2] >= L[2] - 1.0e-12 or  flag==boundaryTags['bottom'] or flag==boundaryTags['front'] or flag==boundaryTags['left'] or flag==boundaryTags['right']:# or flag==boundaryTags['back']:
+        return lambda x,t: 0.0#x[2]*1.0/L[2]
 
 def getDBC_w(x,flag):
     #if flag == boundaryTags['top']:
     if flag == boundaryTags['top'] or flag==boundaryTags['bottom'] or flag==boundaryTags['front'] or flag==boundaryTags['left'] or flag==boundaryTags['right']:
     #if flag==boundaryTags['top'] or flag==boundaryTags['bottom'] or flag==boundaryTags['front'] or flag==boundaryTags['left'] or flag==boundaryTags['right'] or flag==boundaryTags['back']:
         return lambda x,t: 0.0
-    
+#'''   
+ 
 dirichletConditions = {0:getDBC_p,
                        1:getDBC_u,
                        2:getDBC_v,
@@ -91,13 +83,18 @@ def getAFBC_p(x,flag):
         pass
 
 def getAFBC_u(x,flag):
+#    if flag==boundaryTags['back']:
+#        return lambda x,t: 0.0
         pass
 
 def getAFBC_v(x,flag):
-        return lambda x,t: 1.0
-        #pass
+#    if flag==boundaryTags['back']:
+#        return lambda x,t: x[2]/L[2]
+        pass
 
 def getAFBC_w(x,flag):
+#    if flag==boundaryTags['back']:
+#        return lambda x,t: 0.0
         pass
 
 def getDFBC_u(x,flag):
@@ -108,7 +105,7 @@ def getDFBC_u(x,flag):
 def getDFBC_v(x,flag):
    if flag==boundaryTags['back']:
         return lambda x,t: 0.0
-#        pass
+        pass
 
 def getDFBC_w(x,flag):
     if flag==boundaryTags['back']:
