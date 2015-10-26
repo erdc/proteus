0c77bc3020342962d7c4bb79e4333b1399d1aadb
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-10-26 17:02:01 -0400

    ctor arg for which size field algorithm to use

--- proteus/MeshAdaptPUMI/MeshAdaptPUMI.pyx
+++ proteus/MeshAdaptPUMI/MeshAdaptPUMI.pyx
@@ -15,7 +15,7 @@ cdef extern from "cmeshToolsModule.h":
 
 cdef extern from "MeshAdaptPUMI/MeshAdaptPUMI.h":
     cdef cppclass MeshAdaptPUMIDrvr:
-        MeshAdaptPUMIDrvr(double, double, int)
+        MeshAdaptPUMIDrvr(double, double, int, char*)
         int numIter, numAdaptSteps
         int loadModelAndMesh(char *, char*)
         int ConstructFromSerialPUMIMesh(Mesh&)
@@ -35,9 +35,9 @@ cdef class MeshAdaptPUMI:
     cdef MeshAdaptPUMIDrvr *thisptr
     cdef double hmax, hmin
     cdef int numIter, numAdaptSteps
-    def __cinit__(self, hmax=100.0, hmin=1e-8, numIter=10):
+    def __cinit__(self, hmax=100.0, hmin=1e-8, numIter=10, sfConfig="farhad"):
         print hmax,hmin,numIter
-        self.thisptr = new MeshAdaptPUMIDrvr(hmax, hmin, numIter)
+        self.thisptr = new MeshAdaptPUMIDrvr(hmax, hmin, numIter, sfConfig)
     def __dealloc__(self):
         del self.thisptr
     def loadModelAndMesh(self, geomName, meshName):
