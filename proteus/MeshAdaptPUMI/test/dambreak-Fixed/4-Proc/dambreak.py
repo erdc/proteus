e762525b8f487d3829b8c5fa2ae3e9a19ed75702
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-10-26 16:29:31 -0400

    remove old ctor strings from dambreak

--- proteus/MeshAdaptPUMI/test/dambreak-Fixed/4-Proc/dambreak.py
+++ proteus/MeshAdaptPUMI/test/dambreak-Fixed/4-Proc/dambreak.py
@@ -97,7 +97,7 @@ else:
         nnx=4*Refinement
         nny=2*Refinement
     else:
-        domain = Domain.PUMIDomain(fileprefix="Dambreak.smb",modelfile="Dambreak.smd") #initialize the domain
+        domain = Domain.PUMIDomain() #initialize the domain
         domain.numBC=6 #set number of BCs
         domain.numAdaptSteps=6 #set number of adapt steps (loops)
         #Following sets list of face tags of geometric model as mapped from boundary Tags,
