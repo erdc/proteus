4b8771ebecb317b4b4f398286473a6959f098e82
cekees <cekees@gmail.com>
2015-10-27 22:48:41 -0500

    some hacks  just to get it running with new adaption loop

--- proteus/MeshAdaptPUMI/test/dambreak-Fixed/1-Proc/ls_p.py
+++ proteus/MeshAdaptPUMI/test/dambreak-Fixed/1-Proc/ls_p.py
@@ -6,9 +6,9 @@ from proteus.mprans import NCLS
 LevelModelType = NCLS.LevelModel
 
 coefficients = NCLS.Coefficients(V_model=0,RD_model=3,ME_model=2,
-                                 checkMass=False, useMetrics=useMetrics,
+                                 checkMass=True, useMetrics=useMetrics,
                                  epsFact=epsFact_consrv_heaviside,sc_uref=ls_sc_uref,sc_beta=ls_sc_beta)
- 
+
 def getDBC_ls(x,flag):
     return None
 
@@ -17,8 +17,8 @@ dirichletConditions = {0:getDBC_ls}
 advectiveFluxBoundaryConditions =  {}
 diffusiveFluxBoundaryConditions = {0:{}}
 
-class PerturbedSurface_phi:       
+class PerturbedSurface_phi:
     def uOfXT(self,x,t):
         return signedDistance(x)
-    
+
 initialConditions  = {0:PerturbedSurface_phi()}
