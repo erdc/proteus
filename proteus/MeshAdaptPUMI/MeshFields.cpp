3412b3abfc34fc5cd4942c6b1c83311092591e84
Dan Ibanez <dan.a.ibanez@gmail.com>
2015-11-03 20:48:21 -0500

    adding code to copy field back to proteus

--- proteus/MeshAdaptPUMI/MeshFields.cpp
+++ proteus/MeshAdaptPUMI/MeshFields.cpp
@@ -30,16 +30,15 @@ int MeshAdaptPUMIDrvr::transferSolutionToPUMI(double* inArray, int nVar, int nN)
     int i = localNumber(v);
     for(int j = 0; j < nVar; j++)
       tmp[j] = inArray[j * nN + i];
-    apf::setComponents(solution, v, 0, &tmp[0]); 
+    apf::setComponents(solution, v, 0, &tmp[0]);
   }
   m->end(it);
   return 0;
 }
 
-int MeshAdaptPUMIDrvr::transferFieldToPUMI(const char* name, double* inArray,
+int MeshAdaptPUMIDrvr::transferFieldToPUMI(const char* name, double const* inArray,
     int nVar, int nN)
 {
-  fprintf(stderr, "got nVar = %d nN = %d\n", nVar, nN);
   assert(nN == static_cast<int>(m->count(0)));
   apf::Field* f = m->findField(name);
   if (!f) {
@@ -58,14 +57,34 @@ int MeshAdaptPUMIDrvr::transferFieldToPUMI(const char* name, double* inArray,
     int i = localNumber(v);
     for(int j = 0; j < nVar; j++)
       tmp[j] = inArray[i * nVar + j];
-    apf::setComponents(f, v, 0, &tmp[0]); 
+    apf::setComponents(f, v, 0, &tmp[0]);
+  }
+  m->end(it);
+  return 0;
+}
+
+int MeshAdaptPUMIDrvr::transferFieldToProteus(const char* name, double* outArray,
+    int nVar, int nN)
+{
+  assert(nN == static_cast<int>(m->count(0)));
+  apf::Field* f = m->findField(name);
+  assert(f);
+  assert(apf::countComponents(f) == nVar);
+  apf::NewArray<double> tmp(nVar);
+  apf::MeshEntity* v;
+  apf::MeshIterator* it = m->begin(0);
+  while ((v = m->iterate(it))) {
+    int i = localNumber(v);
+    apf::getComponents(f, v, 0, &tmp[0]);
+    for(int j = 0; j < nVar; j++)
+      outArray[i * nVar + j] = tmp[j];
   }
   m->end(it);
   return 0;
 }
 
 int MeshAdaptPUMIDrvr::transferPropertiesToPUMI(double* rho_p, double* nu_p)
-{ 
+{
  rho[0] = rho_p[0]; rho[1] = rho_p[1];
  nu[0] = nu_p[0]; nu[1] = nu_p[1];
  return 0;
@@ -79,7 +98,7 @@ int MeshAdaptPUMIDrvr::transferSolutionToProteus(double* outArray, int nVar, int
   apf::MeshIterator* it = m->begin(0);
   while ((v = m->iterate(it))) {
     int i = localNumber(v);
-    apf::getComponents(solution, v, 0, &tmp[0]); 
+    apf::getComponents(solution, v, 0, &tmp[0]);
     for(int j = 0; j < nVar; j++)
       outArray[j * nN + i] = tmp[j];
   }
@@ -94,14 +113,14 @@ int MeshAdaptPUMIDrvr::transferBCtagsToProteus(int* tagArray,int idx, int* ebN, 
   apf::MeshIterator* it= m->begin(2);
   apf::MeshEntity* f;
   apf::ModelEntity* me;
-  apf::ModelEntity* boundary_face; 
+  apf::ModelEntity* boundary_face;
   int tag = 0;
   int fID,type,boundary_ID;
   int numqpt;
   int count = 0;
 
   char label[9],labelflux[6],type_flag;
-  
+
   //assign a label to the BC type tag
   if(idx == 0) sprintf(&type_flag,"p");
   else if(idx == 1) sprintf(&type_flag,"u");
@@ -140,7 +159,7 @@ int MeshAdaptPUMIDrvr::transferBCtagsToProteus(int* tagArray,int idx, int* ebN, 
     } //end if boundary face
   } //end face loop
   m->end(it);
-  
+
   std::cout<<"Finished Transfer of BC Tags "<<std::endl;
   return 0;
 }
@@ -149,9 +168,9 @@ int MeshAdaptPUMIDrvr::transferBCtagsToProteus(int* tagArray,int idx, int* ebN, 
 int MeshAdaptPUMIDrvr::transferBCsToProteus()
 {
 /*
-  //Want to use some sort of Hierarchic projection 
+  //Want to use some sort of Hierarchic projection
   apf::FieldShape* BC_shape = apf::getHierarchic(2);
-  apf::MeshEntity* v;  
+  apf::MeshEntity* v;
   int nVar = 4; //pressure + 3 velocity components
   //DBC = apf::createPackedField(m, "proteus_DBC", nVar);
   fluxBC = apf::createField(m, "proteus_fluxBC",apf::VECTOR, BC_shape);
@@ -160,7 +179,7 @@ int MeshAdaptPUMIDrvr::transferBCsToProteus()
   apf::MeshIterator* it= m->begin(2);
   apf::MeshEntity* f;
   apf::ModelEntity* me;
-  apf::ModelEntity* boundary_face; 
+  apf::ModelEntity* boundary_face;
   int tag = 0;
   int count = 0;
   int numqpt;
@@ -182,16 +201,16 @@ int MeshAdaptPUMIDrvr::transferBCsToProteus()
     boundary_face = m->findModelEntity(2,tag); //faces
     if(me==boundary_face){
       double test[numqpt];
-      for(int i=0;i<numqpt;i++){test[i]=i;} 
+      for(int i=0;i<numqpt;i++){test[i]=i;}
       m->setDoubleTag(f,fluxtag[1],test);
       double data[numqpt];
       m->getDoubleTag(f,fluxtag[1],&data[0]);
 //std::cout<<"What is the data? "<<data[0]<<" "<<data[5]<<" numqpt "<<numqpt<<std::endl;
       //get adjacent vertices
-      m->getAdjacent(f,0,adjvtx); 
+      m->getAdjacent(f,0,adjvtx);
       //evaluate boundary condition and set value
 
-      //get adjacent edges 
+      //get adjacent edges
       m->getAdjacent(f,1,adjedg);
     }
   }
