880004929f94cd7373310b70496f0f53312d4e84
zhang-alvin <zhanga@romulus.scorec.rpi.edu>
2016-01-11 21:49:02 -0500

    Removed parasolid loading from load Simmetrix BC

--- proteus/MeshAdaptPUMI/MeshAdaptPUMI.pyx
+++ proteus/MeshAdaptPUMI/MeshAdaptPUMI.pyx
@@ -19,7 +19,7 @@ cdef extern from "MeshAdaptPUMI/MeshAdaptPUMI.h":
         MeshAdaptPUMIDrvr(double, double, int, char*, char*)
         int numIter, numAdaptSteps
         int loadModelAndMesh(char *, char*)
-        int getSimmetrixBC(char*, char*)
+        int getSimmetrixBC(char*)
         int constructFromSerialPUMIMesh(Mesh&)
         int constructFromParallelPUMIMesh(Mesh&, Mesh&)
         int updateMaterialArrays(Mesh&, int, int)
@@ -45,7 +45,7 @@ cdef class MeshAdaptPUMI:
     def loadModelAndMesh(self, geomName, meshName):
         return self.thisptr.loadModelAndMesh(geomName, meshName)
     def getSimmetrixBC(self,geomFile,modelFile):
-        return self.thisptr.getSimmetrixBC(geomFile,modelFile)
+        return self.thisptr.getSimmetrixBC(modelFile)
     def constructFromSerialPUMIMesh(self, cmesh):
         cdef CMesh* cmesh_ptr = <CMesh*>cmesh
         return self.thisptr.constructFromSerialPUMIMesh(cmesh_ptr.mesh)
