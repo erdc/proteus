2a241d211905bab4042e503db5f53709a599654d
zhang-alvin <zhanga@romulus.scorec.rpi.edu>
2016-01-11 21:49:02 -0500

    Removed parasolid loading from load Simmetrix BC

--- proteus/MeshAdaptPUMI/MeshAdaptPUMI.h
+++ proteus/MeshAdaptPUMI/MeshAdaptPUMI.h
@@ -46,7 +46,7 @@ class MeshAdaptPUMIDrvr{
   void computeDiffusiveFlux(apf::Mesh*m,apf::Field* voff, apf::Field* visc,apf::Field* pref, apf::Field* velf);
   void getBoundaryFlux(apf::Mesh* m, apf::MeshEntity* ent, apf::Field* voff, apf::Field* visc,apf::Field* pref, apf::Field* velf, double * endflux);
   void getBoundaryFlux(apf::Mesh* m, apf::MeshEntity* ent, double * endflux);
-  int getSimmetrixBC(const char* geomFile, const char* modelFile);
+  int getSimmetrixBC(const char* modelFile);
   void removeBCData();
   char* geomFileName; 
   char* modelFileName; 
