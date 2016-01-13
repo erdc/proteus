21e87e5b3a19808fb82b97c3dec2e8339e3b69c5
zhang-alvin <zhanga@diplomacy.scorec.rpi.edu>
2016-01-13 17:34:55 -0500

    Added test case for floating bar with adapt

---
+++ proteus/MeshAdaptPUMI/test/floating_bar_scorec_error/simModel/prepMesh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash +x
+
+/lore/zhanga/core-sim/build/test/convert Floating_Bar.smd Floating_Bar.sms Floating_Bar.smb
+/lore/zhanga/core-sim/build/test/mdlConvert Floating_Bar.smd Floating_Bar.dmg
