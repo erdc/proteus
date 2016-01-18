f509e5641ac88a4b55fbc83bbfe7d41b7288aa2a
zhang-alvin <zhanga@remus.scorec.rpi.edu>
2016-01-17 19:01:52 -0500

    Revert "simmetrixBCreloaded"
    This reverts commit 7c6b34ff178e7d53d3d47a7af2a50e3800e560b6.
    
    Testing to see if this is the cause of woe.

--- proteus/MeshAdaptPUMI/MeshAdaptPUMI.pyx
+++ proteus/MeshAdaptPUMI/MeshAdaptPUMI.pyx
@@ -19,8 +19,7 @@ cdef extern from "MeshAdaptPUMI/MeshAdaptPUMI.h":
         MeshAdaptPUMIDrvr(double, double, int, char*, char*)
         int numIter, numAdaptSteps
         int loadModelAndMesh(char *, char*)
-        int getSimmetrixBC()
-        void simmetrixBCreloaded(char*)
+        int getSimmetrixBC(char*)
         int constructFromSerialPUMIMesh(Mesh&)
         int constructFromParallelPUMIMesh(Mesh&, Mesh&)
         int updateMaterialArrays(Mesh&, int, int)
@@ -48,8 +47,8 @@ cdef class MeshAdaptPUMI:
         del self.thisptr
     def loadModelAndMesh(self, geomName, meshName):
         return self.thisptr.loadModelAndMesh(geomName, meshName)
-    def simmetrixBCreloaded(self,modelFile):
-        return self.thisptr.simmetrixBCreloaded(modelFile)
+    def getSimmetrixBC(self,modelFile):
+        return self.thisptr.getSimmetrixBC(modelFile)
     def constructFromSerialPUMIMesh(self, cmesh):
         cdef CMesh* cmesh_ptr = <CMesh*>cmesh
         return self.thisptr.constructFromSerialPUMIMesh(cmesh_ptr.mesh)
