299c0d557aa7cda24d1addb444bff4fbd1008b60
cekees <cekees@gmail.com>
2016-02-20 19:33:56 -0600

    moved du scatter forward to standard Newton

--- proteus/NonlinearSolvers.py
+++ proteus/NonlinearSolvers.py
@@ -487,6 +487,8 @@ class Newton(NonlinearSolver):
                     self.betaK_current = self.norm_2_Jinv_current
                 self.linearSolver.prepare(b=r)
             self.du[:]=0.0
+            if self.par_du != None:
+                self.par_du.scatter_forward_insert()
             if not self.directSolver:
                 if self.EWtol:
                     self.setLinearSolverTolerance(r)
@@ -494,6 +496,8 @@ class Newton(NonlinearSolver):
                 self.linearSolver.solve(u=self.du,b=r,par_u=self.par_du,par_b=par_r)
                 self.linearSolverFailed = self.linearSolver.failed()
             #print self.du
+            if self.par_du != None:
+                self.par_du.scatter_forward_insert()
             u-=self.du
             if par_u != None:
                 par_u.scatter_forward_insert()
@@ -1235,8 +1239,6 @@ class NewtonNS(NonlinearSolver):
                     self.betaK_current = self.norm_2_Jinv_current
                 self.linearSolver.prepare(b=r)
             self.du[:]=0.0
-            #if par_du != None:
-            #    par_du.scatter_forward_insert()
             if not self.directSolver:
                 if self.EWtol:
                     self.setLinearSolverTolerance(r)
