1b89da392cd6366ca876f455eccbd8b7c44e4c2b
Tristan de Lataillade <delataillade.tristan@gmail.com>
2016-03-29 14:51:32 +0100

    added BC for flag 0; Gauges files stored in logDir

--- proteus/Gauges.py
+++ proteus/Gauges.py
@@ -1,3 +1,4 @@
+import os
 from collections import defaultdict, OrderedDict
 from itertools import product
 
@@ -10,6 +11,7 @@ from . import Comm
 from .AuxiliaryVariables import AV_base
 from .Profiling import logEvent as log
 from proteus.MeshTools import triangleVerticesToNormals, tetrahedronVerticesToNormals, getMeshIntersections
+from proteus import Profiling
 
 
 def PointGauges(gauges, activeTime=None, sampleRate=0, fileName='point_gauges.csv'):
@@ -254,6 +256,7 @@ class Gauges(AV_base):
             self.globalQuantitiesBuf = None
             self.globalQuantitiesCounts = None
         else:
+            self.fileName = os.path.join(Profiling.logDir, self.fileName)
             self.file = open(self.fileName, 'w')
 
             if self.isLineIntegralGauge:
