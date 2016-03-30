bce8ff721daa70748208bc94db056f2c0edd8f7d
cekees <cekees@gmail.com>
2016-03-29 21:05:20 -0500

    handle edge with NodeTuple's and use bitmask for ownership
    this fixes some problems with parallel q2, but needs further testing

--- proteus/Transport.py
+++ proteus/Transport.py
@@ -2454,12 +2454,6 @@ class OneLevelTransport(NonlinearEquation):
                     cfemIntegrals.updateNumericalDiffusion(self.q[('numDiff',ci,ci)],
                                                            self.q[('grad(u)Xgrad(w)*dV_numDiff',ci,ci)],
                                                            self.elementResidual[ci])
-        # for eN in range(self.mesh.nElements_global):
-        #     for i in range(self.nDOF_test_element[0]):
-        #         print "element residual "+`eN`+'\t'+`i`
-        #         print self.elementResidual[0][eN,i]
-        #         print self.elementResidual[1][eN,i]
-        #         print self.elementResidual[2][eN,i]
         if self.numericalFlux != None and not isinstance(self.numericalFlux, NumericalFlux.DoNothing):
             for ci in range(self.nc):
                 self.ebq_global[('totalFlux',ci)].fill(0.0)
@@ -4253,30 +4247,30 @@ class OneLevelTransport(NonlinearEquation):
                     hasOutflowBoundary = int(self.fluxBoundaryConditions[ci] == 'outFlow')
                     needsOutflowJacobian_int = int(needOutflowJacobian == True)
                     self.sparsityInfo.findNonzeros(self.mesh.nElements_global,
-                                              self.nDOF_test_element[ci],
-                                              self.nDOF_trial_element[cj],
-                                              self.l2g[ci]['nFreeDOF'],
-                                              self.l2g[ci]['freeGlobal'],
-                                              self.l2g[cj]['nFreeDOF'],
-                                              self.l2g[cj]['freeGlobal'],
-                                              self.offset[ci],
-                                              self.stride[ci],
-                                              self.offset[cj],
-                                              self.stride[cj],
-                                              hasNumericalFlux,
-                                              hasDiffusionInMixedForm,
-                                              needNumericalFluxJacobian_int,
-                                              self.mesh.nElementBoundaries_element,
-                                              self.mesh.elementNeighborsArray,
-                                              self.mesh.nInteriorElementBoundaries_global,
-                                              self.mesh.interiorElementBoundariesArray,
-                                              self.mesh.elementBoundaryElementsArray,
-                                              self.mesh.elementBoundaryLocalElementBoundariesArray,
-                                              self.fluxBoundaryConditions[ci] == 'outFlow',
-                                              self.mesh.nExteriorElementBoundaries_global,
-                                              self.mesh.exteriorElementBoundariesArray,
-                                              hasOutflowBoundary,
-                                              needsOutflowJacobian_int)
+                                                   self.nDOF_test_element[ci],
+                                                   self.nDOF_trial_element[cj],
+                                                   self.l2g[ci]['nFreeDOF'],
+                                                   self.l2g[ci]['freeGlobal'],
+                                                   self.l2g[cj]['nFreeDOF'],
+                                                   self.l2g[cj]['freeGlobal'],
+                                                   self.offset[ci],
+                                                   self.stride[ci],
+                                                   self.offset[cj],
+                                                   self.stride[cj],
+                                                   hasNumericalFlux,
+                                                   hasDiffusionInMixedForm,
+                                                   needNumericalFluxJacobian_int,
+                                                   self.mesh.nElementBoundaries_element,
+                                                   self.mesh.elementNeighborsArray,
+                                                   self.mesh.nInteriorElementBoundaries_global,
+                                                   self.mesh.interiorElementBoundariesArray,
+                                                   self.mesh.elementBoundaryElementsArray,
+                                                   self.mesh.elementBoundaryLocalElementBoundariesArray,
+                                                   self.fluxBoundaryConditions[ci] == 'outFlow',
+                                                   self.mesh.nExteriorElementBoundaries_global,
+                                                   self.mesh.exteriorElementBoundariesArray,
+                                                   hasOutflowBoundary,
+                                                   needsOutflowJacobian_int)
                 else:
                     for eN in range(self.mesh.nElements_global):
                         for ii in range(self.l2g[ci]['nFreeDOF'][eN]): #l2g is an array
