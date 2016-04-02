e4d28b1e4b50cfc3b945824579439213c508f545
Alistair Bentley <alistairbntl@gmail.com>
2016-04-02 17:50:55 -0500

    Modified quad test and output.

--- proteus/tests/mesh_tests/stokes_2d_n.py
+++ proteus/tests/mesh_tests/stokes_2d_n.py
@@ -5,7 +5,7 @@ from stokes_2d_p import *
 #######################################################
 
 # context variables
-nLevels = 1
+nLevels = 2
 numeric_scheme = "THQuads"
 useWeakBoundaryConditions = False
 solveIteratively = False
