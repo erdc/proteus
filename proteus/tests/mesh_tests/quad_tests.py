d03a7bdc6e9a84b330f0e2177e53ee492866d549
Alistair Bentley <alistairbntl@gmail.com>
2016-06-07 11:13:03 -0500

    Updates to testing framework. 	- This PR introduces a few new features to the testing suite. 	1) It adds mesh_tests to the travis tests and corrects a 	regression 	2) It introduces a testing class for handling set-up and 	tear downs of test problems. 	3) It introduces nosetests attributes which allow marking 	tests as fast or slow to allow different ranges of testing 	depending on the computing resources avaliable. 	4) Adds a make test option to the Makefile.

--- proteus/tests/mesh_tests/quad_tests.py
+++ proteus/tests/mesh_tests/quad_tests.py
@@ -7,17 +7,18 @@ Test module for 2D Quadrilateral Meshes
 from proteus.iproteus import *
 from proteus import Comm
 comm = Comm.get()
-Profiling.logLevel=7
-Profiling.verbose=True
+#Profiling.logLevel=7
+#Profiling.verbose=True
 import numpy.testing as npt
+import nose
+from nose.plugins.attrib import attr
 from nose.tools import ok_ as ok
 from nose.tools import eq_ as eq
 from nose.tools import set_trace
 
+@attr('fast','mesh')
 def test_mesh_build():
-    '''
-    Test mesh generation and refinment
-    '''
+    """  Test mesh generation and refinment """
     nnx = 4
     nny = 4
     x = (-1.0,-1.0)
@@ -38,36 +39,41 @@ def test_mesh_build():
     assert mlMesh.meshList[0].nElements_global == (nnx-1)*(nny-1), 'Mesh generator has built incorrect number of quads'
     assert mlMesh.meshList[1].nElements_global == 4*(nnx-1)*(nny-1), 'Mesh generator has built incorrect number of quads'
 
-def test_2D_stokes():
-    '''
-    Runs a 2D Poiseulle Stokes problem on Quads with Taylor Hood elements
-    ''' 
-    # import and run a small 2D poiseulle problem
-    import stokes_2d_p
-    import stokes_2d_n
-    pList = [stokes_2d_p]
-    nList = [stokes_2d_n]    
-    so = default_so
-    so.tnList = [0.,1.]
-    so.name = pList[0].name
-    so.sList=[default_s]
-    #opts.logLevel=7
-    opts.verbose=True
-    opts.profile=True
-    opts.gatherArchive=True
-    ns = NumericalSolution.NS_base(so,pList,nList,so.sList,opts)
-    ns.calculateSolution('test1')
-    # test some generated file output
-    import filecmp
-    xmf_file = filecmp.cmp('poiseulleFlow.xmf','poiseulle_xmf.output')
-    assert xmf_file == True, '******** xmf_file compare failed **********'
+@attr('fast','stokes','mesh')
+class Test2DStokesOnQuads():
+    """ Runs a simple 2D Poiseulle Stokes problem on Quads with TH elements """
 
+    def setUp(self):
+        """ Initialize the test problem """
+        import stokes_2d_p
+        import stokes_2d_n
+        pList = [stokes_2d_p]
+        nList = [stokes_2d_n]    
+        so = default_so
+        so.tnList = [0.,1.]
+        so.name = pList[0].name
+        so.sList=[default_s]
+        opts.verbose=True
+        opts.profile=True
+        opts.gatherArchive=True
+        self.ns = NumericalSolution.NS_base(so,pList,nList,so.sList,opts)
 
+    def tearDown(self):
+        if os.path.exists('proteus.log'):
+            os.remove('proteus.log')
+        else:
+            pass
+        os.remove('poiseulleFlow.xmf')
+        os.remove('poiseulleFlow.h5')
+
+    def test_01_FullRun(self):
+        import filecmp
+        self.ns.calculateSolution('test1')
+        xmf_file = filecmp.cmp('poiseulleFlow.xmf','proteus/tests/mesh_tests/poiseulle_xmf.output')
+        assert xmf_file == True, '******** xmf_file compare failed **********'
 
 if __name__ == '__main__':
-#    test_2D_stokes()
     from proteus import Comm
     comm = Comm.init()
-    import nose
     nose.main()
 
