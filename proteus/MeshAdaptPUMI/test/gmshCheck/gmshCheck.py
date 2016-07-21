597720d5d448c5398035cb99e2214e58efefbe30
zhang-alvin <zhanga@catan.scorec.rpi.edu>
2016-07-21 18:19:07 -0400

    Fixed gmshCheck test case

--- proteus/MeshAdaptPUMI/test/gmshCheck/gmshCheck.py
+++ proteus/MeshAdaptPUMI/test/gmshCheck/gmshCheck.py
@@ -67,6 +67,9 @@ del scalar
 scalar = numpy.zeros((mesh.nNodes_global,1),'d')+1.0
 domain.PUMIMesh.transferFieldToPUMI("vof", scalar)
 
+domain.PUMIMesh.get_local_error()
+if(domain.PUMIMesh.willAdapt()):
+  print "Success!"
 
 domain.PUMIMesh.adaptPUMIMesh()
 
