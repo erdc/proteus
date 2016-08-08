80b83c9494e66ba5cf8c727a46ef9b65d6c96b9b
zhang-alvin <zhanga@mastermind.scorec.rpi.edu>
2016-08-08 15:32:06 -0400

    ALPHA: added a set of experimental features pertaining to obtaining error density and guessing the total number of elements

--- proteus/MeshAdaptPUMI/SizeField.cpp
+++ proteus/MeshAdaptPUMI/SizeField.cpp
@@ -523,6 +523,8 @@ int MeshAdaptPUMIDrvr::getERMSizeField(double err_total)
   apf::MeshIterator* it;
   apf::MeshEntity* v;
   int numel = 0;
+  double numel_new = 0;
+  double numel_new2 = 0;
   int nsd = m->getDimension();
   it = m->begin(nsd);
   apf::Field* size_iso_reg = apf::createField(m, "iso_size",apf::SCALAR,apf::getConstant(nsd));
@@ -532,14 +534,24 @@ int MeshAdaptPUMIDrvr::getERMSizeField(double err_total)
   numel = m->count(nsd);
   PCU_Add_Ints(&numel, 1);
 
-  double err_dest = alpha*err_total/sqrt(numel);
-if(comm_rank==0) std::cout<<"refinement ratio "<<alpha<<" error destination "<<err_dest<<" numel "<<numel<<std::endl;
+  //double err_dest = alpha*err_total/sqrt(numel);
+  double scalingRatio = target_error/errRate_max; //target_error here means the desired target Ratio
+  if(comm_rank==0) std::cout<<"Scaling ratio? "<<scalingRatio<<std::endl;
+//if(comm_rank==0) std::cout<<"refinement ratio "<<alpha<<" error destination "<<err_dest<<" numel "<<numel<<std::endl;
   double err_curr = 0.0;
   apf::Vector3 err_vect;
   //compute the new size field
   apf::MeshElement* element;
   apf::MeshEntity* reg;
   it = m->begin(nsd); 
+
+  double volTotal=0;
+  double holdWeight=0;
+  while(reg=m->iterate(it)){      
+    volTotal += apf::measure(m,reg);
+  }
+  PCU_Add_Doubles(&volTotal, 1);
+  it = m->begin(nsd);
   while(reg=m->iterate(it)){
     double h_old;
     double h_new;
@@ -548,11 +560,36 @@ if(comm_rank==0) std::cout<<"refinement ratio "<<alpha<<" error destination "<<e
     h_old = pow(apf::measure(element)*6*sqrt(2),1.0/3.0); //edge of a regular tet
     apf::getVector(err_reg,reg,0,err_vect);
     err_curr = err_vect[0];
+    double err_dest = alpha*err_total/sqrt(numel);//scalingRatio*err_curr;
     if(alpha==1)
       h_new = h_old;
     else
       h_new = h_old*(err_dest/err_curr);
     apf::setScalar(size_iso_reg,reg,0,h_new);
+    
+    //Test feature estimate the number of elements
+    holdWeight += h_old*apf::measure(element)/volTotal;
+    if(h_new>hmax){
+      numel_new += pow(h_old/hmax,3.0);
+      numel_new2 += hmax*apf::measure(element)/volTotal;
+    }
+    else if (h_new<hmin){
+      numel_new += pow(h_old/hmin,3.0);
+      numel_new2 += hmin*apf::measure(element)/volTotal;
+    }
+    else{
+      numel_new += pow(h_old/h_new,3.0);
+      numel_new2 += h_new*apf::measure(element)/volTotal;
+    }
+    //std::cout<<"h_old "<<h_old<<" h_new "<<h_new<<std::endl;
+  }
+  PCU_Add_Doubles(&holdWeight, 1);
+  PCU_Add_Doubles(&numel_new, 1);
+  PCU_Add_Doubles(&numel_new2, 1);
+  if(comm_rank==0){
+    std::cout<<"Total number of new elements? "<<numel_new<<std::endl;
+    std::cout<<"Total number of new elements second try? "<<pow((holdWeight/numel_new2),3)*1.5*numel<<" "<<numel_new2<<" "<<holdWeight<<" "<<numel<<std::endl;
+    std::cout<<"Average number of new elements? "<<(pow((holdWeight/numel_new2),3)*1.5*numel+numel_new)/2.0<<std::endl;
   }
   apf::destroyMeshElement(element);
   m->end(it);
