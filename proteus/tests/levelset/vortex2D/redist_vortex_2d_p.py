e0ec98a4f0d86c4c47ce68d18c144e82001804db
cekees <cekees@gmail.com>
2016-08-11 15:41:31 -0500

    added 2d and 3d vortex level set tests

---
+++ proteus/tests/levelset/vortex2D/redist_vortex_2d_p.py
@@ -0,0 +1,39 @@
+from proteus import *
+from proteus.default_p import *
+from math import *
+from vortex2D import *
+from proteus.mprans import RDLS
+import ls_vortex_2d_p
+
+LevelModelType = RDLS.LevelModel
+
+coefficients = RDLS.Coefficients(applyRedistancing=applyRedistancing,
+                                 epsFact=epsFactRedistance,
+                                 nModelId=0,
+                                 rdModelId=1,
+                                 useMetrics=useMetrics)
+
+#now define the Dirichlet boundary conditions
+
+def getDBC(x,flag):
+    pass
+
+dirichletConditions = {0:getDBC}
+
+if LevelModelType == RDLS.LevelModel:
+    #weakDirichletConditions = {0:RDLS.setZeroLSweakDirichletBCs}
+    weakDirichletConditions = {0:RDLS.setZeroLSweakDirichletBCsSimple}
+else:
+    weakDirichletConditions = {0:coefficients.setZeroLSweakDirichletBCs}
+
+#weakDirichletConditions = {0:coefficients.setZeroLSweakDirichletBCs2}
+#weakDirichletConditions = None
+
+
+initialConditions  = ls_vortex_2d_p.initialConditions
+
+fluxBoundaryConditions = {0:'noFlow'}
+
+advectiveFluxBoundaryConditions =  {}
+
+diffusiveFluxBoundaryConditions = {0:{}}
