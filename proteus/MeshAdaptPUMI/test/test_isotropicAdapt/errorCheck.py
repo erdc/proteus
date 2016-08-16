a7254048a23d47729ef9d298eccafeac934e8ded
zhang-alvin <zhanga@mastermind.scorec.rpi.edu>
2016-08-16 12:48:27 -0400

    TESTS: Updated test names and added tests for isotropic adapt function option, parallel error estimator and adapt

--- proteus/MeshAdaptPUMI/test/test_isotropicAdapt/errorCheck.py
+++ proteus/MeshAdaptPUMI/test/test_isotropicAdapt/errorCheck.py
@@ -17,12 +17,13 @@ from petsc4py import PETSc
 import os
 print os.getcwd()
 
-testDir='./proteus/MeshAdaptPUMI/test/gmshCheck/'
-Model=testDir + 'Couette.null'
-Mesh=testDir + 'Couette.msh'
+#testDir='./proteus/MeshAdaptPUMI/test/isotropicCheck/'
+testDir='./'
+Model=testDir + 'Couette.smd'
+Mesh=testDir + 'Couette.smb'
 
 domain = Domain.PUMIDomain() #initialize the domain
-domain.PUMIMesh=MeshAdaptPUMI.MeshAdaptPUMI(hmax=0.01, hmin=0.008, numIter=1,sfConfig='alvin',maType='isotropic',logType="on")
+domain.PUMIMesh=MeshAdaptPUMI.MeshAdaptPUMI(hmax=0.01, hmin=0.008, numIter=1,sfConfig="isotropic")
 domain.PUMIMesh.loadModelAndMesh(Model, Mesh)
 domain.faceList=[[80],[76],[42],[24],[82],[78]]
 
@@ -44,7 +45,7 @@ domain.PUMIMesh.transferPropertiesToPUMI(rho,nu,g)
 
 #Couette Flow
 
-print "Couette Flow"
+print "Couette Flow with Isotropic Adapt"
 Lz = 0.05
 Uinf = 2e-3
 
@@ -67,40 +68,20 @@ del scalar
 scalar = numpy.zeros((mesh.nNodes_global,1),'d')+1.0
 domain.PUMIMesh.transferFieldToPUMI("vof", scalar)
 
-domain.PUMIMesh.get_local_error()
-if(domain.PUMIMesh.willAdapt()):
-  print "Success!"
-
 domain.PUMIMesh.adaptPUMIMesh()
 
-'''
-#Poiseuille Flow
-print "Poiseuille Flow"
-Lz = 0.05;
-Ly = 0.2;
-Uinf =  0.5/0.0010021928*(1/Ly)
+mesh = MeshTools.TetrahedralMesh()
+mesh.convertFromPUMI(domain.PUMIMesh,
+                 domain.faceList,
+                 parallel = comm.size() > 1,
+                 dim = domain.nd)
+mlMesh = MeshTools.MultilevelTetrahedralMesh(
+  0,0,0,skipInit=True,
+  nLayersOfOverlap=0,
+  parallelPartitioningType=MeshTools.MeshParallelPartitioningTypes.element)
 
-vector=numpy.zeros((mesh.nNodes_global,3),'d')
-dummy = numpy.zeros(mesh.nNodes_global); 
-vector[:,0] = dummy
-vector[:,1] = -Uinf*(mesh.nodeArray[:,2]*mesh.nodeArray[:,2]-Lz*mesh.nodeArray[:,2]) #v-velocity
-vector[:,2] = dummy
-domain.PUMIMesh.transferFieldToPUMI("velocity", vector)
-del vector
-del dummy
+mlMesh.generateFromExistingCoarseMesh(
+    mesh,1,
+    nLayersOfOverlap=0,
+    parallelPartitioningType=MeshTools.MeshParallelPartitioningTypes.element)
 
-scalar=numpy.zeros((mesh.nNodes_global,1),'d')
-scalar[:,0] = 1-mesh.nodeArray[:,1]/Ly
-domain.PUMIMesh.transferFieldToPUMI("p", scalar)
-del scalar
-
-scalar=numpy.zeros((mesh.nNodes_global,1),'d')
-scalar[:,0] = mesh.nodeArray[:,2]
-domain.PUMIMesh.transferFieldToPUMI("phi", scalar)
-del scalar
-
-scalar = numpy.zeros((mesh.nNodes_global,1),'d')+1.0
-domain.PUMIMesh.transferFieldToPUMI("vof", scalar)
-
-domain.PUMIMesh.adaptPUMIMesh()
-'''
