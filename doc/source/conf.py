68ec5d4c0331a84f7e56e4b77fcf1a145c2bb378
Chris Kees <cekees@gmail.com>
2017-01-04 07:19:10 -0600

    Update jupyter (#482)
    * updating jupyter version and stack dependencies
    
    this enables many new ipython widgets and jupyter notebooks features
    
    * force fetch on Makefile:default_stack repos
    
    * added cycler package
    
    * added backports-shutil dep
    
    * added more ipython deps to stack
    
    * update stack commit again
    
    * fixing shapely in stack
    
    * removed scorec from default stack
    
    * added python-lzma
    
    * fixing default_stack target
    
    * updated Dockerfile to work with current jupyterhub
    
    * updated hashdist and hashstack commits
    
    * close hdf5 files after each test runs
    
    * updated test output, fixed gauge MPI bug,  removed some warnings
    
    * tried to fix int conversion problem in WaveTools.py
    
    * added some system dependencies for travis builds
    
    * fixed  spacing in .travis.yaml--got to get a yml emacs mode
    
    * trying pip install of pyliblzma so hit can do .xz files
    
    * turning down hit verbosity
    
    * updated hashdist to turn off progress bar that fills up logs
    
    * updated Dockerfile to allow multiple modes of running the container
    
    supports terminal, ipython, local notebook, and jupyterhub
    
    * updated Dockerfile with more OS packages and hashdist_default
    
    * updated zmq in stack and how pyzmq is build in Dockerfile
    
    * cleaned up Dockerfile and tried to fix pip3 pyzmq build
    
    * more stack and hashdist fixes
    
    * added new test data
    
    * fixed nbextensions in jupyterhub config
    
    * Close .h5 file in import module.
    
    * Delete renamed output files.
    
    * updated to petsc 3.7.5
    
    * update version numbers and Dockerfile
    
    * added PROTEUS_RELEASE environment variable in .travis.yml
    
    * added better version extraction

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -238,7 +238,7 @@ html_short_title = u"Proteus"
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "tritonBorder.ico"
+html_favicon = "_static/tritonBorder.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -327,7 +327,7 @@ latex_documents = [
 #latex_use_parts = False
 
 # Additional stuff for the LaTeX preamble.
-latex_preamble = '\input{macros}'
+latex_elements['preamble'] = '\input{macros}'
 
 # If true, show page references after internal links.
 #latex_show_pagerefs = False
