9b2d3f5e619358b67170b295f15224d8dce76537
zhang-alvin <alv.zhang@gmail.com>
2017-01-20 15:58:49 -0500

    TST: modified meshLoad test to check specific mesh statistics

--- proteus/tests/MeshAdaptPUMI/meshLoad/test_meshLoad.py
+++ proteus/tests/MeshAdaptPUMI/meshLoad/test_meshLoad.py
@@ -1,32 +1,25 @@
 #!/usr/bin/env python
 
-#from pyadh import *
-#from proteus.iproteus import *
 from ctypes import *
 from proteus import MeshTools
 from proteus import cmeshTools
 from proteus.MeshAdaptPUMI import MeshAdaptPUMI
-from proteus import Archiver
-from tables import *
-
-from petsc4py import PETSc
-
+from nose.tools import eq_ as eq
+from nose.tools import ok_ as ok
 import os
-print os.getcwd()
 
-testDir='./proteus/MeshAdaptPUMI/test/meshLoad/'
-cubeMdl=testDir + 'cube.dmg'
-cube670p1=testDir + 'pumi670/cube.smb'
-MeshAdaptPUMI = MeshAdaptPUMI.MeshAdaptPUMI()
-MeshAdaptPUMI.loadModelAndMesh(cubeMdl, cube670p1)
+def test_meshLoadPUMI(verbose=0):
 
-mesh = MeshTools.TetrahedralMesh()
-mesh.cmesh = cmeshTools.CMesh()
-MeshAdaptPUMI.constructFromSerialPUMIMesh(mesh.cmesh)
-mesh.buildFromC(mesh.cmesh)
-
-print "Done reading in mesh"
-print "meshInfo says : \n", mesh.meshInfo()
-
-mesh.writeMeshEnsight("mesh","n")
-cmeshTools.writeTetgenFiles(mesh.cmesh,"tetgen",1)
+    testDir=os.path.dirname(os.path.abspath(__file__))
+    cubeMdl=testDir + '/cube.dmg'
+    cube670p1=testDir + '/cube.smb'
+    meshAdaptInstance = MeshAdaptPUMI.MeshAdaptPUMI()
+    meshAdaptInstance.loadModelAndMesh(cubeMdl, cube670p1)
+    mesh = MeshTools.TetrahedralMesh()
+    mesh.cmesh = cmeshTools.CMesh()
+    meshAdaptInstance.constructFromSerialPUMIMesh(mesh.cmesh)
+    mesh.buildFromC(mesh.cmesh)
+    eq(mesh.nElements_global,670)
+    eq(mesh.nNodes_global,190)
+    eq(mesh.nEdges_global,977)
+    eq(mesh.nElementBoundaries_global,1458)
