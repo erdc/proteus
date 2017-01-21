afd43c2de49cc203b159d2e53d843affd6c70d0d
Alistair Bentley <alistairbntl@gmail.com>
2017-01-20 21:43:45 -0500

    Fixed regression in PCD_shell operator test.       - added a few additional tests for sparse matrix to PETSc4py.       - adjusted class heirarchy for test class.

--- proteus/tests/linalgebra_tests/test_sparse_conversions.py
+++ proteus/tests/linalgebra_tests/test_sparse_conversions.py
@@ -5,7 +5,8 @@ Test module for some linear algebra tools functions.
 from proteus.iproteus import *
 from proteus import superluWrappers
 from proteus import LinearAlgebraTools
-from petsc4py import PETSc as p4pyPETSc
+from petsc4py import PETSc as p4py
+import proteus.test_utils.TestTools
 import numpy
 import os, sys
 
@@ -43,7 +44,7 @@ def test_sparse_2_dense():
     col_idx = [0  , 4 ,0 ,1 ,5 ,1 ,2 ,3 ,0 ,2 ,3 ,4 ,1 ,3 ,4 ,5  ,1 ,4 , 5 ]
     row_idx = [0, 2, 5, 8, 12, 16, 19]
     size_n  = len(row_idx)-1
-    petsc_mat = p4pyPETSc.Mat().createAIJ(size = (size_n,size_n), \
+    petsc_mat = p4py.Mat().createAIJ(size = (size_n,size_n), \
                                           csr  = (row_idx, col_idx, vals))
     dense_mat = LinearAlgebraTools.petsc4py_sparse_2_dense \
                 (petsc_mat)
@@ -75,8 +76,52 @@ def test_superlu_2_petsc():
                                                 'sparse_mat_1.txt'))
     assert numpy.allclose(dense_mat,comparison_mat)
 
+class TestPetscMat(proteus.test_utils.TestTools.BasicTest):
 
+    def test_01(self):
+        Qp = numpy.array([[1, 0, 3],
+                          [1.5, -2.1, 0],
+                          [0, 4., 3]])
+        vals_Qp = [1., 3., 1.5, -2.1, 4., 3.]
+        col_idx_Qp = [0, 2, 0, 1, 1, 2]
+        row_idx_Qp = [0, 2, 4, 6]
+        size_n = len(row_idx_Qp)-1
+        petsc_Qp = p4py.Mat().createAIJ(size=(size_n,size_n),
+                                        csr = (row_idx_Qp,
+                                               col_idx_Qp,
+                                               vals_Qp))
+        proteus_Qp = LinearAlgebraTools.petsc4py_sparse_2_dense(petsc_Qp)
+        assert (Qp==proteus_Qp).all()
+        
+    def test_02(self):
+        Fp = numpy.array([[3.2, 1.1, 0.],
+                           [6.3, 0. , 1.],
+                           [-5.1, 0., 0.]])
+        vals_Fp = [3.2, 1.1, 6.3, 1., -5.1]
+        col_idx_Fp = [0, 1, 0, 2, 0]
+        row_idx_Fp = [0, 2, 4, 5]
+        size_n = len(row_idx_Fp)-1
+        petsc_Fp = p4py.Mat().createAIJ(size=(size_n,size_n),
+                                        csr = (row_idx_Fp,
+                                               col_idx_Fp,
+                                               vals_Fp))
+        proteus_Fp = LinearAlgebraTools.petsc4py_sparse_2_dense(petsc_Fp)
+        assert (Fp==proteus_Fp).all()
+
+    def test_03(self):
+        Ap = numpy.array([[3., 1., 2.],
+                          [7., 4., 0.],
+                          [3., 8., 1.1]])
+        vals_Ap = [3., 1., 2., 7., 4., 3., 8., 1.1]
+        col_idx_Ap = [0, 1, 2, 0, 1, 0, 1, 2]
+        row_idx_Ap = [0, 3, 5, 8]
+        size_n = len(row_idx_Ap) - 1
+        petsc_Ap = p4py.Mat().createAIJ(size=(size_n,size_n),
+                                        csr = (row_idx_Ap,
+                                               col_idx_Ap,
+                                               vals_Ap))
+        proteus_Ap = LinearAlgebraTools.petsc4py_sparse_2_dense(petsc_Ap)
+        assert (Ap==proteus_Ap).all()
+    
 if __name__ == '__main__':
-    test_sparse_2_dense()
-    test_superlu_2_dense()
-    test_superlu_2_petsc()
+    pass
