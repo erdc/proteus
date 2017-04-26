282ca59d9e042460e7ef7eef55e4e571bfde2232
Manuel Quezada de Luna <manuel.quezada.dl@gmail.com>
2017-04-26 14:55:55 -0500

    minor change on dam break with flat bathymetry test

--- proteus/tests/my_SWEs_invariant_domain/dam_break_flat_bottom/sw_hump_2d_c0p1_n.py
+++ proteus/tests/my_SWEs_invariant_domain/dam_break_flat_bottom/sw_hump_2d_c0p1_n.py
@@ -2,7 +2,7 @@ from proteus import *
 from proteus.default_n import *
 from sw_hump_2d_p import *
 
-refinement=5
+refinement=4
 runCFL=0.75
 #use_EV_stabilization=True
 #use_first_order_flatB_GP_stabilization=True
