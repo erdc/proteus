added library preloads and fixed  cfl calculation problem.

proteus/mprans/RANS2P.py
@@ -1861,9 +1861,13 @@ class LevelModel(proteus.Transport.OneLevelTransport):
                         self.velocityPostProcessor.vpp_algorithms[ci].computeGeometricInfo()
                         self.velocityPostProcessor.vpp_algorithms[ci].updateConservationJacobian[cj] = True
         OneLevelTransport.calculateAuxiliaryQuantitiesAfterStep(self)
-        self.q[('cfl',0)][:] = np.sqrt(self.q[('velocity',0)][...,0]*self.q[('velocity',0)][...,0] +
-                                       self.q[('velocity',0)][...,1]*self.q[('velocity',0)][...,1] +
-                                       self.q[('velocity',0)][...,2]*self.q[('velocity',0)][...,2])/self.elementDiameter[:,np.newaxis]
+        if  self.coefficients.nd ==3:
+            self.q[('cfl',0)][:] = np.sqrt(self.q[('velocity',0)][...,0]*self.q[('velocity',0)][...,0] +
+                                           self.q[('velocity',0)][...,1]*self.q[('velocity',0)][...,1] +
+                                           self.q[('velocity',0)][...,2]*self.q[('velocity',0)][...,2])/self.elementDiameter[:,np.newaxis]
+        else:
+            self.q[('cfl',0)][:] = np.sqrt(self.q[('velocity',0)][...,0]*self.q[('velocity',0)][...,0] +
+                                           self.q[('velocity',0)][...,1]*self.q[('velocity',0)][...,1])/self.elementDiameter[:,np.newaxis]
     def updateAfterMeshMotion(self):
         pass
 