c0c9d5de167ba83fc41521a8356147a8392c7913
zhang-alvin <alv.zhang@gmail.com>
2017-05-16 14:21:58 -0500

    ENH: added faceList and regList data structures to domain base type for compatibility with PUMI

--- proteus/Domain.py
+++ proteus/Domain.py
@@ -52,6 +52,9 @@ class D_base:
         # use_gmsh hack
         self.use_gmsh = False
         self.MeshOptions = MeshTools.MeshOptions(self)
+        # for PUMI compatibility
+        self.faceList=[] #list of the boundary IDs that have corresponding BCs, might be the same as self.facets[]
+        self.regList=[] # list of regions in the domain, this might be the same as self.regions[]
 
     def writeAsymptote(self, fileprefix):
         """
