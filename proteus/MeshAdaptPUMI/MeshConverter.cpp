d40c84277317f6483b5fa807dca88e3323350536
alvin <alv.zhang@gmail.com>
2017-05-26 18:41:18 -0400

    MAINT: Clean up of a comment

--- proteus/MeshAdaptPUMI/MeshConverter.cpp
+++ proteus/MeshAdaptPUMI/MeshConverter.cpp
@@ -392,7 +392,6 @@ int MeshAdaptPUMIDrvr::updateMaterialArrays(Mesh& mesh,
     int proteus_material,
     int scorec_tag)
 {
-  //int dim = m->getDimension();
   apf::ModelEntity* geomEnt = m->findModelEntity(dim, scorec_tag);
   apf::MeshIterator* it = m->begin(dim);
   apf::MeshEntity* f;
