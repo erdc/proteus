ce0c31600035c065a66549d9bf90126964e3a471
Chris Kees <cekees@gmail.com>
2017-05-27 18:21:49 -0500

    check 3d gmsh to within 2% relative difference

--- proteus/tests/test_gmsh_generation.py
+++ proteus/tests/test_gmsh_generation.py
@@ -40,14 +40,23 @@ class TestGMSH(unittest.TestCase):
         gmsh_cmd = "gmsh {0:s} -v 10 -3 -o {1:s} -format msh".format(domain.geofile+".geo", domain.geofile+".msh")
         check_call(gmsh_cmd, shell=True)
         MeshTools.msh2simplex(domain.geofile, nd=3)
+        # cek disabling exact tests due to cross-platform non-reproducibility 
+        # with open('gmsh_mesh_test_3D.node', 'r') as nodefile:
+        #     npt.assert_equal(nodefile.readline(), '7674 3 0 1\n')
+        # with open('gmsh_mesh_test_3D.edge', 'r') as edgefile:
+        #     npt.assert_equal(edgefile.readline(), '9384 1\n')
+        # with open('gmsh_mesh_test_3D.face', 'r') as facefile:
+        #     npt.assert_equal(facefile.readline(), '6256 3 1\n')
+        # with open('gmsh_mesh_test_3D.ele', 'r') as elefile:
+        #     npt.assert_equal(elefile.readline(), '37473 4 1\n')
         with open('gmsh_mesh_test_3D.node', 'r') as nodefile:
-            npt.assert_equal(nodefile.readline(), '7674 3 0 1\n')
+            assert(abs(int(nodefile.readline().split()[0]) - 7674)/7674.0 < .02)
         with open('gmsh_mesh_test_3D.edge', 'r') as edgefile:
-            npt.assert_equal(edgefile.readline(), '9384 1\n')
+            assert(abs(int(edgefile.readline().split()[0]) - 9384)/9384.0 < .02)
         with open('gmsh_mesh_test_3D.face', 'r') as facefile:
-            npt.assert_equal(facefile.readline(), '6256 3 1\n')
+            assert(abs(int(facefile.readline().split()[0]) - 6256)/6256.0 < .02)
         with open('gmsh_mesh_test_3D.ele', 'r') as elefile:
-            npt.assert_equal(elefile.readline(), '37473 4 1\n')
+            assert(abs(int(elefile.readline().split()[0]) - 37473)/37473.0 < .02)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
