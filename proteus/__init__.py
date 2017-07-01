f2d84c12bef73551d2605f50af52bd8141cbeba7
Chris Kees <cekees@gmail.com>
2017-07-01 13:11:31 +0100

    use normalized form of dev version (dev0)

--- proteus/__init__.py
+++ proteus/__init__.py
@@ -9,7 +9,7 @@ except ImportError:
     import pkgutil
     __path__ = pkgutil.extend_path(__path__, __name__)
 
-__version__ = '1.3.3dev'
+__version__ = '1.3.3dev0'
 
 __all__ = ["Archiver",
            "Domain",
