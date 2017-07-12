1c9ae7c3574c35c439ead75b71938ad605b1fc4e
Alistair Bentley <alistairbntl@gmail.com>
2017-07-12 19:51:56 -0400

    Adds a Chebyshev semi-iteration solver.      - Add a new class to create and apply a Chebyshev semi-iteration      - Introduces several tests to verify resonable output      - Adds a new function for turning dense numpy arrays        into petsc4py matrices (really only useful for testing)

---
+++ proteus/tests/solver_tests/test_iterative_methods.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+"""
+
+Test module for solver shell operator.
+
+"""
+import proteus.test_utils.TestTools
+from proteus.iproteus import *
+from proteus import Comm
+from proteus import LinearAlgebraTools as LAT
+from proteus import LinearSolvers as LS
+
+import os
+import sys
+import inspect
+import petsc4py
+import numpy as np
+import pytest
+import pickle
+
+from petsc4py import PETSc as p4pyPETSc
+
+proteus.test_utils.TestTools.addSubFolders( inspect.currentframe() )
+
+class TestIterativeMethods(proteus.test_utils.TestTools.BasicTest):
+
+    def setup_method(self,method):
+        self.petsc_options = p4pyPETSc.Options()
+        self._scriptdir = os.path.dirname(__file__)
+        self.quad_mass_matrix = np.load(os.path.join(self._scriptdir,
+                                        'import_modules/quad_mass_matrix.npy'))
+
+    def teardown_method(self,method):
+        pass
+
+    def test_dense_numpy_2_petsc4py(self):
+        A_petsc = LAT.dense_numpy_2_petsc4py(self.quad_mass_matrix)
+        A_new = LAT.petsc4py_sparse_2_dense(A_petsc)
+        assert np.linalg.norm(self.quad_mass_matrix - A_new) == 0
+            
+    def test_chebyshev_iteration_1(self):
+        '''  Tests the pcd_shell operators produce correct output. '''
+        A = np.diag(1./np.diag(self.quad_mass_matrix)).dot(self.quad_mass_matrix)
+        n = self.quad_mass_matrix.shape[0]
+        alpha = 1./4
+        beta = 9./4
+        x0 = np.zeros(n)
+        b1 = np.ones(n)
+        for i in range(0,n,2):
+            b1[i] = 0.
+        A_petsc = LAT.dense_numpy_2_petsc4py(A)
+        x0_petsc = p4pyPETSc.Vec().createWithArray(x0)
+        b1_petsc = p4pyPETSc.Vec().createWithArray(b1)
+        solver = LS.ChebyshevSemiIteration(A_petsc,
+                                           b1_petsc,
+                                           x0_petsc,
+                                           10,
+                                           alpha,
+                                           beta)
+        solver.apply()
+        expected = np.load(os.path.join(self._scriptdir,'import_modules/sol_10.npy'))
+        actual = solver.x_k.getArray().reshape(n,1)
+        assert np.allclose(expected,actual)
+
+    def test_chebyshev_iteration_2(self):
+        '''  Tests the pcd_shell operators produce correct output. '''
+        A = np.diag(1./np.diag(self.quad_mass_matrix)).dot(self.quad_mass_matrix)
+        n = self.quad_mass_matrix.shape[0]
+        alpha = 1./4
+        beta = 9./4
+        x0 = np.zeros(n)
+        b1 = np.zeros(n)
+        for i in range(0,n):
+            b1[i] = i
+        A_petsc = LAT.dense_numpy_2_petsc4py(A)
+        x0_petsc = p4pyPETSc.Vec().createWithArray(x0)
+        b1_petsc = p4pyPETSc.Vec().createWithArray(b1)
+        solver = LS.ChebyshevSemiIteration(A_petsc,
+                                           b1_petsc,
+                                           x0_petsc,
+                                           20,
+                                           alpha,
+                                           beta,
+                                           save_iterations=True)
+        solver.apply()
+        with open(os.path.join(self._scriptdir,'import_modules/sol_20_lst'),'rb') as fp:
+            expected = pickle.load(fp)
+        x_true = np.linalg.solve(A,b1)
+        err_lst = []
+        for i,item in enumerate(expected):
+            assert np.allclose(item.reshape(n,1),solver.iteration_results[i],1e-12)
+        
+if __name__ == '__main__':
+    pass
