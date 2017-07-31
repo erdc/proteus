5c71ffd302a9ea82b35d2ea2dfa3519235b37a1d
dtoconn <devin.t.oconnor@gmail.com>
2017-07-31 14:48:02 +0000

    Debugging force calculations.

--- proteus/mprans/RANS3PF2D.h
+++ proteus/mprans/RANS3PF2D.h
@@ -1004,11 +1004,11 @@ namespace proteus
 
 	C += (D_s*C_surf + (1.0 - H_s)*C_vol);
 
-        //force_x = dV*D_s*(-p*phi_s_normal[0] + nu*(phi_s_normal[0]*grad_u[0] + phi_s_normal[1]*grad_u[1]) + C_surf*(u-u_s)*rho);
-	//force_y = dV*D_s*(-p*phi_s_normal[1] + nu*(phi_s_normal[0]*grad_v[0] + phi_s_normal[1]*grad_v[1]) + C_surf*(v-v_s)*rho);
+        force_x = dV*D_s*(-p*phi_s_normal[0] + nu*(phi_s_normal[0]*grad_u[0] + phi_s_normal[1]*grad_u[1]) + C_surf*(u-u_s)*rho);
+        force_y = dV*D_s*(-p*phi_s_normal[1] + nu*(phi_s_normal[0]*grad_v[0] + phi_s_normal[1]*grad_v[1]) + C_surf*(v-v_s)*rho);
 
-        force_x = dV*D_s*(-p*phi_s_normal[0]);
-        force_y = dV*D_s*(-p*phi_s_normal[1]);
+        //force_x = dV*D_s*(-p*phi_s_normal[0]);
+        //force_y = dV*D_s*(-p*phi_s_normal[1]);
 
         //force_x = dV*D_s;
         //force_y = dV*D_s;
