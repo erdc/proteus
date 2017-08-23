f7daa9f324d93f88eb59ccbc105eddddaa80cdae
alvin <alv.zhang@gmail.com>
2017-08-23 12:35:19 -0400

    TST: Ensure that no velocity post-processing is done if partitioned by elements with no overlaps

--- proteus/Transport.py
+++ proteus/Transport.py
@@ -6353,6 +6353,7 @@ class MultilevelTransport:
             #
             logEvent(memory("boundary conditions","MultilevelTransport"),level=4)
             logEvent("Initializing OneLevelTransport",level=2)
+            uDict[0].femSpace.mesh.nLayersOfOverlap = mesh.nLayersOfOverlap
             transport=self.OneLevelTransportType(uDict,
                                             phiDict,
                                             testSpaceDict,
