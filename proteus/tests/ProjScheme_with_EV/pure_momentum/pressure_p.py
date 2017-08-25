cebff0a557b4ac244d90cc682ebf3b11b4e7eb0a
manuel-quezada <manuel.quezada.dl@gmail.com>
2017-08-25 18:33:53 -0500

    Add convergence test on NS momentum equations

--- proteus/tests/ProjScheme_with_EV/pure_momentum/pressure_p.py
+++ proteus/tests/ProjScheme_with_EV/pure_momentum/pressure_p.py
@@ -9,26 +9,20 @@ name = "pressure"
 coefficients=Pres.Coefficients(modelIndex=PRESSURE_model,
                                fluidModelIndex=V_model,
                                pressureIncrementModelIndex=PINC_model,
-                               useRotationalForm=True)
+                               useRotationalForm=False)
 
 def getDBC_p(x,flag):
-    if flag == boundaryTags['top'] and openTop:
-        return lambda x,t: 0.0
+    return lambda x,t: 0.0
 
 def getFlux(x,flag):
-    if not(flag == boundaryTags['top'] and openTop):
-        return lambda x,t: 0.0
+    return lambda x,t: 0.0
 
 class getIBC_p:
-    def __init__(self,waterLevel):
-        self.waterLevel=waterLevel
+    def __init__(self):
+        pass
     def uOfXT(self,x,t):
-       if signedDistance(x) < 0:
-           return -(L[1] - self.waterLevel)*rho_1*g[1] - (self.waterLevel - x[1])*rho_0*g[1]
-       else:
-           return -(L[1] - x[1])*rho_1*g[1]
+        return 0.0
 
-initialConditions = {0:getIBC_p(waterLine_z)}
-
+initialConditions = {0:getIBC_p()}
 dirichletConditions = {0:getDBC_p } # pressure bc are explicitly set
 advectiveFluxBoundaryConditions = {0:getFlux}
