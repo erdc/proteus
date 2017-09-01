45c6f2a4f5292b23c499c3f7a30476360e34389a
dtoconn <devin.t.oconnor@gmail.com>
2017-09-01 19:20:36 +0000

    Fixed error in SizeField.cpp

--- proteus/MeshAdaptPUMI/SizeField.cpp
+++ proteus/MeshAdaptPUMI/SizeField.cpp
@@ -54,7 +54,7 @@ int MeshAdaptPUMIDrvr::calculateSizeField()
   assert(phif);
   ////////////////////////////////////////
   apf::Field *phisError = m->findField("phi_s");
-  assert(phis);
+  assert(phisError);
   /////////////////////////////////////////
   apf::Field *phiCorr = m->findField("phiCorr");
   assert(phiCorr);
