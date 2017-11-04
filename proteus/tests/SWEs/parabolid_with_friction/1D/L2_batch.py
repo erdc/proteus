0f4b1b76b902c65f0dff7884d5909513e2a0b16a
manuel-quezada <manuel.quezada.dl@gmail.com>
2017-11-04 02:57:10 +0100

    Mquezada/fix minor bugs in sw es (#640)
    * change the scale of the 1D paraboloid problem to match with literature
    
    * change scale of 2D paraboloid prob to match literature
    
    * add LINEAR_FRICTION as parameter. Note, the default value is 0
    
    * change default parameters for 1D Riemann problem
    
    * change defaults in dam break over bumps
    
    * change default values for 2D paraboloid problem
    
    * change default parameters for malpasset test
    
    * TMP. Modify xi=10 to improve cfl
    
    * use FE timeIntegration by default in malpasset test. I think I still have a bug here
    
    * change some more defaults on 1D paraboloid prob
    
    * fixed comparisons to None
    
    * fixed setup files (dependencies for VOF and MCorr) and data
    
    * updated for release 1.4.2
    
    * Comment on parameter xi

--- proteus/tests/SWEs/parabolid_with_friction/1D/L2_batch.py
+++ proteus/tests/SWEs/parabolid_with_friction/1D/L2_batch.py
@@ -4,7 +4,6 @@ from proteus import Comm
 comm = Comm.get()
 ctx = Context.get()
 
-
 # simulation flags for error analysis
 #
 # simFlagsList is initialized in proteus.iproteus
@@ -12,9 +11,10 @@ ctx = Context.get()
 
 simFlagsList[0]['errorQuantities']=['u']
 simFlagsList[0]['errorTypes']= ['numericalSolution'] #compute error in soln and glob. mass bal
-simFlagsList[0]['errorNorms']= ['L1','L2'] #compute L2 norm in space or H0 or ...
+simFlagsList[0]['errorNorms']= ['L1','L2','LI'] #compute L2 norm in space or H0 or ...
 simFlagsList[0]['errorTimes']= ['Last'] #'All', 'Last'
 simFlagsList[0]['echo']=True
+simFlagsList[0]['echoRelativeErrors']=True
 
 #
 start
