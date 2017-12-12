7394ac1066ca9a44af946d1d0a49a4f6a05407c8
Tristan de Lataillade <delataillade.tristan@gmail.com>
2017-12-12 23:02:08 +0000

    time dependent monitor function for moving mesh

--- proteus/mprans/MoveMeshMonitor.py
+++ proteus/mprans/MoveMeshMonitor.py
@@ -19,6 +19,7 @@ class Coefficients(TransportCoefficients.PoissonEquationCoefficients):
         self.nd = nd
         aOfX = [lambda x: np.array([[1., 0.], [0., 1.]])]
         fOfX = [self.uOfX]  # scaled function reciprocal
+        self.t = 0
         #super(MyCoeff, self).__init__(aOfX, fOfX)
         TransportCoefficients.PoissonEquationCoefficients.__init__(self, aOfX, fOfX)
             
@@ -33,16 +34,19 @@ class Coefficients(TransportCoefficients.PoissonEquationCoefficients):
         self.model.mesh_array0 = copy.deepcopy(self.mesh.nodeArray)
         self.PHI = np.zeros_like(self.model.mesh.nodeArray) 
         self.grads = np.zeros((len(self.model.u[0].dof),2))
-        self.areas_nodes = np.zeros(self.mesh.elementNodesArray.shape[0])
+        self.areas_nodes = np.zeros(len(self.mesh.nodeArray))
         self.areas = np.zeros(self.mesh.elementNodesArray.shape[0])
         self.nearest_nodes = np.array([i for i in range(len(self.mesh.nodeArray))])
+        self.eN_phi = np.array([None for i in range(len(self.mesh.nodeArray))])  # element containing displaced phi (guess)
         self.nearest_nodes0 = np.array([i for i in range(len(self.mesh.nodeArray))])
         self.mesh.nodeVelocityArray = np.zeros(self.mesh.nodeArray.shape,'d')
+        self.uOfXTatNodes = np.zeros(len(self.mesh.nodeArray))
 
     def preStep(self, t, firstStep=False):
         logEvent("Updating area function scaling coefficient",
                  level=3)
         integral_1_over_f = 0.
+        self.t = t
         xx = self.model.q['x']
         JJ = self.model.q['abs(det(J))']
         N_k = self.model.q['x'].shape[1]
@@ -52,7 +56,7 @@ class Coefficients(TransportCoefficients.PoissonEquationCoefficients):
             area = 0
             for k in xrange(N_k):
                 area += JJ[e, k]*q_weights[k]
-                integral_1_over_f += JJ[e, k]*q_weights[k]/self.myfunc(xx[e,k])
+                integral_1_over_f += JJ[e, k]*q_weights[k]/self.myfunc(xx[e,k], self.t)
             self.areas[e] = area
             nE += 1
         self.C = integral_1_over_f/(nE)  # update scaling coefficient
@@ -74,6 +78,16 @@ class Coefficients(TransportCoefficients.PoissonEquationCoefficients):
         self.gradientRecoveryAtNodes()
         # area recovery
         self.areaRecoveryAtNodes()
+        # evaluate f at nodes
+        self.evaluateFunAtNodes()
+        # gamma parameter
+        f_over_g = self.uOfXTatNodes/self.areas_nodes
+        f_over_g_max = max(f_over_g)
+        f_over_g_min = min(f_over_g)
+        self.gamma = f_over_g_max/f_over_g_min
+        self.gamma0 = 10.  # user defined parameter
+        self.na = np.log(self.gamma)/np.log(self.gamma0)
+        print("gamma", f_over_g_max, f_over_g_min, self.gamma)
         # pseudo-time step
         eps = 0.1
         self.pseudoTimeStepping(eps)
@@ -82,6 +96,8 @@ class Coefficients(TransportCoefficients.PoissonEquationCoefficients):
         self.model.mesh.nodeArray[:] = self.PHI
         # re-initialise nearest nodes
         self.nearest_nodes[:] = self.nearest_nodes0[:]
+        # re-initialise containing element
+        self.eN_phi[:] = None
 
     def uOfX(self, x, eN=None):
         """
@@ -93,10 +109,10 @@ class Coefficients(TransportCoefficients.PoissonEquationCoefficients):
             area = 1.
         else:
             area = self.areas[eN]
-        f = 1./(self.myfunc(x)*self.C)-1./area
+        f = 1./(self.myfunc(x, self.t)*self.C)-1./area
         return f
     
-    def dist_search(self, x, node):
+    def dist_search(self, x, node, eN=None):
         """
         search element containing containing coords x starting with a guessed
         nearest node
@@ -115,55 +131,74 @@ class Coefficients(TransportCoefficients.PoissonEquationCoefficients):
         nearest_node = node
         min_dist = np.sqrt(np.sum((x-self.mesh.nodeArray[node])**2))
         found_node = False
-        while found_node is False:
-            nearest_node0 = nearest_node
-            if node+1 < len(femSpace.mesh.nodeElementOffsets):
-                for eOffset in range(femSpace.mesh.nodeElementOffsets[node],
-                                     femSpace.mesh.nodeElementOffsets[node+1]):
-                    eN = self.mesh.nodeElementsArray[eOffset]
-                    checkedElements.append(eN)
-                    for node in self.mesh.elementNodesArray[eN]:
-                        node_coords = self.mesh.nodeArray[node]
-                        dist = np.sqrt(np.sum((node_coords-x)**2))
-                        if dist < min_dist:
-                            min_dist = dist
-                            nearest_node = node
-            else:
-                assert False, 'problem to find element'
-            if nearest_node0 == nearest_node:
-                found_node = True
-        # now find element containing x around nearest_node 
         element = None
-        coords = self.mesh.nodeArray[nearest_node]
-        for eOffset in range(self.mesh.nodeElementOffsets[nearest_node],
-                             self.mesh.nodeElementOffsets[nearest_node+1]):
-            eN = self.mesh.nodeElementsArray[eOffset]
-            checkedElements.append(eN)
-            # union of set
-            patchBoundaryNodes|=set(femSpace.mesh.elementNodesArray[eN])
-            # evaluate the inverse map for element eN (global to local)
-            xi = femSpace.elementMaps.getInverseValue(eN, coords)
-            #J = femSpace.elementMaps.getJacobianValues(eN, )
+        if eN is not None:
+            xi = femSpace.elementMaps.getInverseValue(eN, x)
             # query whether xi lies within the reference element
             if femSpace.elementMaps.referenceElement.onElement(xi):
                 element = eN
-                #print("found1", eN)
         if element is None:
-            # extra loop if case coords is in neighbour element
-            for node in patchBoundaryNodes:
-                for eOffset in range(self.mesh.nodeElementOffsets[node],
-                                     self.mesh.nodeElementOffsets[node+1]):
-                    eN = self.mesh.nodeElementsArray[eOffset]
-                    if eN not in checkedElements:
-                        checkedElements.append(eN)
-                        # evaluate the inverse map for element eN
-                        xi = femSpace.elementMaps.getInverseValue(eN, coords)
-                        # query whether xi lies within the reference element
-                        if femSpace.elementMaps.referenceElement.onElement(xi):
-                            element = eN
-                            #print("found2", eN)
-        return eN, nearest_node, xi
-
+            while found_node is False:
+                nearest_node0 = nearest_node
+                for nOffset in range(femSpace.mesh.nodeStarOffsets[nearest_node0],
+                                     femSpace.mesh.nodeStarOffsets[nearest_node0+1]):
+                    node = self.mesh.nodeStarArray[nOffset]
+                    node_coords = self.mesh.nodeArray[node]
+                    dist = np.sqrt(np.sum((node_coords-x)**2))
+                    if dist < min_dist:
+                        min_dist = dist
+                        nearest_node = node
+                if nearest_node0 == nearest_node:
+                    found_node = True
+            # now find element containing x around nearest_node
+            for eOffset in range(self.mesh.nodeElementOffsets[nearest_node],
+                                 self.mesh.nodeElementOffsets[nearest_node+1]):
+                eN = self.mesh.nodeElementsArray[eOffset]
+                checkedElements.append(eN)
+                # union of set
+                nodes = self.mesh.nodeStarArray[femSpace.mesh.nodeStarOffsets[nearest_node]:femSpace.mesh.nodeStarOffsets[nearest_node+1]]
+                patchBoundaryNodes|=set(nodes)
+                # evaluate the inverse map for element eN (global to local)
+                xi = femSpace.elementMaps.getInverseValue(eN, x)
+                # query whether xi lies within the reference element
+                if femSpace.elementMaps.referenceElement.onElement(xi):
+                    element = eN
+            if element is None:
+                # extra loop if case coords is in neighbour element
+                for node in patchBoundaryNodes:
+                    for eOffset in range(self.mesh.nodeElementOffsets[node],
+                                         self.mesh.nodeElementOffsets[node+1]):
+                        eN = self.mesh.nodeElementsArray[eOffset]
+            #            patchBoundaryNodes|=set(femSpace.mesh.elementNodesArray[eN])
+                        if eN not in checkedElements:
+                            checkedElements.append(eN)
+                            # evaluate the inverse map for element eN
+                            xi = femSpace.elementMaps.getInverseValue(eN, x)
+                            # query whether xi lies within the reference element
+                            if femSpace.elementMaps.referenceElement.onElement(xi):
+                                element = eN
+            #if element is None:
+            #    while element is None:
+            #    # extra loop if case coords is in neighbour element
+            #        print("patch", patchBoundaryNodes)
+            #        for node in copy.copy(patchBoundaryNodes):
+            #            for eOffset in range(self.mesh.nodeElementOffsets[node],
+            #                                 self.mesh.nodeElementOffsets[node+1]):
+            #                eN = self.mesh.nodeElementsArray[eOffset]
+            #                patchBoundaryNodes|=set(femSpace.mesh.elementNodesArray[eN])
+            #                if eN not in checkedElements:
+            #                    checkedElements.append(eN)
+            #                    # evaluate the inverse map for element eN
+            #                    xi = femSpace.elementMaps.getInverseValue(eN, coords)
+            #                    # query whether xi lies within the reference element
+            #                    if femSpace.elementMaps.referenceElement.onElement(xi):
+            #                        element = eN
+        return element, nearest_node, xi
+    
+    def evaluateFunAtNodes(self):
+        for i in range(len(self.mesh.nodeArray)):
+            self.uOfXTatNodes[i] = self.myfunc(self.mesh.nodeArray[i], self.t)
+            
     def getGradientValue(self, eN, xi):
         femSpace = self.model.u[0].femSpace
         value = 0.0
@@ -234,35 +269,23 @@ class Coefficients(TransportCoefficients.PoissonEquationCoefficients):
                 flag = self.mesh.nodeMaterialTypes[i]
                 if flag == 0:
                     if j > 0:
-                        eN, nearest_node, xi = self.dist_search(phi, self.nearest_nodes[i])
-                        v_grad = self.getGradientValue(eN, xi)
-                        area = self.areas[eN]
+                        eN, nearest_node, xi = self.dist_search(phi, self.nearest_nodes[i], self.eN_phi[i])
+                        self.eN_phi[i] = eN
                         self.nearest_nodes[i] = nearest_node
+                        if eN is None:
+                            print("Element not found for:", i)
+                            print("node", i, self.mesh.nodeArray[i])
+                            print("new coords", phi)
+                            v_grad = self.grads[i]
+                            area = self.areas_nodes[i]
+                        else:
+                            v_grad = self.getGradientValue(eN, xi)
+                            area = self.areas[eN]
                     else:
                         v_grad = self.grads[i]
                         area = self.areas_nodes[i]
-                    dphi = v_grad/(t*1./(self.myfunc(phi)*self.C)+(1-t)*1./area)
-                    # # RK4
-                    # # -----
-                    # phi1 = copy.copy(phi)
-                    # phi2 = copy.copy(phi1)
-                    # phi2[0] += dphi[0]*dt/2.
-                    # phi2[1] += dphi[1]*dt/2.
-                    # v_grad2, node2 = self.dist_search(phi2, i)
-                    # dphi2 = v_grad2/(t*(self.myfunc(phi2)*self.C)+(1-t)*1)
-                    # phi3 = copy.copy(phi2)
-                    # phi3[0] += dphi2[0]*dt/2.
-                    # phi3[1] += dphi2[1]*dt/2.
-                    # v_grad3, node3 = self.dist_search(phi3, i)
-                    # dphi3 = v_grad3/(t*(self.myfunc(phi3)*self.C)+(1-t)*1)
-                    # phi4 = copy.copy(phi3)
-                    # phi4[0] += dphi3[0]*dt/2.
-                    # phi4[1] += dphi3[1]*dt/2.
-                    # v_grad4, node4 = self.dist_search(phi4, i)
-                    # dphi4 = v_grad4/(t*(self.myfunc(phi4)*self.C)+(1-t)*1)
-                    # phii = phi1[:2]+(dt/6.)*(dphi+2*dphi2+2*dphi3+dphi4)
-                    # phi[0] = phii[0]
-                    # phi[1] = phii[1]
+                        eN = 0
+                    dphi = v_grad/(t*1./(self.myfunc(phi, self.t)*self.C)+(1-t)*1./area)
                     # # -----
                     # Euler
                     # # -----
@@ -288,3 +311,27 @@ def calculate_area(x, detJ, weights):
         for k in xrange(N_k):
             area += detJ[k]*weights[k]
         return area
+
+def recoveryAtNodes(variable, grad_v, nodeElementOffsets):
+    """
+    variable:
+         Variable in element
+    """
+    recovered_variable = np.zeros(len(nodeElementOffsets))
+    for node in range(len(nodeElementOffsets)):
+        nb_el = 0
+        grad_av = 0.
+        for eOffset in range(nodeElementOffsets[node],
+                             nodeElementOffsets[node+1]):
+            nb_el += 1
+            eN = self.mesh.nodeElementsArray[eOffset]
+            grad_eN_av = 0
+            # for k in range(n_quad):
+            #     grad_k = self.model.q[('grad(u)',0)][eN, k]
+            #     grad_eN_av += grad_k
+            # grad_eN_av /= n_quad
+            grad_eN_av = self.areas[eN]  # same value at all quad points
+            grad_av += grad_eN_av
+        grad_av /= nb_el
+        recovered_variable[node] = grad_av
+    return recovered_variable
