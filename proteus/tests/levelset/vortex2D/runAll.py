4d275c736016088d5b427d5d863a1be5c5dd99e9
Y^2 <wacyyang@gmail.com>
2017-12-20 22:24:02 +0000

    correct a logical error

--- proteus/tests/levelset/vortex2D/runAll.py
+++ proteus/tests/levelset/vortex2D/runAll.py
@@ -7,7 +7,7 @@ for i in range(3):
     for j in range(3):
         print(' '.join(['parun -v -l 5', 'ls_vortex_2d_so.py', '-C', " 'hk=%0.1f lRefinement=%d applyCorrection=True applyRedistancing=True'" %(hk[i],ref[j])]))
         os.system(' '.join(['parun -v -l 5', 'ls_vortex_2d_so.py', '-C', " 'hk=%0.1f lRefinement=%d applyCorrection=True applyRedistancing=True'" %(hk[i],ref[j])]))
-        print(' '.join(['parun -v -l 5', 'ls_vortex_2d_so.py', '-C', " 'hk=%0.1f lRefinement=%d applyCorrection=True applyRedistancing=False'" %(hk[i],ref[j])]))
-        os.system(' '.join(['parun -v -l 5', 'ls_vortex_2d_so.py', '-C', " 'hk=%0.1f lRefinement=%d applyCorrection=True applyRedistancing=False'" %(hk[i],ref[j])]))
+        print(' '.join(['parun -v -l 5', 'ls_vortex_2d_so.py', '-C', " 'hk=%0.1f lRefinement=%d applyCorrection=False applyRedistancing=True'" %(hk[i],ref[j])]))
+        os.system(' '.join(['parun -v -l 5', 'ls_vortex_2d_so.py', '-C', " 'hk=%0.1f lRefinement=%d applyCorrection=False applyRedistancing=True'" %(hk[i],ref[j])]))
         print(' '.join(['parun -v -l 5', 'ls_vortex_2d_so.py', '-C', " 'hk=%0.1f lRefinement=%d applyCorrection=False applyRedistancing=False'" %(hk[i],ref[j])]))
         os.system(' '.join(['parun -v -l 5', 'ls_vortex_2d_so.py', '-C', " 'hk=%0.1f lRefinement=%d applyCorrection=False applyRedistancing=False'" %(hk[i],ref[j])]))
