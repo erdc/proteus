3c599f2dbba2d069c0614297ff155d33633c2264
Y^2 <wacyyang@gmail.com>
2017-12-28 20:21:39 +0000

    compare dt*betaBDF+H_phi_old with 0 in getResidual

--- proteus/mprans/VOF.py
+++ proteus/mprans/VOF.py
@@ -769,12 +769,12 @@ class LevelModel(proteus.Transport.OneLevelTransport):
         self.q[('u', 0)] = numpy.zeros((self.mesh.nElements_global, self.nQuadraturePoints_element), 'd')
         self.q[('dV_u', 0)] = (1.0 / self.mesh.nElements_global) * numpy.ones((self.mesh.nElements_global, self.nQuadraturePoints_element), 'd')
         self.q[('grad(u)', 0)] = numpy.zeros((self.mesh.nElements_global, self.nQuadraturePoints_element, self.nSpace_global), 'd')
-        self.q[('m', 0)] = self.q[('u', 0)]
+        self.q[('m', 0)] = self.q[('u', 0)].copy()
         self.q[('m_last', 0)] = numpy.zeros((self.mesh.nElements_global, self.nQuadraturePoints_element), 'd')
         self.q[('mt', 0)] = numpy.zeros((self.mesh.nElements_global, self.nQuadraturePoints_element), 'd')
         self.q['dV'] = numpy.zeros((self.mesh.nElements_global, self.nQuadraturePoints_element), 'd')
         self.q['dV_last'] = -1000 * numpy.ones((self.mesh.nElements_global, self.nQuadraturePoints_element), 'd')
-        self.q[('m_tmp', 0)] = self.q[('u', 0)]
+        self.q[('m_tmp', 0)] = self.q[('u', 0)].copy()
         self.q[('cfl', 0)] = numpy.zeros((self.mesh.nElements_global, self.nQuadraturePoints_element), 'd')
         self.q[('numDiff', 0, 0)] = numpy.zeros((self.mesh.nElements_global, self.nQuadraturePoints_element), 'd')
         self.ebqe[('u', 0)] = numpy.zeros((self.mesh.nExteriorElementBoundaries_global, self.nElementBoundaryQuadraturePoints_elementBoundary), 'd')
