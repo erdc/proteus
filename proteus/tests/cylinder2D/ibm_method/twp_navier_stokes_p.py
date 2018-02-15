91a92b285bcd802c15c11e72b7dd4453cb52d038
Y^2 <wacyyang@gmail.com>
2018-02-15 19:12:05 +0000

    add REAME.md pytest files

--- proteus/tests/cylinder2D/ibm_method/twp_navier_stokes_p.py
+++ proteus/tests/cylinder2D/ibm_method/twp_navier_stokes_p.py
@@ -2,7 +2,7 @@ from proteus import *
 from proteus.default_p import *
 from cylinder import *
 from proteus.mprans import RANS3PF
-
+name="rans3p"
 LevelModelType = RANS3PF.LevelModel
 if useOnlyVF:
     LS_model = None
