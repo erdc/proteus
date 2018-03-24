ab89062dbf9d89dc4ad251e2f84b687e640d82ed
Tristan de Lataillade <delataillade.tristan@gmail.com>
2018-03-24 19:19:01 +0000

    MBD: added h5/xml for moorings visualisation

--- proteus/mbd/ChRigidBody.pyx
+++ proteus/mbd/ChRigidBody.pyx
@@ -33,11 +33,14 @@ from libcpp.memory cimport (shared_ptr,
                             make_shared)
 from collections import OrderedDict
 from cython.operator cimport dereference as deref
+import xml.etree.ElementTree as ET
+import h5py
 # chrono C++ headers
 cimport ChronoHeaders as ch
 # chrono Python headers
 from proteus.mbd cimport pyChronoCore as pych
 from proteus.mprans import BodyDynamics as bd
+from proteus.Archiver import indentXML
 
 
 cdef extern from "ChRigidBody.h":
@@ -1372,6 +1375,7 @@ cdef class ProtChSystem:
         object model_mesh
         object model_addedmass
         ProtChAddedMass ProtChAddedMass
+        int tCount
 
     def __cinit__(self, np.ndarray gravity, int nd=3, dt_init=0., sampleRate=0):
         self.thisptr = newSystem(<double*> gravity.data)
@@ -1406,6 +1410,7 @@ cdef class ProtChSystem:
         self.t = 0.
         self.chrono_dt = 1.
         self.ProtChAddedMass = ProtChAddedMass(self)
+        self.tCount = 0
 
     def GetChTime(self):
         """Gives time of Chrono system simulation
@@ -1524,6 +1529,7 @@ cdef class ProtChSystem:
             s.poststep()
         self.record_values = False
         self.first_step = False  # first step passed
+        self.tCount += 1
 
     def addBodyEasyBox(self, pych.ChBodyEasyBox body):
         """Hack to add BodyEasyBox
@@ -1959,6 +1965,7 @@ cdef class ProtChMoorings:
       int[:] nearest_node_array
       int[:] containing_element_array
       int[:] owning_rank
+      string hdfFileName
     def __cinit__(self,
                   ProtChSystem system,
                   Mesh mesh,
@@ -2015,6 +2022,136 @@ cdef class ProtChMoorings:
 
     def recordStrainEta(self, double[:] eta):
         self._record_etas = eta
+
+    def _recordH5(self):
+        tCount = self.ProtChSystem.tCount
+        self.hdfFileName = self.name
+        hdfFileName = os.path.join(Profiling.logDir, self.hdfFileName)+'.h5'
+        if tCount == 0:
+            f = h5py.File(hdfFileName, 'w')
+        else:
+            f = h5py.File(hdfFileName, 'a')
+        pos = self.getNodesPosition()
+        element_connection = np.array([[i, i+1] for i in range(len(pos)-1)])
+        dset = f.create_dataset('nodes_t'+str(tCount), pos.shape)
+        dset[...] = pos
+        dset = f.create_dataset('elements_t'+str(tCount), element_connection.shape, dtype='i8')
+        dset[...] = element_connection
+        # strain
+        datav = np.append(self.getNodesTension(eta=-1), [self.getNodesTension(eta=1)[-1]], axis=0)
+        print(datav, datav.shape, datav.shape[0])
+        dset = f.create_dataset('tensions_t'+str(tCount), datav.shape)
+        dset[...] = datav
+        dset = f.create_dataset('sx_t'+str(tCount), (datav.shape[0],))
+        dset[...] = datav[:,0]
+        dset = f.create_dataset('sy_t'+str(tCount), (datav.shape[0],))
+        dset[...] = datav[:,1]
+        dset = f.create_dataset('sz_t'+str(tCount), (datav.shape[0],))
+        dset[...] = datav[:,2]
+        # velocity
+        datav = self.getNodesVelocity()
+        dset = f.create_dataset('velocity_t'+str(tCount), datav.shape)
+        dset[...] = datav
+        dset = f.create_dataset('ux_t'+str(tCount), (datav.shape[0],))
+        dset[...] = datav[:,0]
+        dset = f.create_dataset('uy_t'+str(tCount), (datav.shape[0],))
+        dset[...] = datav[:,1]
+        dset = f.create_dataset('uz_t'+str(tCount), (datav.shape[0],))
+        dset[...] = datav[:,2]
+        # acceleration
+        datav = self.getNodesAcceleration()
+        dset = f.create_dataset('acceleration_t'+str(tCount), datav.shape)
+        dset[...] = datav
+        dset = f.create_dataset('ax_t'+str(tCount), (datav.shape[0],))
+        dset[...] = datav[:,0]
+        dset = f.create_dataset('ay_t'+str(tCount), (datav.shape[0],))
+        dset[...] = datav[:,1]
+        dset = f.create_dataset('az_t'+str(tCount), (datav.shape[0],))
+        dset[...] = datav[:,2]
+        # drag
+        datav = self.getDragForces()
+        dset = f.create_dataset('drag_t'+str(tCount),datav.shape)
+        dset[...] = datav
+        dset = f.create_dataset('dragx_t'+str(tCount), (datav.shape[0],))
+        dset[...] = datav[:,0]
+        dset = f.create_dataset('dragy_t'+str(tCount), (datav.shape[0],))
+        dset[...] = datav[:,1]
+        dset = f.create_dataset('dragz_t'+str(tCount), (datav.shape[0],))
+        dset[...] = datav[:,2]
+        # addedmass
+        datav = self.getAddedMassForces()
+        dset = f.create_dataset('am_t'+str(tCount), datav.shape)
+        dset[...] = datav
+        dset = f.create_dataset('amx_t'+str(tCount), (datav.shape[0],))
+        dset[...] = datav[:,0]
+        dset = f.create_dataset('amy_t'+str(tCount), (datav.shape[0],))
+        dset[...] = datav[:,1]
+        dset = f.create_dataset('amz_t'+str(tCount), (datav.shape[0],))
+        dset[...] = datav[:,2]
+
+    def _recordXML(self):
+        tCount = self.ProtChSystem.tCount
+        t = self.ProtChSystem.t
+        xmlFile = os.path.join(Profiling.logDir, self.name)+'.xmf'
+        if tCount == 0:
+            root = ET.Element("Xdmf",
+                            {"Version":"2.0",
+                             "xmlns:xi":"http://www.w3.org/2001/XInclude"})
+            domain = ET.SubElement(root, "Domain")
+            arGridCollection = ET.SubElement(domain,
+                                            "Grid",
+                                            {"Name":"Mesh"+" Spatial_Domain",
+                                             "GridType":"Collection",
+                                             "CollectionType":"Temporal"})
+        else:
+            tree = ET.parse(xmlFile)
+            root = tree.getroot()
+            domain = root[0]
+            arGridCollection = domain[0]
+        Xdmf_ElementTopology = "Polyline"
+        pos = self.getNodesPosition()
+        Xdmf_NumberOfElements= len(pos)-1
+        Xdmf_NodesPerElement = 2
+        dataItemFormat = "HDF"
+
+        arGrid = ET.SubElement(arGridCollection,"Grid",{"GridType":"Uniform"})
+        arTime = ET.SubElement(arGrid,"Time",{"Value":str(t),"Name":str(tCount)})
+        topology = ET.SubElement(arGrid,
+                                "Topology",
+                                {"Type":Xdmf_ElementTopology,
+                                "NumberOfElements":str(Xdmf_NumberOfElements)})
+
+        elements = ET.SubElement(topology,
+                                "DataItem",
+                                {"Format":dataItemFormat,
+                                "DataType":"Int",
+                                "Dimensions":"%i %i" % (Xdmf_NumberOfElements,Xdmf_NodesPerElement)})
+        elements.text = self.hdfFileName+".h5:/elements_t"+str(tCount)
+        geometry = ET.SubElement(arGrid,"Geometry",{"Type":"XYZ"})
+        nodes = ET.SubElement(geometry,"DataItem",
+                              {"Format":dataItemFormat,
+                               "DataType":"Float",
+                               "Precision":"8",
+                               "Dimensions":"%i %i" % (pos.shape[0], pos.shape[1]) })
+        nodes.text = self.hdfFileName+".h5:/nodes_t"+str(tCount)
+        attrs = ['ux', 'uy', 'uz', 'ax', 'ay', 'az', 'dragx', 'dragy', 'dragz', 'amx', 'amy', 'amz', 'sx', 'sy', 'sz']
+        for name in attrs:
+            attr = ET.SubElement(arGrid,"Attribute",{"Name":name,
+                                                     "AttributeType":"Scalar",
+                                                     "Center":"Node"})
+            data = ET.SubElement(attr,"DataItem",{"Name":name,
+                                                  "DataType":"Float",
+                                                  "Precision":"8",
+                                                  "Dimensions":"%i" % (pos.shape[0])})
+            data.text = self.hdfFileName+".h5:/"+name+"_t"+str(tCount)
+
+        tree = ET.ElementTree(root)
+
+        with open(xmlFile, "w") as f:
+            xmlHeader = "<?xml version=\"1.0\" ?>\n<!DOCTYPE Xdmf SYSTEM \"Xdmf.dtd\" []>\n"
+            f.write(xmlHeader)
+            indentXML(tree.getroot())
+            tree.write(f)
 
     def _recordValues(self):
         """Records values in csv files
@@ -2173,6 +2310,8 @@ cdef class ProtChMoorings:
         comm = Comm.get().comm.tompi4py()
         if comm.rank == self.ProtChSystem.chrono_processor and self.ProtChSystem.record_values is True:
             self._recordValues()
+            self._recordH5()
+            self._recordXML()
 
     def setNodesPositionFunction(self, function_position, function_tangent=None):
         """Function to build nodes
