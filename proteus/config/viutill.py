3fe153aa884cf597b6f62d81989fd56f15bb14d8
Chris Kees <cekees@gmail.com>
2018-03-25 22:18:21 -0500

    fixed utility server configuration

--- proteus/config/viutill.py
+++ proteus/config/viutill.py
@@ -1,9 +1,2 @@
 from default import *
 
-PROTEUS_PRELOAD_LIBS=[]
-PROTEUS_EXTRA_LINK_ARGS=['-L/app/acml/acml-4.4.0/gfortran64/lib','-lacml'] + platform_extra_link_args
-PROTEUS_EXTRA_FC_LINK_ARGS=['-L/app/acml/acml-4.4.0/gfortran64/lib','-lacml']
-PROTEUS_BLAS_LIB_DIR = '/app/acml/acml-4.4.0/gfortran64/lib'
-PROTEUS_BLAS_LIB   = 'acml'
-PROTEUS_LAPACK_LIB_DIR = '/app/acml/acml-4.4.0/gfortran64/lib'
-PROTEUS_LAPACK_LIB = 'acml'
