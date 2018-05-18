c71409c0810ba3d3bb6466330bf9a73592d8f43c
manuel-quezada <manuel.quezada.dl@gmail.com>
2018-05-18 18:21:37 -0500

    Initial implementation for DSW2DCV

--- proteus/tests/DSWEs/sw_hump_2d_p.py
+++ proteus/tests/DSWEs/sw_hump_2d_p.py
@@ -6,22 +6,21 @@ import numpy as np
 
 nd=2
 
-T=1000.00
-nDTout=10
-
-L=(8000.0,800.0)
+L=(10.0,1.0)
 g = 9.81
+
+h1=0.1
+h2=0.11
+x0 = 2
+D = np.sqrt(g * h2)
+
+T=5.00
+nDTout=100
+
 domain = RectangularDomain(L=L,x=[0,0,0])
+mannings=0
 
-h0=10
-a=3000
-B=2
-k=0.001
-p = np.sqrt(8*g*h0)/a
-s = np.sqrt(p**2 - k**2)/2.
-mannings=k
-
-cE=1
+cE=0
 LUMPED_MASS_MATRIX=1
 LINEAR_FRICTION=1
 
@@ -35,135 +34,85 @@ domain.writePoly("tank2d")
 ######################
 def bathymetry_function(X):
     x=X[0]
-    return h0*(x-L[0]/2)**2/a/a
-
-def eta_function(x,t):
-    coeff1 = a**2*B**2/8./g/g/h0
-    coeff2 = -B**2/4./g
-    coeff3 = -1./g
-    
-    eta_part1 = coeff1*np.exp(-k*t)*(-s*k*np.sin(2*s*t)+(k**2/4.-s**2)*np.cos(2*s*t))
-    eta_part2 = coeff2*np.exp(-k*t)
-    eta_part3 = coeff3*np.exp(-k*t/2.)*(B*s*np.cos(s*t)+k*B/2.*np.sin(s*t))*(x-L[0]/2)
-    
-    return h0 + eta_part1 + eta_part2 + eta_part3
+    return 0*x
 
 ##############################
 ##### INITIAL CONDITIONS #####
 ##############################
+def solitary(X,t):
+    xi = X[0] - D*t-x0
+    z1 = 3.0*(h2-h1)
+    z2 = h2 * h1**2
+    z = np.sqrt(z1 / z2)
+    soliton =  h1 + (h2 - h1) * 1.0/(np.cosh(xi/2.0 * z)**2)
+    return soliton
+
 class water_height_at_t0:
     def uOfXT(self,X,t):
-        eta = eta_function(X[0],0)
-        h = eta-bathymetry_function(X)
-        hp = max(h,0.)
-        return hp
+        return solitary(X,t)
+
+class mom_at_t0:
+    def uOfXT(self,X,t):
+        return D*(solitary(X,t) - h1)
+
+class eta_at_t0:
+    def uOfXT(self,X,t):
+        return solitary(X,t)**2.
 
 class Zero:
     def uOfXT(self,x,t):
         return 0.0
 
+analyticalSolution = {0:water_height_at_t0(),
+                      1:Zero(),
+                      2:Zero(),
+                      3:Zero(),
+                      4:Zero()}
+
 initialConditions = {0:water_height_at_t0(),
-                     1:Zero(),
+                     1:mom_at_t0(),
                      2:Zero(),
-                     3:Zero(),
+                     3:eta_at_t0(),
                      4:Zero()}
-
-##########################
-##### EXACT SOLUTION #####
-##########################
-class water_height:
-    def uOfXT(self,X,t):
-        h = eta_function(X[0],t) - bathymetry_function(X)
-        hp = max(h,0.)
-        return hp
-        
-class exact_velx:
-    def uOfXT(self,X,t):
-        return B*np.exp(-k*t/2.)*np.sin(s*t)
-
-class exact_momx:
-    def uOfXT(self,X,t):
-        ht = eta_function(X[0],t) - bathymetry_function(X)
-        if (ht >= 0):
-            h=ht
-        else:
-            h=0
-        u = B*np.exp(-k*t/2.)*np.sin(s*t)
-        return h*u
-
-analyticalSolution = {0:water_height(),
-                      1:exact_momx(),
-                      2:Zero()}
 
 ###################################
 ##### FOR BOUNDARY CONDITIONS #####
 ###################################
 def getDBC_h(x,flag):
-    return None
-#    if (x[0] in [0.0]) or flag in [bt['left']]:
-#        return lambda x,t: eta_function(x[0],t) - bathymetry_function(x)
-#    else:
-#        return None
+    if x[0]==0 or x[0]==L[0]:
+        return lambda x,t: h1
 
-def getDBC_u(x,flag):
-    return None
-#    if (x[0] in [0.0]) or flag in [bt['left']]:
-#        return lambda x,t: (eta_function(x[0],t) - bathymetry_function(x)) * (B*np.exp(-k*t/2.)*np.sin(s*t))
-#    else:
-#        return None
+def getDBC_hu(x,flag):
+    None
+    #if [0]==0 or x[0]==L[0]:
+    #    return lambda x,t: 0.
     
-def getDBC_v(x,flag):
-    return lambda x,t: 0.0
+def getDBC_hv(x,flag):
+    None
+    #return lambda x,t: 0.0
+
+def getDBC_heta(x,flag):
+    None
+    #if x[0]==0 or x[0]==L[0]:
+    #    return lambda x,t: h1**2.
+
+def getDBC_hw(x,flag):
+    None
+    #return lambda x,t: 0.0    
 
 dirichletConditions = {0:getDBC_h,
-                       1:getDBC_u,
-                       2:getDBC_v,
-                       3:getDBC_u,
-                       4:getDBC_v}
+                       1:getDBC_hu,
+                       2:getDBC_hv,
+                       3:getDBC_heta,
+                       4:getDBC_hw}
 
 fluxBoundaryConditions = {0:'outFlow',
                           1:'outFlow',
                           2:'outFlow',
                           3:'outFlow',
                           4:'outFlow'}
-
-def getAFBC_h(x,flag):
-    return lambda x,t: 0.0
-
-def getAFBC_u(x,flag):
-    if flag == 0:
-        return lambda x,t: 0.0
-    else:
-        return None
-def getAFBC_v(x,flag):
-    if flag == 0:
-        return lambda x,t: 0.0
-    else:
-        return None
-
-advectiveFluxBoundaryConditions =  {0:getAFBC_h,
-                                    1:getAFBC_u,
-                                    2:getAFBC_v,
-                                    3:getAFBC_u,
-                                    4:getAFBC_v}
-
-def getDFBC_u(x,flag):
-    if flag == 0:
-        return lambda x,t: 0.0
-    else:
-        return None
-
-def getDFBC_v(x,flag):
-    if flag == 0:
-        return lambda x,t: 0.0
-    else:
-        return None
-
-diffusiveFluxBoundaryConditions = {0:{},
-                                   1:{1:getDFBC_u},
-                                   2:{2:getDFBC_v},
-                                   3:{3:getDFBC_u},
-                                   4:{4:getDFBC_v}}
+advectiveFluxBoundaryConditions =  {}
+diffusiveFluxBoundaryConditions = {}
 
 #########################################
 ##### CREATE MODEL AND COEFFICIENTS #####
