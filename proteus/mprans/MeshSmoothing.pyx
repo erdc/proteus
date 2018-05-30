47a1ba2a4affdbfbc8d6756df073bf721f9be2e1
Tristan de Lataillade <delataillade.tristan@gmail.com>
2018-05-30 21:40:50 +0100

    MoveMeshMonitor: moving some functions to pyx; quality metrics added [WIP]

--- proteus/mprans/MeshSmoothing.pyx
+++ proteus/mprans/MeshSmoothing.pyx
@@ -1,5 +1,6 @@
 cimport numpy as np
 import numpy as np
+from libcpp cimport bool
 
 # cdef extern from "../CompKernel.h":
 #     cdef cppclass CompKernelSpaceMapping
@@ -18,12 +19,15 @@ import numpy as np
 # 				                       double& z);
 
 
-def smoothLaplace(double[:,:] nodeArray,
-                  int[:] nodeStarOffsets,
-                  int[:] nodeStarArray,
-                  int[:] nodeMaterialTypes,
-                  int nNodes_owned,
-                  int nSmooth=1):
+def smoothNodesLaplace(double[:,:] nodeArray,
+                       int[:] nodeStarOffsets,
+                       int[:] nodeStarArray,
+                       int[:] nodeMaterialTypes,
+                       int nNodes_owned,
+                       int nSmooth=1,
+                       double[:,:] boundaryNormals=None,
+                       int[:] fixedNodes=None,
+                       bool apply_directly=False):
     """
     Laplace Smoothing:
     Mesh nodes are displaced to the centroid of neighbouring nodes
@@ -41,20 +45,33 @@ def smoothLaplace(double[:,:] nodeArray,
     nSmooth: int
         number of times Laplace smoothing should be executed
     """
-    cySmoothLaplace(nodeArray,
-                    nodeStarOffsets,
-                    nodeStarArray,
-                    nodeMaterialTypes,
-                    nNodes_owned,
-                    nSmooth)
+    return cySmoothNodesLaplace(nodeArray,
+                                nodeStarOffsets,
+                                nodeStarArray,
+                                nodeMaterialTypes,
+                                nNodes_owned,
+                                nSmooth,
+                                boundaryNormals,
+                                fixedNodes,
+                                apply_directly)
 
 
-cdef cySmoothLaplace(double[:,:] nodeArray,
-                     int[:] nodeStarOffsets,
-                     int[:] nodeStarArray,
-                     int[:] nodeMaterialTypes,
-                     int nNodes_owned,
-                     int nSmooth):
+cdef double[:,:] cySmoothNodesLaplace(double[:,:] nodeArray,
+                                      int[:] nodeStarOffsets,
+                                      int[:] nodeStarArray,
+                                      int[:] nodeMaterialTypes,
+                                      int nNodes_owned,
+                                      int nSmooth,
+                                      double[:,:] boundaryNormals,
+                                      int[:] fixedNodes,
+                                      bool apply_directly=False):
+    cdef double[:,:] disp = np.zeros_like(nodeArray)
+    cdef double[:,:] nodeArrayMod
+    if not apply_directly:
+        nodeArrayMod = np.zeros_like(nodeArray)
+        nodeArrayMod[:] = nodeArray
+    else:
+        nodeArrayMod = nodeArray
     cdef double[:] sum_star = np.zeros(3)
     cdef int nNodeInStar
     for i in range(nSmooth):
@@ -63,13 +80,39 @@ cdef cySmoothLaplace(double[:,:] nodeArray,
             if nodeMaterialTypes[node] == 0:
                 for nOffset in range(nodeStarOffsets[node],
                                      nodeStarOffsets[node+1]):
-                    sum_star[0] += nodeArray[nodeStarArray[nOffset]][0]
-                    sum_star[1] += nodeArray[nodeStarArray[nOffset]][1]
-                    sum_star[2] += nodeArray[nodeStarArray[nOffset]][2]
+                    sum_star[0] += nodeArrayMod[nodeStarArray[nOffset], 0]
+                    sum_star[1] += nodeArrayMod[nodeStarArray[nOffset], 1]
+                    sum_star[2] += nodeArrayMod[nodeStarArray[nOffset], 2]
                 nNodeInStar = abs(nodeStarOffsets[node]-nodeStarOffsets[node+1])
-                nodeArray[node][0] = sum_star[0]/nNodeInStar
-                nodeArray[node][1] = sum_star[1]/nNodeInStar
-                nodeArray[node][2] = sum_star[2]/nNodeInStar
+                nodeArrayMod[node, 0] = sum_star[0]/nNodeInStar
+                nodeArrayMod[node, 1] = sum_star[1]/nNodeInStar
+                nodeArrayMod[node, 2] = sum_star[2]/nNodeInStar
+            else:
+                if boundaryNormals is not None:
+                    fixed = False
+                    if fixedNodes is not None:
+                        if fixedNodes[nodeMaterialTypes[node]] == 1:
+                            fixed = True
+                    if fixed is False:
+                        bN = boundaryNormals[nodeMaterialTypes[node]]
+                        if not bN[0] == 0 and bN[1] == 0 and bN[2] == 0:
+                            for nOffset in range(nodeStarOffsets[node],
+                                                 nodeStarOffsets[node+1]):
+                                sum_star[0] += nodeArrayMod[nodeStarArray[nOffset], 0]
+                                sum_star[1] += nodeArrayMod[nodeStarArray[nOffset], 1]
+                                sum_star[2] += nodeArrayMod[nodeStarArray[nOffset], 2]
+                            nNodeInStar = abs(nodeStarOffsets[node]-nodeStarOffsets[node+1])
+                            nodeArrayMod[node, 0] = sum_star[0]/nNodeInStar*(1-bN[0])
+                            nodeArrayMod[node, 1] = sum_star[1]/nNodeInStar*(1-bN[1])
+                            nodeArrayMod[node, 2] = sum_star[2]/nNodeInStar*(1-bN[2])
+            if i == nSmooth-1:
+                disp[node, 0] = nodeArrayMod[node, 0]-nodeArray[node, 0]
+                disp[node, 1] = nodeArrayMod[node, 1]-nodeArray[node, 1]
+                disp[node, 2] = nodeArrayMod[node, 2]-nodeArray[node, 2]
+    return disp
+            
+
+
 
 # cdef cyCalcuteMapping_element(gg)
 # ck.calculateMapping_element(eN,
@@ -82,3 +125,110 @@ cdef cySmoothLaplace(double[:,:] nodeArray,
 #                             jacDet,
 #                             jacInv,
 #                             x,y,z);
+
+cdef tuple cyGetQualityMetrics(double[:,:,:] J_array,
+                               double[:,:] detJ_array,
+                               double[:] target_area_array,
+                               int nd):
+    #W = np.array([[1., 0.5],
+    #              [0., 0.866]]) # [0., np.sqrt(3)/2.]
+    cdef double[:] J
+    cdef double[:] JT
+    cdef double detJ
+    cdef double trJTJ
+    cdef double[:,:] JTJ
+    cdef double dilation
+    cdef double distortion
+    cdef double[:] distortion_array
+    cdef double[:] dilation_array
+    for eN in range(len(target_area_array)):
+        detJ = detJ_array[eN, 0]  # index 0 as it is the same for all quadrature points
+        dilation = target_area_array[eN]/detJ  
+        if dilation < 1.:
+            dilation = 1/dilation
+        dilation_array[eN] = dilation-1
+        J = J_array[eN][0]
+        JT = J.T
+        JTJ = np.zeros_like(J)
+        JTJ = np.dot(J, JT)
+        trJTJ = 0.
+        for i in range(len(J)):
+            trJTJ += JTJ[i,i]
+        distortion = (1./nd*trJTJ)**(nd/2.)/detJ
+        distortion = 1-1./distortion
+        distortion_array[eN] = distortion
+        dilation_array[eN] = dilation
+    return distortion_array, dilation_array
+
+
+def getQualityMetrics(double[:,:,:] J_array,
+                      double[:,:] detJ_array,
+                      double[:] target_area_array,
+                      int nd):
+    return cyGetQualityMetrics(J_array=J_array,
+                             detJ_array=detJ_array,
+                             target_area_array=target_area_array,
+                             nd=nd)
+
+
+cdef double[:,:] cySmoothNodesQuality(double[:] distortion,
+                                      double[:] dilation,
+                                      double[:,:] nodeArray,
+                                      int nNodes_owned,
+                                      int[:] nodeMaterialTypes,
+                                      int[:] nodeElementOffsets,
+                                      int[:] nodeElementsArray,
+                                      int[:] elementNodesArray,
+                                      bool apply_directly=False):
+    cdef double[:,:] disp = np.zeros_like(nodeArray)
+    cdef double[:,:] nodeArrayMod
+    if not apply_directly:
+        nodeArrayMod = np.zeros_like(nodeArray)
+        nodeArrayMod[:] = nodeArray
+    else:
+        nodeArrayMod = nodeArray
+    cdef double[:] weighted_pos = np.zeros(3)
+    cdef double weight = 0
+    cdef double weights = 0
+    for node in range(nNodes_owned):
+        if nodeMaterialTypes[node] == 0:
+            weights = 0
+            weighted_pos[0] = 0
+            weighted_pos[1] = 0
+            weighted_pos[2] = 0
+            for eOffset in range(nodeElementOffsets[node],
+                                 nodeElementOffsets[node+1]):
+                eN = nodeElementsArray[eOffset]
+                for eNnode in elementNodesArray[eN]:
+                    if eNnode != node:
+                        weight = distortion[eN]
+                        weighted_pos[0] += nodeArrayMod[eNnode, 0]*weight
+                        weighted_pos[1] += nodeArrayMod[eNnode, 1]*weight
+                        weighted_pos[2] += nodeArrayMod[eNnode, 2]*weight
+                        weights += weight
+                nodeArrayMod[node, 0] = weighted_pos[0]/weights
+                nodeArrayMod[node, 1] = weighted_pos[1]/weights
+                nodeArrayMod[node, 2] = weighted_pos[2]/weights
+        disp[node, 0] = nodeArrayMod[node, 0]-nodeArray[node, 0]
+        disp[node, 1] = nodeArrayMod[node, 1]-nodeArray[node, 1]
+        disp[node, 2] = nodeArrayMod[node, 2]-nodeArray[node, 2]
+    return disp
+
+def smoothNodesQuality(double[:] distortion,
+                       double[:] dilation,
+                       double[:,:] nodeArray,
+                       int nNodes_owned,
+                       int[:] nodeMaterialTypes,
+                       int[:] nodeElementOffsets,
+                       int[:] nodeElementsArray,
+                       int[:] elementNodesArray,
+                       bool apply_directly=False):
+    return cySmoothNodesQuality(distortion=distortion,
+                                dilation=dilation,
+                                nodeArray=nodeArray,
+                                nNodes_owned=nNodes_owned,
+                                nodeMaterialTypes=nodeMaterialTypes,
+                                nodeElementOffsets=nodeElementOffsets,
+                                nodeElementsArray=nodeElementsArray,
+                                elementNodesArray=elementNodesArray,
+                                apply_directly=False)
