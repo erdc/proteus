74b1e6f3e24253baa973c44293b2cabbbeff4f78
YY <wacyyang@gmail.com>
2018-07-26 15:02:50 -0500

    initialize phi_s when use_ball_as_particle

--- proteus/mprans/RANS3PF.py
+++ proteus/mprans/RANS3PF.py
@@ -671,15 +671,18 @@ class Coefficients(proteus.TransportCoefficients.TC_base):
 
         logEvent("updating {0} particles...".format(self.nParticles))
         for i in range(self.nParticles):
-            if self.granular_sdf_Calc is not None:
-                sdf = lambda x: self.granular_sdf_Calc(x,i)
+            if self.use_ball_as_particle == 1:
+                sdf = lambda x: (np.linalg.norm(x-self.ball_center[i]),0)
             else:
-                sdf = lambda x: self.particle_sdfList[i](0.0, x)
+                if self.granular_sdf_Calc is not None:
+                    sdf = lambda x: self.granular_sdf_Calc(x,i)
+                else:
+                    sdf = lambda x: self.particle_sdfList[i](0.0, x)
 
             for j in range(mesh.nodeArray.shape[0]):
                 sdf_at_node, _ = sdf(mesh.nodeArray[j, :])
                 if (abs(sdf_at_node) < abs(self.phi_s[j])):
-                        self.phi_s[j] = sdf_at_node
+                            self.phi_s[j] = sdf_at_node
 
         # cek we eventually need to use the local element diameter
         self.eps_density = self.epsFact_density * mesh.h
