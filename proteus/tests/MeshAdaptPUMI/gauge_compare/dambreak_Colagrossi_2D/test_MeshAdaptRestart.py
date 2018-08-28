7a0723b82bf0a913f3f08fdc8145bfea84ab8288
zhang-alvin <alv.zhang@gmail.com>
2018-08-27 21:41:29 -0400

    TST: added gauge comparison test for post-adapt restart/solution transfer via 2D dambreak case

---
+++ proteus/tests/MeshAdaptPUMI/gauge_compare/dambreak_Colagrossi_2D/test_MeshAdaptRestart.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from nose.tools import eq_ as eq
+from nose.tools import ok_ as ok
+import subprocess
+import os
+import pytest
+
+@pytest.mark.slowTest
+
+def test_MeshAdaptRestart_adaptiveTime_BackwardEuler_baseline(verbose=0):
+    """Get baseline data"""
+    subprocess.call("parun -C \"gen_mesh=False usePUMI=True adapt=0 fixedTimeStep=False\" -D \"baseline\" dambreak_Colagrossi_so.py",shell=True)
+    assert(True)
+
+
+def test_MeshAdaptRestart_adaptiveTime_BackwardEuler(verbose=0):
+    """Test restart workflow"""
+    subprocess.call("parun -C \"gen_mesh=False usePUMI=True adapt=1 fixedTimeStep=False\" -D \"adapt_0\" dambreak_Colagrossi_so.py",shell=True)
+    assert(True)
+
+def test_MeshAdaptRestart(verbose=0):
+    #subprocess.call("diff normal adapt_0/pressure.csv baseline/pressure.csv")
+    with open('baseline/pressure.csv') as file1, open('adapt_0/pressure.csv') as file2:
+        for line1, line2 in zip(file1, file2):
+            if(line1 != line2):
+                pytest.fail("pressure gauge values are not the same!\n")
+            
+if __name__ == '__main__':
+    import nose
+    nose.main(defaultTest='test_MeshAdaptRestart:test_MeshAdaptRestart_adaptiveTime_BackwardEuler_baseline, test_MeshAdaptRestart:test_MeshAdaptRestart_adaptiveTime_BackwardEuler, test_MeshAdaptRestart:test_MeshAdaptRestart')
+
