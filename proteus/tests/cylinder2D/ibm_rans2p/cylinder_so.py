a7144dd29832e2084fa9d353ca3214bd8280b554
wacyyang <32206243+wacyyang@users.noreply.github.com>
2018-08-30 07:17:47 -0500

    Add IBM into RANS2P (#792)
    * add ibm to rans2p2d
    
    * add a new test on ibm+rans2p
    
    * add 3D ibm into rans2p
    
    * add a 3D test for ibm+rans2p
    
    * cleaned up errors in README files and updated comparison files

--- proteus/tests/cylinder2D/ibm_rans2p/cylinder_so.py
+++ proteus/tests/cylinder2D/ibm_rans2p/cylinder_so.py
@@ -6,22 +6,10 @@ from proteus.SplitOperator import Sequential_FixedStep_Simple, defaultSystem
 
 
 pnList = [("twp_navier_stokes_p", "twp_navier_stokes_n"),#0
-      ("pressureincrement_p", "pressureincrement_n"),#1
-      ("pressure_p", "pressure_n"),#2
-      ("pressureInitial_p", "pressureInitial_n")]#3
+          ]#3
 
-cylinder.VOF_model=None
-cylinder.VOS_model=None
-cylinder.SED_model=None
-cylinder.V_model=0
-cylinder.PINC_model=1
-cylinder.PRESSURE_model=2
-cylinder.PINIT_model=3
 
 name = "cylinder"
-
-#modelSpinUpList = [cylinder.VOF_model, cylinder.LS_model, cylinder.V_model, cylinder.PINIT_model]
-modelSpinUpList = [cylinder.PINIT_model]
 
 # class Sequential_MinAdaptiveModelStepPS(Sequential_MinAdaptiveModelStep):
 #     def __init__(self,modelList,system=defaultSystem,stepExact=True):
@@ -32,7 +20,7 @@ modelSpinUpList = [cylinder.PINIT_model]
 class Sequential_MinAdaptiveModelStepPS(Sequential_FixedStep):
     def __init__(self,modelList,system=defaultSystem,stepExact=True):
         Sequential_FixedStep.__init__(self,modelList,system,stepExact)
-        self.modelList = modelList[:len(pnList)-1]
+        self.modelList = modelList[:len(pnList)]
 
 dt_system_fixed = cylinder.dt_fixed
 systemStepControllerType = Sequential_MinAdaptiveModelStepPS
@@ -43,10 +31,6 @@ systemStepControllerType = Sequential_MinAdaptiveModelStepPS
 # systemStepControllerType = Sequential_FixedStep #Sequential_FixedStep_Simple # uses time steps in so.tnList
 # dt_system_fixed = 0.01; 
 # systemStepExact=False;
-
-
-needEBQ_GLOBAL = False
-needEBQ = False
 
 tnList = cylinder.tnList
 
