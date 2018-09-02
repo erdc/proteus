640f8cff9c8bca0098fb01c82f23987f55e0fa70
Chris Kees <cekees@gmail.com>
2018-09-02 14:04:23 -0500

    AnalyticalSolutions: fixed tests and arbitrary point list support

--- proteus/canalyticalSolutions.pyx
+++ proteus/canalyticalSolutions.pyx
@@ -46,88 +46,88 @@ cdef extern from "analyticalSolutions.h":
     extern int cSTflowSphere_Vz "STflowSphere_Vz"(int *iwork, double *rwork, int nPoints, double t, double *x, double *u)
 
 def PlaneCouetteFlow_u(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cPlaneCouetteFlow_u(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cPlaneCouetteFlow_u(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def diffusionSin1D(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cdiffusionSin1D(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cdiffusionSin1D(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def diffusionSin2D(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cdiffusionSin2D(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cdiffusionSin2D(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def diffusionSin3D(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cdiffusionSin3D(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cdiffusionSin3D(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def diffusionSin1D_r(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u, np.ndarray r) -> int:
-    return cdiffusionSin1D_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data, <double* > r.data)
+    return cdiffusionSin1D_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data, <double* > r.data)
 def diffusionSin2D_r(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u, np.ndarray r) -> int:
-    return cdiffusionSin2D_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data, <double* > r.data)
+    return cdiffusionSin2D_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data, <double* > r.data)
 def diffusionSin3D_r(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u, np.ndarray r) -> int:
-    return cdiffusionSin3D_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data, <double* > r.data)
+    return cdiffusionSin3D_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data, <double* > r.data)
 def LinearAD_DiracIC(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cLinearAD_DiracIC(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cLinearAD_DiracIC(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def LinearAD_DiracIC_advectiveVelocity(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray f) -> int:
-    return cLinearAD_DiracIC_advectiveVelocity(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > f.data)
+    return cLinearAD_DiracIC_advectiveVelocity(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > f.data)
 def LinearAD_DiracIC_diffusiveVelocity(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray f) -> int:
-    return cLinearAD_DiracIC_diffusiveVelocity(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > f.data)
+    return cLinearAD_DiracIC_diffusiveVelocity(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > f.data)
 def LinearAD_DiracIC_du(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray du) -> int:
-    return cLinearAD_DiracIC_du(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > du.data)
+    return cLinearAD_DiracIC_du(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > du.data)
 def LinearAD_DiracIC_totalVelocity(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cLinearAD_DiracIC_totalVelocity(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cLinearAD_DiracIC_totalVelocity(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def LinearAD_SteadyState(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cLinearAD_SteadyState(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cLinearAD_SteadyState(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def LinearADR_Decay_DiracIC(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cLinearADR_Decay_DiracIC(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cLinearADR_Decay_DiracIC(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def LinearADR_Decay_DiracIC_dr(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u, np.ndarray dr) -> int:
-    return cLinearADR_Decay_DiracIC_dr(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data, <double* > dr.data)
+    return cLinearADR_Decay_DiracIC_dr(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data, <double* > dr.data)
 def LinearADR_Decay_DiracIC_r(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u, np.ndarray r) -> int:
-    return cLinearADR_Decay_DiracIC_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data, <double* > r.data)
+    return cLinearADR_Decay_DiracIC_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data, <double* > r.data)
 def LinearADR_Sine(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cLinearADR_Sine(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cLinearADR_Sine(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def LinearADR_Sine_advectiveVelocity(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray f) -> int:
-    return cLinearADR_Sine_advectiveVelocity(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > f.data)
+    return cLinearADR_Sine_advectiveVelocity(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > f.data)
 def LinearADR_Sine_diffusiveVelocity(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray f) -> int:
-    return cLinearADR_Sine_diffusiveVelocity(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > f.data)
+    return cLinearADR_Sine_diffusiveVelocity(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > f.data)
 def LinearADR_Sine_dr(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u, np.ndarray dr) -> int:
-    return cLinearADR_Sine_dr(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data, <double* > dr.data)
+    return cLinearADR_Sine_dr(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data, <double* > dr.data)
 def LinearADR_Sine_du(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray du) -> int:
-    return cLinearADR_Sine_du(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > du.data)
+    return cLinearADR_Sine_du(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > du.data)
 def LinearADR_Sine_r(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u, np.ndarray r) -> int:
-    return cLinearADR_Sine_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data, <double* > r.data)
+    return cLinearADR_Sine_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data, <double* > r.data)
 def LinearADR_Sine_totalVelocity(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cLinearADR_Sine_totalVelocity(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cLinearADR_Sine_totalVelocity(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def NonlinearAD_SteadyState(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cNonlinearAD_SteadyState(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cNonlinearAD_SteadyState(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def NonlinearADR_Decay_DiracIC(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cNonlinearADR_Decay_DiracIC(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cNonlinearADR_Decay_DiracIC(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def NonlinearADR_Decay_DiracIC_dr(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u, np.ndarray dr) -> int:
-    return cNonlinearADR_Decay_DiracIC_dr(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data, <double* > dr.data)
+    return cNonlinearADR_Decay_DiracIC_dr(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data, <double* > dr.data)
 def NonlinearADR_Decay_DiracIC_r(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u, np.ndarray r) -> int:
-    return cNonlinearADR_Decay_DiracIC_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data, <double* > r.data)
+    return cNonlinearADR_Decay_DiracIC_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data, <double* > r.data)
 def NonlinearDAE(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cNonlinearDAE(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cNonlinearDAE(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def NonlinearDAE_f(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cNonlinearDAE_f(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cNonlinearDAE_f(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def PlanePoiseuilleFlow_u(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cPlanePoiseuilleFlow_u(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cPlanePoiseuilleFlow_u(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def PoiseuillePipeFlow(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cPoiseuillePipeFlow(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cPoiseuillePipeFlow(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def PoiseuillePipeFlow_P(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cPoiseuillePipeFlow_P(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cPoiseuillePipeFlow_P(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def poissonsEquationExp1D(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cpoissonsEquationExp1D(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cpoissonsEquationExp1D(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def poissonsEquationExp2D(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cpoissonsEquationExp2D(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cpoissonsEquationExp2D(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def poissonsEquationExp3D(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cpoissonsEquationExp3D(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cpoissonsEquationExp3D(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def poissonsEquationExp3D_dr(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u, np.ndarray dr) -> int:
-    return cpoissonsEquationExp3D_dr(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data, <double* > dr.data)
+    return cpoissonsEquationExp3D_dr(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data, <double* > dr.data)
 def poissonsEquationExp1D_r(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u, np.ndarray r) -> int:
-    return cpoissonsEquationExp1D_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data, <double* > r.data)
+    return cpoissonsEquationExp1D_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data, <double* > r.data)
 def poissonsEquationExp2D_r(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u, np.ndarray r) -> int:
-    return cpoissonsEquationExp2D_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data, <double* > r.data)
+    return cpoissonsEquationExp2D_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data, <double* > r.data)
 def poissonsEquationExp3D_r(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u, np.ndarray r) -> int:
-    return cpoissonsEquationExp3D_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data, <double* > r.data)
+    return cpoissonsEquationExp3D_r(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data, <double* > r.data)
 def STflowSphere_P(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cSTflowSphere_P(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cSTflowSphere_P(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def STflowSphere_Vx(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cSTflowSphere_Vx(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cSTflowSphere_Vx(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def STflowSphere_Vy(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cSTflowSphere_Vy(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cSTflowSphere_Vy(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
 def STflowSphere_Vz(np.ndarray iwork, np.ndarray rwork, double t, np.ndarray x, np.ndarray u) -> int:
-    return cSTflowSphere_Vz(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[3], t, < double* > x.data, < double* > u.data)
+    return cSTflowSphere_Vz(< int* > iwork.data, < double* > rwork.data, x.size/x.shape[len(x.shape)-1], t, < double* > x.data, < double* > u.data)
