67fc83e56e87dd8433581fd5b808a0658dd45197
manuel-quezada <manuel.quezada.dl@gmail.com>
2018-09-22 11:34:50 -0500

    Add automated test for dam over 3 bumps

--- proteus/tests/SWFlows/test_SWFlows.py
+++ proteus/tests/SWFlows/test_SWFlows.py
@@ -11,11 +11,11 @@ class TestSWFlows(object):
 
     def setup_method(self,method):
         self._scriptdir = os.path.dirname(__file__)
-        
+
     def test_solitary(self):
         # call runSWEs
         os.system("parun --SWEs solitary.py")
-        
+
         # COMPARE VS SAVED FILES #
         expected_path = 'comparison_files/solitary.h5'
         expected = tables.open_file(os.path.join(self._scriptdir,expected_path))
@@ -23,7 +23,7 @@ class TestSWFlows(object):
         assert np.allclose(expected.root.h_t3,actual.root.h_t3,atol=1e-10)
         expected.close()
         actual.close()
-        
+
     def test_parab1D(self):
         # Call runSWEs
         os.system("parun --SWEs parab1D.py")
@@ -33,5 +33,17 @@ class TestSWFlows(object):
         expected = tables.open_file(os.path.join(self._scriptdir,expected_path))
         actual = tables.open_file('parab1D.h5','r')
         assert np.allclose(expected.root.h_t11,actual.root.h_t11,atol=1e-10)
+        expected.close()
+        actual.close()
+
+    def test_dam3Bumps(self):
+        # Call runSWEs
+        os.system("parun --SWEs dam3Bumps.py")
+
+        # COMPARE VS SAVED FILES #
+        expected_path = 'comparison_files/dam3Bumps.h5'
+        expected = tables.open_file(os.path.join(self._scriptdir,expected_path))
+        actual = tables.open_file('dam3Bumps.h5','r')
+        assert np.allclose(expected.root.h_t4,actual.root.h_t4,atol=1e-10)
         expected.close()
         actual.close()
