972144700d7975608c79fb2701f610c8a082d0b2
manuel-quezada <manuel.quezada.dl@gmail.com>
2018-09-28 19:04:41 -0500

    Remove stuff about SUPG and shock capturing from shallow water model

--- proteus/SWFlows/models/sw_n.py
+++ proteus/SWFlows/models/sw_n.py
@@ -60,10 +60,7 @@ else:
 # ************************************ #
 # ********** NUMERICAL FLUX ********** #
 # ************************************ #
-try_supg_stabilization = True
-subgridError = SW2D.SubgridError(coefficients,nd,lag=True)
-shockCapturing = SW2D.ShockCapturing(coefficients,nd,shockCapturingFactor=0.1,lag=True)
-numericalFluxType = SW2D.NumericalFlux
+numericalFluxType = SW2DCV.NumericalFlux
 
 # ************************************ #
 # ********** LINEAR ALGEBRA ********** #
