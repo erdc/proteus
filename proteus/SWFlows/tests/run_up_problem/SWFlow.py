cc7ce842c2b438e8b809a140d54384716cd0bdca
manuel-quezada <manuel.quezada.dl@gmail.com>
2018-10-01 17:46:33 -0500

    Change some parameters in run up soliton

--- proteus/SWFlows/tests/run_up_problem/SWFlow.py
+++ proteus/SWFlows/tests/run_up_problem/SWFlow.py
@@ -17,7 +17,7 @@ from proteus import WaveTools as wt
 # *************************** #
 opts= Context.Options([
     ('sw_model',0,"sw_model = {0,1} for {SWEs,DSWEs}"),
-    ("final_time",9.0,"Final time for simulation"),
+    ("final_time",25.0,"Final time for simulation"),
     ("dt_output",0.1,"Time interval to output solution"),
     ("cfl",0.33,"Desired CFL restriction"),
     ("refinement",4,"Refinement level")
@@ -26,9 +26,10 @@ opts= Context.Options([
 ###################
 # DOMAIN AND MESH #
 ###################
-L=(45.0,4.5) 
+L=(100.0,10.) #45,4.5
+L0=[-90,0,0]  #-35,0,0
 refinement = opts.refinement
-domain = RectangularDomain(L=L,x=[-35,0,0])
+domain = RectangularDomain(L=L,x=L0)
 
 # CREATE REFINEMENT #
 nnx0=6
@@ -49,7 +50,7 @@ k_wavenumber = np.sqrt(3.0 * a/(4.0 * h0**3))  # wavenumber
 z = np.sqrt(3.0 * a * h0) / (2.0 * h0 * np.sqrt(h0 * (1.0 + a)))
 wavelength = 2.0 / k_wavenumber * np.arccosh(np.sqrt(1.0 / 0.050))  # wavelength of solitary wave
 c = np.sqrt(g * (1.0 + a) * h0)
-x0 = - h0/slope - wavelength/2.0  # location of the toe of the beach
+x0 = -50.0 - h0/slope - wavelength/2.0  # location of the toe of the beach
 
 def soliton(x,t): #
     sechSqd = (1.00/np.cosh( z*(x-x0-c*t)))**2.00
@@ -67,15 +68,11 @@ def bathymetry(X):
 ##### BOUNDARY CONDITIONS #####
 ###############################
 def water_height_DBC(X,flag):
-    if X[0]==-35:
+    if X[0]==L0[0]:
         return lambda x,t: h0
-    elif X[0]==L[0]:
-        return lambda X,t: 0.
     
 def x_mom_DBC(X,flag):
-    if X[0]==-35:
-        return lambda X,t: 0.0
-    elif X[0]==L[0]:
+    if X[0]==L0[0]:
         return lambda X,t: 0.0
     
 ##############################
