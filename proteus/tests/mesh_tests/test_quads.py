e3974e5ca08a07e8cb4969c646b3b1110331c3bd
Alistair <alistairbntl@gmail.com>
2018-10-02 11:44:26 -0400

    debbuing tests to help find swaped dof

--- proteus/tests/mesh_tests/test_quads.py
+++ proteus/tests/mesh_tests/test_quads.py
@@ -38,6 +38,26 @@ def simple_mesh():
                                                    useC=False)
     yield mlMesh, nnx, nny
 
+@pytest.fixture(scope="module")
+def simple_mesh_with_c():
+    nnx = 4
+    nny = 4
+    x = (-1.0,-1.0)
+    L = (2.0,2.0)
+    refinementLevels = 1
+    nLayersOfOverlap = 1
+    parallelPartitioningType = 0
+    skipInit = False
+    mlMesh = MeshTools.MultilevelQuadrilateralMesh(nnx,nny,1,
+                                                   x[0],x[1],0.0,
+                                                   L[0],L[1],1,
+                                                   refinementLevels,
+                                                   skipInit,
+                                                   nLayersOfOverlap,
+                                                   parallelPartitioningType,
+                                                   useC=True)
+    yield mlMesh, nnx, nny
+
 @pytest.mark.MeshTools
 def test_mesh_build(simple_mesh):
     """  Test mesh generation and refinment """
@@ -46,11 +66,24 @@ def test_mesh_build(simple_mesh):
     assert mlMesh.meshList[1].nElements_global == 4*(nnx-1)*(nny-1), 'Mesh generator has built incorrect number of quads'
 
 @pytest.mark.MeshTools
+def test_mesh_build_1(simple_mesh_with_c):
+    """ Test mesh generation with c """
+    mlMesh,nnx,nny = simple_mesh_with_c
+    assert mlMesh.meshList[0].nElements_global == (nnx-1)*(nny-1), 'Mesh generator has built incorrect number of quads'
+    # TODO (ARB) - add an additional test for refinement when its ready
+
+@pytest.mark.MeshTools
 def test_calc_quad_area(simple_mesh):
     mlMesh, nnx, nny = simple_mesh
     for i in range(9):
-        assert mlMesh.meshList[0]._calc_quad_area(i) == old_div(4.,9.)
+        assert mlMesh.meshList[0]._calc_quad_area(i) == 4. / 9.
 
+@pytest.mark.MeshTools
+def test_calc_quad_area(simple_mesh_with_c):
+    mlMesh, nnx, nny = simple_mesh_with_c
+    for i in range(9):
+        assert mlMesh.meshList[0]._calc_quad_area(i) == 4. / 9.
+        
 @pytest.mark.MeshTools
 def test_calc_hmax(simple_mesh):
     mlMesh, nnx, nny = simple_mesh
