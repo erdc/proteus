bd63f4fa7840364d259be764ffbcb0c326123796
manuel-quezada <manuel.quezada.dl@gmail.com>
2018-10-12 09:55:17 -0500

    Mquezada/two phase flow (#801)
    * Change alternatives for computing metrics
    
    * Add pre stage to CLSVOF
    
    * Do some extra adjustments
    
    * More adjustments and fix tests
    
    * add specialized vector for reconstruction of vof (out of clsvof)
    
    * Add first files for TwoPhaseFlow simulations
    
    * organize TwoPhaseFlow folder
    
    * Add falling bubble test to TwoPhaseFlow
    
    * change parun to account for organization of TwoPhaseFlow folder
    
    * checks involving arTime string and str(t) no longer match
    
    changed str(t) to match the way we format arTime:
    "{0:e}".format(t)
    
    * Set AUTOMATED_TEST=True for rising bubble problem
    
    * Add support for rans3p and do some general clean up
    
    * Add option -n to runTwoPhaseFlow to handle parallel runs
    
    * Pass context options and data dir to runTwoPhaseFlow.py
    
    * change _n files in all models to account for a different way to generate the mesh
    
    * change again triangleOptions within _n files
    
    * Change the way the gravity parameter is handled
    
    * change way of reading the context option ns_model
    
    * add solitary wave test
    
    * Add MeshRefinement file for solitary wave problem
    
    * Add rising bubble problem (two benchmarks)
    
    * Change again way to handle gravity
    
    * Fix minor bugs in the models for 3D problems
    
    * Add 3D version of falling bubble test
    
    * Change structure to consider a TwoPhaseFlowProblem class
    
    * Fix bug in _p models
    
    * Manage useSuperlu in a cleaner way
    
    * Add useSuperlu parameter to TwoPhaseFlowProblem class
    
    * Improve set of asserts
    
    * add and pass petsc.options.asm file for parallel runs
    
    * Adjust tests and add some new ones
    
    * Add 2D buckling flow
    
    * Minor change in 2D buckling problem
    
    * Minor modifications to 2D buckling flow problem
    
    * Add TODO.txt list
    
    * Add adaptivity (not working in parallel yet)
    
    * Allow passing different names to TwoPhaseFlow problem
    
    * clean options in TwoPhaseFlow tests
    
    * Add 5 automated tests for TwoPhaseFlow
    
    * Fix automated tests
    
    * checkout CLSVOF comparison files from master
    
    * Checkout NonlinearSolvers from master
    
    * Change name of test and add some paths to setup.py
    
    * debug stuff in the so files and tests
    
    * Minor fix in so file
    
    * import module problems in automated test
    
    * adjust tests
    
    * Fix tests for TwoPhaseFlow to run on Travis
    
    * Add small documentation tex file
    
    * Add assert to make sure that triangleOptions is not the default if the mesh is unstructured
    
    * rename tests within TwoPhaseFlow
    
    * set useSuperlu=True by default
    
    * Add test for nonlinear waves
    
    * Update --clean command and documentation
    
    * change comparison files for tests after changing default value of useSuperlu

---
+++ proteus/TwoPhaseFlow/models/rans3p_n.py
@@ -0,0 +1,108 @@
+from __future__ import absolute_import
+from proteus import *
+from proteus.default_n import *
+from rans3p_p import *
+
+# *********************************************** #
+# ********** Read from myTpFlowProblem ********** #
+# *********************************************** #
+cfl = myTpFlowProblem.cfl
+FESpace = myTpFlowProblem.FESpace
+he = myTpFlowProblem.he
+useSuperlu = myTpFlowProblem.useSuperlu
+domain = myTpFlowProblem.domain
+
+# *************************************** #
+# ********** MESH CONSTRUCTION ********** #
+# *************************************** #
+triangleFlag = myTpFlowProblem.triangleFlag
+nnx = myTpFlowProblem.nnx
+nny = myTpFlowProblem.nny
+nnz = myTpFlowProblem.nnz
+triangleOptions = domain.MeshOptions.triangleOptions
+
+# ******************************** #
+# ********** PARAMETERS ********** #
+# ******************************** #
+ns_shockCapturingFactor = rans3p_parameters['ns_shockCapturingFactor']
+ns_lag_shockCapturing = rans3p_parameters['ns_lag_shockCapturing']
+ns_lag_subgridError = rans3p_parameters['ns_lag_subgridError']
+
+# ************************************** #
+# ********** TIME INTEGRATION ********** #
+# ************************************** #
+timeDiscretization=rans3p_parameters['timeDiscretization']
+if timeDiscretization=='vbdf':
+    timeIntegration = VBDF
+    timeOrder=2
+    stepController  = Min_dt_cfl_controller
+else: #backward euler
+    timeIntegration = BackwardEuler_cfl
+    stepController  = Min_dt_cfl_controller
+runCFL=cfl
+
+# ******************************************* #
+# ********** FINITE ELEMENT SAPCES ********** #
+# ******************************************* #
+elementQuadrature = FESpace['elementQuadrature']
+elementBoundaryQuadrature = FESpace['elementBoundaryQuadrature']
+if nd==2:
+    femSpaces = {0:FESpace['velBasis'],
+	         1:FESpace['velBasis']}
+else:
+    femSpaces = {0:FESpace['velBasis'],
+	         1:FESpace['velBasis'],
+                 2:FESpace['velBasis']}
+
+# ************************************** #
+# ********** NONLINEAR SOLVER ********** #
+# ************************************** #
+fullNewtonFlag = True
+multilevelNonlinearSolver = Newton
+levelNonlinearSolver      = Newton
+nonlinearSmoother = None
+nonlinearSolverConvergenceTest = 'rits'
+levelNonlinearSolverConvergenceTest = 'rits'
+
+# ******************************************************** #
+# ********** NUMERICAL FLUXES AND STABILIZATION ********** #
+# ******************************************************** #
+numericalFluxType = RANS3PF.NumericalFlux
+conservativeFlux  = None
+subgridError = RANS3PF.SubgridError(coefficients=coefficients,
+                                    nd=nd,
+                                    lag=ns_lag_subgridError,
+                                    hFactor=FESpace['hFactor'])
+shockCapturing = RANS3PF.ShockCapturing(coefficients=coefficients,
+                                        nd=nd,
+                                        shockCapturingFactor=ns_shockCapturingFactor,
+                                        lag=ns_lag_shockCapturing)
+
+# ************************************ #
+# ********** LINEAR ALGEBRA ********** #
+# ************************************ #
+massLumping = False
+matrix = SparseMatrix
+linearSmoother = None
+# Linear solver 
+multilevelLinearSolver = KSP_petsc4py
+levelLinearSolver      = KSP_petsc4py
+if useSuperlu:
+    multilevelLinearSolver = LU
+    levelLinearSolver      = LU
+#
+linear_solver_options_prefix = 'rans3p_'
+linearSolverConvergenceTest             = 'r-true'
+
+# ******************************** #
+# ********** TOLERANCES ********** #
+# ******************************** #
+ns_nl_atol_res = max(1.0e-10, 0.01 * he ** 2)
+nl_atol_res = ns_nl_atol_res
+tolFac = 0.0
+linTolFac = 0.01
+l_atol_res = 0.01*ns_nl_atol_res
+
+useEisenstatWalker = False
+maxNonlinearIts = 50
+maxLineSearches = 0
---
+++ scripts/runTwoPhaseFlow.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+import os
+import optparse
+import sys
+import proteus
+
+parser = optparse.OptionParser()
+parser.add_option("-l", "--log",
+                  help="Store information about what the code is doing, 0=none, 10=everything",
+                  action="store",
+                  type="int",
+                  dest="logLevel",
+                  default=1)
+parser.add_option("--clean",
+                  help="Clean folder",
+                  action="store_true",
+                  dest="clean")
+parser.add_option("-C",
+                  help="Context options",
+                  action="store",
+                  type="string",
+                  default="",
+                  dest="context")
+parser.add_option("--pumi",
+                  help="Run with adaptivity",
+                  action="store_true",
+                  default=False,
+                  dest="pumi")
+parser.add_option("-D", "--dataDir",
+                  help="Data directory",
+                  action="store",
+                  type="string",
+                  dest="dataDir")
+parser.add_option("-n","--num_proc",
+                  help="Number of processors to use",
+                  type="int",
+                  action="store",
+                  default=1,
+                  dest="num_proc")
+parser.add_option("-f","--fileName",
+                  help="Name of setup file",
+                  action="store",
+                  type="string",
+                  dest="fileName",
+                  default="")
+
+(opts,args) = parser.parse_args()
+
+################
+# CLEAN FOLDER #
+################
+if opts.clean is not None:
+    os.system("rm -r *face *.csv __pycache__ *mesh* *.poly *.pyc *.log *.edge *.ele *.neig *.node *.h5 *.xmf *~ *#* *.txt *smb *pos *dmg splitMesh *pumi* *dat *res")
+    exit()
+
+############
+# DATA DIR #
+############
+dataDir = "" if opts.dataDir is None else "-D " + opts.dataDir
+
+########
+# PUMI #
+########
+if opts.pumi:
+    from os import listdir
+    from os.path import isfile, join
+    path = "./splitMesh/"
+
+    # FIRST STAGE: CREATE THE MESH IN SERIAL #
+
+    err = os.system("parun --TwoPhaseFlow --pumi -l" + str(opts.logLevel) + " -v TwoPhaseFlow_so.py ")
+    if(err):
+        import sys
+        sys.exit("error during proteus mesh to PUMI model derivation phase\n")
+
+    # SECOND STAGE: SPLIT THE MESH IN PARALLEL #
+    split = ("mpiexec -np " + str(opts.num_proc) +
+             " split Reconstructed.dmg Reconstructed.smb " + path + " " + str(opts.num_proc))
+    os.system("rm -r " + path)
+    os.system(split)
+    # rename files
+    files = [f for f in listdir(path) if isfile(join(path, f))]
+    [os.rename(path+f,path+"splitMesh"+str(counter)+".smb") for counter,f in enumerate(files)]
+
+#############
+# FILE NAME #
+#############
+fileName = ""
+if opts.fileName != "":
+    fileName = " -f " + opts.fileName
+
+##############
+# CALL PARUN #
+##############
+assert opts.num_proc >= 1, "Argument of -n must be an integer larger or equal to 1"
+usePumi = "--pumi --pumiStage 2 " if opts.pumi else " "
+if opts.num_proc==1:
+    os.system("parun --TwoPhaseFlow " +
+              usePumi +
+              "-l" + str(opts.logLevel) +
+              " -v TwoPhaseFlow_so.py " +
+              dataDir +
+              " -C '" + opts.context + "'" +
+              fileName)
+else:
+    path_utils = proteus.__path__[0]+"/TwoPhaseFlow/utils/"
+    petsc_options = path_utils + "petsc.options.asm"
+    os.system("mpirun -np " + str(opts.num_proc) +
+              " parun --TwoPhaseFlow --TpFlowParallel " +
+              usePumi +
+              "-l" + str(opts.logLevel) +
+              " -v TwoPhaseFlow_so.py " +
+              dataDir +
+              " -O " + petsc_options +
+              " -C '" + opts.context + "'" +
+              fileName)
+
