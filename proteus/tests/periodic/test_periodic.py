cfcb26efc661fda5d97d22d02eedba76b5047d8e
Alistair <alistairbntl@gmail.com>
2018-10-24 13:54:26 -0400

    2D and 3D duct tests with constant pressure null space

--- proteus/tests/periodic/test_periodic.py
+++ proteus/tests/periodic/test_periodic.py
@@ -41,22 +41,35 @@ def load_periodic_duct(request):
             s = default_s
             sList.append(s)
     else:
-        sList = so.sList    
+        sList = so.sList
     yield so, pList, nList, sList
+
+#    def clean_up():
+#        expected.close()
+#        actual.close()
+    request.addfinalizer(clean_up)
 
 @pytest.fixture()
 def load_periodic_opts_2D(request):
     opts.contextOptions = "periodic=True grid=True nd=2 nnx=42 triangles=False spaceOrder=1 weak=True coord=True pc_type='selfp_petsc'" 
-    proteus.Context.contextOptionsString=opts.contextOptions   
+    proteus.Context.contextOptionsString=opts.contextOptions
     opts.petscOptionsFile = './petsc/petsc.options.schur.selfp_petsc.superlu'
     proteus.Comm.argv = TestTools.fixture_set_petsc_options_from_file(opts.petscOptionsFile)
     comm = Comm.init()
 
 @pytest.fixture()
 def load_periodic_opts_3D(request):
-    opts.contextOptions = "periodic=True nd=3 coord=True pc_type='selfp_petsc'" 
+    opts.contextOptions = "periodic=True nd=3 coord=True pc_type='selfp_petsc'"
     proteus.Context.contextOptionsString=opts.contextOptions
     opts.petscOptionsFile = './petsc/petsc.options.schur.selfp_petsc.superlu'
+    proteus.Comm.argv = TestTools.fixture_set_petsc_options_from_file(opts.petscOptionsFile)
+    comm = Comm.init()
+
+@pytest.fixture()
+def load_periodic_opts_3D_T2(request):
+    opts.contextOptions = "periodic=True nd=3 coord=True pc_type='selfp_petsc' A_block_AMG=True" 
+    proteus.Context.contextOptionsString=opts.contextOptions
+    opts.petscOptionsFile = './petsc/petsc.options.schur.selfp_petsc.amg'
     proteus.Comm.argv = TestTools.fixture_set_petsc_options_from_file(opts.petscOptionsFile)
     comm = Comm.init()
 
@@ -69,6 +82,17 @@ def test_periodic_2D(load_periodic_opts_2D,
                                    opts)
     ns.calculateSolution('test_run')
 
+    script_dir = os.path.dirname(__file__)
+    relpath = 'comparison_files/basic_2d_test.h5'
+    expected = tables.open_file(os.path.join(script_dir,relpath))
+    actual = tables.open_file('duct11t12dpghe0.0975609756098.h5')
+
+    assert numpy.allclose(expected.root.velocity_t59.read(),
+                          actual.root.velocity_t59.read(),
+                          atol=1e-10)
+    expected.close()
+    actual.close()
+
 def test_periodic_3D(load_periodic_opts_3D,
                      load_periodic_duct):
     ns = NumericalSolution.NS_base(load_periodic_duct[0],
@@ -77,3 +101,34 @@ def test_periodic_3D(load_periodic_opts_3D,
                                    load_periodic_duct[3],
                                    opts)
     ns.calculateSolution('test_run')
+
+    script_dir = os.path.dirname(__file__)
+    relpath = 'comparison_files/basic_3d_test.h5'
+    expected = tables.open_file(os.path.join(script_dir,relpath))
+    actual = tables.open_file('ductp1t13dpghe0.2.h5')
+
+    assert numpy.allclose(expected.root.velocity_t24.read(),
+                          actual.root.velocity_t24.read(),
+                          atol=1e-10)
+    expected.close()
+    actual.close()
+
+def test_periodic_3D_amg(load_periodic_opts_3D_T2,
+                         load_periodic_duct):
+    ns = NumericalSolution.NS_base(load_periodic_duct[0],
+                                   load_periodic_duct[1],
+                                   load_periodic_duct[2],
+                                   load_periodic_duct[3],
+                                   opts)
+    ns.calculateSolution('test_run')
+
+    script_dir = os.path.dirname(__file__)
+    relpath = 'comparison_files/basic_3d_test.h5'
+    expected = tables.open_file(os.path.join(script_dir,relpath))
+    actual = tables.open_file('ductp1t13dpghe0.2.h5')
+
+    assert numpy.allclose(expected.root.velocity_t24.read(),
+                          actual.root.velocity_t24.read(),
+                          atol=1e-10)
+    expected.close()
+    actual.close()
