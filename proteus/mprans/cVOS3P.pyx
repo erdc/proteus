126204fc4e86e73edb521e276f47e48e33ef2c87
manuel-quezada <manuel.quezada.dl@gmail.com>
2018-10-31 16:11:27 +0100

    Mquezada/update sed penalty (#837)
    * Fixing bug with array length
    
    * fix problem with newstr in SpatialTools caused by str vs repr - like the one in Spatial tools
    
    * First step towards unification of VOF and VOF3P
    
    * Clean VOF models wrt white spaces
    
    * Add test for vos
    
    * step towards unification of VOF and VOF3P
    
    * more adjustments to VOF and VOF3P
    
    * vos working by itself not with rans sed
    
    * push files for debugging sed transport full model

--- proteus/mprans/cVOS3P.pyx
+++ proteus/mprans/cVOS3P.pyx
@@ -178,6 +178,8 @@ cdef extern from "mprans/VOS3P.h" namespace "proteus":
                           double * dLow,
                           double * fluxMatrix,
                           double * limitedFlux,
+                          double * min_u_bc,
+                          double * max_u_bc,
                           int * csrRowIndeces_DofLoops,
                           int * csrColumnOffsets_DofLoops)
         void calculateResidual_entropy_viscosity(double dt,
@@ -700,6 +702,8 @@ cdef class VOS3P:
                      numpy.ndarray dLow,
                      numpy.ndarray fluxMatrix,
                      numpy.ndarray limitedFlux,
+                     numpy.ndarray min_u_bc,
+                     numpy.ndarray max_u_bc,
                      numpy.ndarray csrRowIndeces_DofLoops,
                      numpy.ndarray csrColumnOffsets_DofLoops):
         self.thisptr.kth_FCT_step(dt,
@@ -715,6 +719,8 @@ cdef class VOS3P:
                                   < double * > dLow.data,
                                   < double * > fluxMatrix.data,
                                   < double * > limitedFlux.data,
+                                  < double * > min_u_bc.data,
+                                  < double * > max_u_bc.data,
                                   < int * > csrRowIndeces_DofLoops.data,
                                   < int * > csrColumnOffsets_DofLoops.data)
     def calculateResidual_entropy_viscosity(self,
