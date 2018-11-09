a0bd6c51ca9b7c1ffd6288449eee4d5e58d94b3f
Aggelos Dimakopoulos <asd@hrwallingford.com>
2018-11-09 20:57:15 +0000

    Adimako/update sed penalty (#824)
    * Fixing bug with array length
    
    * Removing useless variable nu_s
    
    * fix problem with newstr in SpatialTools caused by str vs repr - like the one in Spatial tools

--- proteus/mprans/RANS3PSed.py
+++ proteus/mprans/RANS3PSed.py
@@ -142,9 +142,11 @@ class Coefficients(proteus.TransportCoefficients.TC_base):
     def __init__(self,
                  epsFact=1.5,
                  sigma=72.8,
-                 rho_0=998.2, nu_0=1.004e-6,
-                 rho_1=1.205, nu_1=1.500e-5,
-                 rho_s=2600.0, nu_s=1.004e-6,
+                 rho_0=998.2,
+                 nu_0=1.004e-6,
+                 rho_1=1.205,
+                 nu_1=1.500e-5,
+                 rho_s=2600.0,
                  g=[0.0, 0.0, -9.8],
                  nd=3,
                  ME_model=5,
@@ -264,7 +266,6 @@ class Coefficients(proteus.TransportCoefficients.TC_base):
         self.rho_1 = rho_1
         self.nu_1 = nu_1
         self.rho_s = rho_s
-        self.nu_s = nu_s
         self.g = numpy.array(g)
         self.nd = nd
         #
@@ -1868,7 +1869,6 @@ class LevelModel(proteus.Transport.OneLevelTransport):
             self.coefficients.rho_1,
             self.coefficients.nu_1,
             self.coefficients.rho_s,
-            self.coefficients.nu_s,
             self.coefficients.smagorinskyConstant,
             self.coefficients.turbulenceClosureModel,
             self.Ct_sge,
@@ -2098,7 +2098,6 @@ class LevelModel(proteus.Transport.OneLevelTransport):
             self.coefficients.rho_1,
             self.coefficients.nu_1,
             self.coefficients.rho_s,
-            self.coefficients.nu_s,
             self.coefficients.smagorinskyConstant,
             self.coefficients.turbulenceClosureModel,
             self.Ct_sge,
