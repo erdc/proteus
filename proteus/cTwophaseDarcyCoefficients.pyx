4b60cc50b6e5814bdec8044705c786c02235ac1f
Chris Kees <cekees@gmail.com>
2018-12-27 16:13:15 -0600

    [WIP] added function declarations

---
+++ proteus/cTwophaseDarcyCoefficients.pyx
@@ -0,0 +1,189 @@
+# A type of -*- python -*- file
+import numpy as np
+cimport numpy as np
+
+def twophaseDarcy_fc_sd_het_matType():
+    """Evaluate the coefficients of the fully coupled formulation of
+    (slightly) compressible, two-phase Darcy flow for a heterogeneous
+    medium, sparse diffusion rep. for het
+
+    """
+
+def twophaseDarcy_fc_sd_het_matType_nonPotentialForm():
+    """"Evaluate the coefficients of the fully coupled formulation of
+    (slightly) compressible, two-phase Darcy flow for a heterogeneous
+    medium, sparse diffusion rep. for het
+
+    """
+
+def twophaseDarcy_fc_pp_sd_het_matType():
+    """Evaluate the coefficients of the fully coupled formulation of
+    (slightly) compressible, two-phase Darcy flow for a heterogeneous
+    medium, pressure-pressure sparse diffusion rep. for het
+
+    """
+
+def twophaseDarcy_incompressible_split_sd_pressure_het_matType():
+    """Evaluate the pressure coefficients of the split fractional flow
+formulation of incompressible, two-phase Darcy flow for a
+heterogeneous medium, sparse diffusion rep het
+
+    """
+
+def twophaseDarcy_incompressible_split_sd_saturation_het_matType():
+    """Evaluate the saturation coefficients of the split fractional flow
+formulation of incompressible, two-phase Darcy flow for a
+heterogeneous medium, sparse diffusion rep het
+
+    """
+
+def twophaseDarcy_slightCompressible_split_sd_pressure_het_matType():
+    """Evaluate the pressure coefficients of the split fractional flow
+formulation of slight compressible, two-phase Darcy flow for a
+heterogeneous medium, sparse diffusion rep het
+
+    """
+
+def twophaseDarcy_slightCompressible_split_sd_saturation_het_matType():
+    """Evaluate the saturation coefficients of the split fractional flow
+formulation of slight compressible, two-phase Darcy flow for a
+heterogeneous medium, sparse diffusion rep het
+
+    """
+
+def twophaseDarcy_compressibleN_split_sd_pressure_het_matType():
+    """Evaluate the pressure coefficients of the split fractional flow
+formulation of compressible non-wetting phase, two-phase Darcy flow
+for a heterogeneous medium, sparse diffusion rep het
+
+    """
+
+def twophaseDarcy_compressibleN_split_sd_saturation_het_matType():
+    """Evaluate the saturation coefficients of the split fractional flow
+formulation of compressible non-wetting phase, two-phase Darcy flow
+for a heterogeneous medium, sparse diffusion rep het
+
+    """
+
+def twophaseDarcy_vol_frac():
+    """compute phase volume fractions from point-vals for sw and zoned
+material types for porosity (omega)
+
+    """
+
+def generateSplineTables():
+    """generate spline table look up arrays"""
+
+def twophaseDarcy_incompressible_split_pp_sd_saturation_het_matType():
+    """Evaluate the saturation coefficients of the split fractional flow
+formulation of incompressible, two-phase Darcy flow for a
+heterogeneous medium, sparse diffusion rep het capillary pressure is
+primary variable
+
+    """
+
+def twophaseDarcy_fc():
+    """Evaluate the coefficients of the fully coupled formulation of
+incompressible, two-phase Darcy flow
+
+    """
+
+def twophaseDarcy_fc_sd_diag():
+    """Evaluate the coefficients of the fully coupled formulation of
+incompressible, two-phase Darcy flow, sparse diffusion rep. assuming
+isotropic Ks
+
+    """
+
+def twophaseDarcy_fc_het_matType():
+    """Evaluate the coefficients of the fully coupled formulation of
+incompressible, two-phase Darcy flow for a heterogeneous medium
+
+    """
+
+def twophaseDarcy_fc_sd_diag_het_matType():
+    """Evaluate the coefficients of the fully coupled formulation of
+incompressible, two-phase Darcy flow for a heterogeneous medium,
+sparse diffusion rep. assuming isotropic Ks
+
+    """
+
+def twophaseDarcy_fc_ff():
+    """Evaluate the coefficients of the fully coupled fractional flow
+formulation of incompressible, two-phase Darcy flow
+
+    """
+
+def twophaseDarcy_fc_ff_sd_diag():
+    """Evaluate the coefficients of the fully coupled fractional flow
+formulation of incompressible, two-phase Darcy flow, sparse
+diff. diagonal het
+
+    """
+
+def twophaseDarcy_fc_ff_sd_diag_het_matType():
+    """Evaluate the coefficients of the fully coupled fractional flow
+formulation of incompressible, two-phase Darcy flow for a
+heterogeneous medium, sparse diff. diagonal het
+
+    """
+
+def twophaseDarcy_fc_ff_het_matType():
+    """Evaluate the coefficients of the fully coupled fractional flow
+formulation of incompressible, two-phase Darcy flow for a
+heterogeneous medium
+
+    """
+
+def twophaseDarcy_split_saturation():
+    """Evaluate the coefficients of the split fractional flow formulation
+of incompressible, two-phase Darcy flow
+
+    """
+
+def twophaseDarcy_split_sd_diag_saturation():
+    """Evaluate the coefficients of the split fractional flow formulation
+of incompressible, two-phase Darcy flow, sparse diff. diagonal het
+
+    """
+
+def twophaseDarcy_split_saturation_het_matType():
+    """Evaluate the coefficients of the split fractional flow formulation
+of incompressible, two-phase Darcy flow for a heterogeneous medium
+
+    """
+
+def twophaseDarcy_split_sd_diag_saturation_het_matType():
+    """Evaluate the coefficients of the split fractional flow formulation
+of incompressible, two-phase Darcy flow for a heterogeneous medium,
+sparse diffusion diagonal
+
+    """
+
+def twophaseDarcy_split_pressure():
+    """Evaluate the coefficients of the split fractional flow formulation
+of incompressible, two-phase Darcy flow
+
+    """
+
+def twophaseDarcy_split_sd_diag_pressure():
+    """Evaluate the coefficients of the split fractional flow formulation
+of incompressible, two-phase Darcy flow, sparse diffusion diagonal
+het
+
+    """
+
+def twophaseDarcy_split_pressure_het_matType():
+    """Evaluate the coefficients of the split fractional flow formulation
+of incompressible, two-phase Darcy flow for a heterogeneous medium
+
+    """
+
+def twophaseDarcy_split_sd_diag_pressure_het_matType():
+    """Evaluate the coefficients of the split fractional flow formulation
+of incompressible, two-phase Darcy flow for a heterogeneous medium,
+sparse diffusion diagonal het
+
+    """
+
+
