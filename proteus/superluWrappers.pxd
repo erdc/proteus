a4460b0f95d811de58f5eb1983ea95b5434c91fe
Alistair <alistairbntl@gmail.com>
2019-01-15 09:53:40 -0500

    Declare SuperMatrix in Cython headerfile

---
+++ proteus/superluWrappers.pxd
@@ -0,0 +1,32 @@
+cdef extern from "proteus_superlu.h":
+    ctypedef enum _Stype_t 'Stype_t':
+        _SLU_NC 'SLU_NC'
+        _SLU_NCP 'SLU_NCP'
+        _SLU_NR 'SLU_NR'
+        _SLU_SC 'SLU_SC'
+        _SLU_SCP 'SLU_SCP'
+        _SLU_SR 'SLU_SR'
+        _SLU_DN 'SLU_DN'
+        _SLU_NR_loc 'SLU_NR_loc'
+    ctypedef enum _Dtype_t 'Dtype_t':
+        _SLU_S 'SLU_S'
+        _SLU_D 'SLU_D'
+        _SLU_C 'SLU_C'
+        _SLU_Z 'SLU_Z'
+    ctypedef enum _Mtype_t 'Mtype_t':
+        _SLU_GE 'SLU_GE'
+        _SLU_TRLU 'SLU_TRLU'
+        _SLU_TRUU 'SLU_TRUU'
+        _SLU_TRL 'SLU_TRL'
+        _SLU_TRU 'SLU_TRU'
+        _SLU_SYL 'SLU_SYL'
+        _SLU_SYU 'SLU_SYU'
+        _SLU_HEL 'SLU_HEL'
+        _SLU_HEU 'SLU_HEU'
+    ctypedef struct _SuperMatrix 'SuperMatrix':
+        _Stype_t Stype
+        _Dtype_t Dtype
+        _Mtype_t Mtype
+        int nrow
+        int ncol
+        void * Store
--- proteus/superluWrappers.pyx
+++ proteus/superluWrappers.pyx
@@ -16,30 +16,6 @@ cdef extern from "proteus_superlu.h":
         _SamePattern "SamePattern"
         _SamePattern_SameRowPerm "SamePattern_SameRowPerm"
         _FACTORED "FACTORED"
-    ctypedef enum _Stype_t 'Stype_t':
-        _SLU_NC 'SLU_NC'
-        _SLU_NCP 'SLU_NCP'
-        _SLU_NR 'SLU_NR'
-        _SLU_SC 'SLU_SC'
-        _SLU_SCP 'SLU_SCP'
-        _SLU_SR 'SLU_SR'
-        _SLU_DN 'SLU_DN'
-        _SLU_NR_loc 'SLU_NR_loc'
-    ctypedef enum _Dtype_t 'Dtype_t':
-        _SLU_S 'SLU_S'
-        _SLU_D 'SLU_D'
-        _SLU_C 'SLU_C'
-        _SLU_Z 'SLU_Z'
-    ctypedef enum _Mtype_t 'Mtype_t':
-        _SLU_GE 'SLU_GE'
-        _SLU_TRLU 'SLU_TRLU'
-        _SLU_TRUU 'SLU_TRUU'
-        _SLU_TRL 'SLU_TRL'
-        _SLU_TRU 'SLU_TRU'
-        _SLU_SYL 'SLU_SYL'
-        _SLU_SYU 'SLU_SYU'
-        _SLU_HEL 'SLU_HEL'
-        _SLU_HEU 'SLU_HEU'
     ctypedef enum _trans_t "trans_t":
         _NOTRANS 'NOTRANS'
         _TRANS 'TRANS'
@@ -50,13 +26,6 @@ cdef extern from "proteus_superlu.h":
         pass
     ctypedef struct _superlu_options_t "superlu_options_t":
         _fact_t Fact
-    ctypedef struct _SuperMatrix 'SuperMatrix':
-        _Stype_t Stype
-        _Dtype_t Dtype
-        _Mtype_t Mtype
-        int nrow
-        int ncol
-        void * Store
     void cdgstrs "dgstrs"(_trans_t, _SuperMatrix *, _SuperMatrix *, int *, int *, _SuperMatrix *, _SuperLUStat_t*, int *)
     void cdgstrf "dgstrf"(_superlu_options_t *, _SuperMatrix *, int, int, int *, void *, int, int *, int *, _SuperMatrix *, _SuperMatrix *, _GlobalLU_t*, _SuperLUStat_t *, int *)
     void cStatInit "StatInit"(_SuperLUStat_t *)
@@ -82,7 +51,6 @@ class SparseMatrix(object):
         self.nzvals = nzvals
         self.colind = colind
         self.rowptr = rowptr
-#        self._cSparseMatrx = _SparseMatrix
         self._cSparseMatrix = cSparseMatrix(nr,
                                             nc,
                                             nnz,
