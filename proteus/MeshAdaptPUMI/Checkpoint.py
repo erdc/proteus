32aa1c4aa8bb7a7a8d8938a70207838a558f49be
alvin <alv.zhang@gmail.com>
2019-01-15 22:11:18 -0500

    STATE: save mesh and info successfully through json

--- proteus/MeshAdaptPUMI/Checkpoint.py
+++ proteus/MeshAdaptPUMI/Checkpoint.py
@@ -19,16 +19,68 @@ class Checkpointer:
       self.counter = 0
       self.frequency = frequency
     def doSomething(self):
-      print self.counter
-      print self.A
-      print self.B 
-      print self.NSobject
       self.transferInfo()
       self.saveMesh()
       self.counter+=1
+
+      #simplifyModelListOld(self.NSobject.modelListOld)
+      modelListOld=EncodeModel(self.NSobject.modelListOld,self.NSobject.systemStepController.t_system_last)
+      #pickling is apparently unsafe so we use json to try storing modelListOld
+      filename = "checkpointInfo"+str(self.counter)
+      f = open(filename, 'w')
+      import json
+      json.dump(modelListOld.__dict__,f)
+      f.close()
+      #f2 = open(filename,'w')
+      #f2.write("Time for hotstart is "+str(self.NSobject.systemStepController.t_system_last))
+      #f2.close()
     def transferInfo(self):
       self.NSobject.PUMI_transferFields()
     def saveMesh(self):
       fileName="checkpoint"+str(self.counter)+"_.smb"
       self.NSobject.pList[0].domain.PUMIMesh.writeMesh(fileName)
+    #def loadCheckPoint(self,filename):
+      #I want to load the mesh
+
+#Maybe I can form an abstract class for encoding decoding?
+class EncodeModel:
+      "Grab only necessary components from modelListOld so far consistent only with first-order time integrator" 
+      def __init__(self,modelListOld,hotStartTime):
+        self.numModels = len(modelListOld)
+        self.hotStartTime = hotStartTime
+        self.stepController=[]
+        self.timeIntegration=[]
+        self.shockCapturing=[]
+        for i in range(0,self.numModels):
+          self.stepController.append([modelListOld[i].stepController.dt_model,modelListOld[i].stepController.t_model,modelListOld[i].stepController.t_model_last, modelListOld[i].stepController.substeps])
+          if(hasattr(modelListOld[i].levelModelList[0].timeIntegration,'dtLast')):
+            self.timeIntegration.append([modelListOld[i].levelModelList[0].timeIntegration.dt,modelListOld[i].levelModelList[0].timeIntegration.t,modelListOld[i].levelModelList[0].timeIntegration.dt,modelListOld[i].levelModelList[0].timeIntegration.dtLast])
+          else:
+            self.timeIntegration.append([modelListOld[i].levelModelList[0].timeIntegration.dt,modelListOld[i].levelModelList[0].timeIntegration.t,modelListOld[i].levelModelList[0].timeIntegration.dt])
+          if(modelListOld[i].levelModelList[0].shockCapturing is not None):
+            self.shockCapturing.append([modelListOld[i].levelModelList[0].shockCapturing.nSteps, modelListOld[i].levelModelList[0].shockCapturing.nStepsToDelay])
+
+          #self.stepController.append(modelListOld[i].stepController.__dict__)
+          #self.timeIntegration.append(modelListOld[i].levelModelList[0].timeIntegration.__dict__)
+          #if(modelListOld[i].levelModelList[0].shockCapturing is not None):
+          #  self.shockCapturing.append(modelListOld[i].levelModelList[0].shockCapturing.__dict__)
+        #import pdb; pdb.set_trace()
+        #import sys; sys.exit()
+
+class DecodeModel:
+      "create a modelListOld that can interact with the post-adapt restart capabilities" 
+      def __init__(self,modelListOld):
+        self.numModels = modelListOld.numModels
+        #self.hotStartTime = hotStartTime
+        self.stepController={}
+        self.timeIntegration={}
+        self.shockCapturing={}
+        #for i in range(0,self.numModels):
+        #  self.stepController.append([modelListOld[i].stepController.dt_model,modelListOld[i].stepController.t_model,modelListOld[i].stepController.t_model_last, modelListOld[i].stepController.substeps])
+        #  if(hasattr(modelListOld[i].levelModelList[0].timeIntegration,'dtLast')):
+        #    self.timeIntegration.append([modelListOld[i].levelModelList[0].timeIntegration.dt,modelListOld[i].levelModelList[0].timeIntegration.t,modelListOld[i].levelModelList[0].timeIntegration.dt,modelListOld[i].levelModelList[0].timeIntegration.dtLast])
+        #  else:
+        #    self.timeIntegration.append([modelListOld[i].levelModelList[0].timeIntegration.dt,modelListOld[i].levelModelList[0].timeIntegration.t,modelListOld[i].levelModelList[0].timeIntegration.dt])
+        #  if(modelListOld[i].levelModelList[0].shockCapturing is not None):
+        #    self.shockCapturing.append([modelListOld[i].levelModelList[0].shockCapturing.nSteps, modelListOld[i].levelModelList[0].shockCapturing.nStepsToDelay])
 
