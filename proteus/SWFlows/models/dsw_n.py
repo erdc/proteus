3821f3b25480438eae4b73602596c45a4b7f0a80
Eric Tovar <ejtovar@math.tamu.edu>
2019-01-24 18:43:12 -0600

    adding dsw n and p files

---
+++ proteus/SWFlows/models/dsw_n.py
@@ -0,0 +1,83 @@
+from __future__ import absolute_import
+from proteus import *
+from proteus.default_n import *
+from dsw_p import *
+
+# *********************************************** #
+# ********** Read from mySWFlowProblem ********** #
+# *********************************************** #
+# READ FROM CONTEXT #
+runCFL = mySWFlowProblem.cfl
+FESpace = mySWFlowProblem.FESpace
+he = mySWFlowProblem.he
+useSuperlu = mySWFlowProblem.useSuperlu
+domain = mySWFlowProblem.domain
+SSPOrder = mySWFlowProblem.swe_parameters['SSPOrder']
+LUMPED_MASS_MATRIX = mySWFlowProblem.swe_parameters['LUMPED_MASS_MATRIX']
+
+# *************************************** #
+# ********** MESH CONSTRUCTION ********** #
+# *************************************** #
+if domain is not None:
+    triangleFlag = mySWFlowProblem.triangleFlag
+    nnx = mySWFlowProblem.nnx
+    nny = mySWFlowProblem.nny
+    nnz = mySWFlowProblem.nnz
+    triangleOptions = domain.MeshOptions.triangleOptions
+
+# ************************************** #
+# ********** TIME INTEGRATION ********** #
+# ************************************** #
+timeIntegration = DSW2DCV.RKEV
+timeOrder = SSPOrder
+nStagesTime = SSPOrder
+
+# ****************************************** #
+# ********** TIME STEP CONTROLLER ********** #
+# ****************************************** #
+stepController = Min_dt_controller
+
+# ******************************************* #
+# ********** FINITE ELEMENT SAPCES ********** #
+# ******************************************* #
+elementQuadrature = FESpace['elementQuadrature']
+elementBoundaryQuadrature = FESpace['elementBoundaryQuadrature']
+femSpaces = {0: FESpace['basis'],
+             1: FESpace['basis'],
+             2: FESpace['basis'],
+             3: FESpace['basis'],
+             4: FESpace['basis']}
+
+# ************************************** #
+# ********** NONLINEAR SOLVER ********** #
+# ************************************** #
+multilevelNonlinearSolver  = Newton
+fullNewtonFlag = False #NOTE: False just if the method is explicit
+if (LUMPED_MASS_MATRIX==1):
+    levelNonlinearSolver = ExplicitLumpedMassMatrixShallowWaterEquationsSolver
+else:
+    levelNonlinearSolver = ExplicitConsistentMassMatrixShallowWaterEquationsSolver
+
+# ************************************ #
+# ********** NUMERICAL FLUX ********** #
+# ************************************ #
+numericalFluxType = DSW2DCV.NumericalFlux
+
+# ************************************ #
+# ********** LINEAR ALGEBRA ********** #
+# ************************************ #
+matrix = SparseMatrix
+multilevelLinearSolver = LU
+levelLinearSolver = LU
+levelNonlinearSolverConvergenceTest = 'r'
+linearSolverConvergenceTest = 'r-true'
+
+# ******************************** #
+# ********** TOLERANCES ********** #
+# ******************************** #
+nl_atol_res = 1.0e-5
+nl_rtol_res = 0.0
+l_atol_res = 1.0e-7
+l_rtol_res = 0.0
+tolFac = 0.0
+maxLineSearches=0
