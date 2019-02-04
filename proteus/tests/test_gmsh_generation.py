ea50647b73875d2eda194745f72a22b00500d400
Tristan de Lataillade <delataillade.tristan@gmail.com>
2019-02-04 11:51:09 +0000

    fix gmsh and added mass tests

--- proteus/tests/test_gmsh_generation.py
+++ proteus/tests/test_gmsh_generation.py
@@ -24,25 +24,25 @@ class TestGMSH(unittest.TestCase):
         domain.segments = [[0, 1], [1, 2], [2, 3], [3, 0]]
         domain.facets = [[[0, 1, 2, 3]]]
         domain.writeGeo('gmsh_mesh_test_2D', he_max=0.1)
-        gmsh_cmd = "gmsh {0:s} -v 10 -2 -o {1:s} -format msh".format(domain.geofile+".geo", domain.geofile+".msh")
+        gmsh_cmd = "gmsh {0:s} -v 10 -2 -o {1:s} -format msh2".format(domain.geofile+".geo", domain.geofile+".msh")
         check_call(gmsh_cmd, shell=True)
         MeshTools.msh2simplex(domain.geofile, nd=2)
         with open('gmsh_mesh_test_2D.node', 'r') as nodefile:
-            npt.assert_equal(nodefile.readline(), '3425 2 0 1\n')
+            npt.assert_equal(nodefile.readline(), '3433 2 0 1\n')
         with open('gmsh_mesh_test_2D.edge', 'r') as edgefile:
-            npt.assert_equal(edgefile.readline(), '10072 1\n')
+            npt.assert_equal(edgefile.readline(), '10096 1\n')
         with open('gmsh_mesh_test_2D.ele', 'r') as elefile:
-            npt.assert_equal(elefile.readline(), '6648 3 1\n')
+            npt.assert_equal(elefile.readline(), '6664 3 1\n')
 
     def test_gmsh_generation_3D(self):
         domain = Domain.PiecewiseLinearComplexDomain()
         cube = st.Cuboid(domain, dim=[2.,2.,2.])
         st.assembleDomain(domain)
         domain.writeGeo('gmsh_mesh_test_3D', he_max=0.1)
-        gmsh_cmd = "gmsh {0:s} -v 10 -3 -o {1:s} -format msh".format(domain.geofile+".geo", domain.geofile+".msh")
+        gmsh_cmd = "gmsh {0:s} -v 10 -3 -o {1:s} -format msh2".format(domain.geofile+".geo", domain.geofile+".msh")
         check_call(gmsh_cmd, shell=True)
         MeshTools.msh2simplex(domain.geofile, nd=3)
-        # cek disabling exact tests due to cross-platform non-reproducibility 
+        # cek disabling exact tests due to cross-platform non-reproducibility
         # with open('gmsh_mesh_test_3D.node', 'r') as nodefile:
         #     npt.assert_equal(nodefile.readline(), '7674 3 0 1\n')
         # with open('gmsh_mesh_test_3D.edge', 'r') as edgefile:
