6f060fbc9c97a9c25141b2638d9c0dcceff449f1
Eric Tovar <ejtovar@math.tamu.edu>
2019-02-05 14:59:04 -0600

    updating files

--- proteus/SWFlows/tests/solitary_wave/SWFlow.py
+++ proteus/SWFlows/tests/solitary_wave/SWFlow.py
@@ -30,23 +30,22 @@ opts = Context.Options([
     ('sw_model', 0, "sw_model = {0,1} for {SWEs,DSWEs}"),
     ("final_time", 4.0, "Final time for simulation"),
     ("dt_output", 0.1, "Time interval to output solution"),
-    ("cfl", 0.1, "Desired CFL restriction"),
+    ("cfl", 0.33, "Desired CFL restriction"),
     ("refinement", 4, "Refinement level")
 ])
 
 ###################
 # DOMAIN AND MESH #
 ###################
-L = (10.0, 0.50)
-X_coords = (0.0, 8.0)  # this is domain, used in BCs
-refinement = opts.refinement
+L = (12.0, 1.0)
+X_coords = (0.0, 12.0)  # this is domain, used in BCs
 domain = RectangularDomain(L=L, x=[0, 0, 0])
 
 # CREATE REFINEMENT #
+refinement = opts.refinement
 nnx0 = 6
 nnx = (nnx0 - 1) * (2**refinement) + 1
 nny = old_div((nnx - 1), 10) + 1
-
 he = old_div(L[0], float(nnx - 1))
 triangleOptions = "pAq30Dena%f" % (0.5 * he**2,)
 
@@ -63,8 +62,8 @@ z = np.sqrt(old_div(3.0 * (h2 - h1), h2 * h1**2))
 
 def soliton(x, t):
     phase = x - D * t - x0
-    a1 = z * phase / 2.0
-    return h1 + (h2 - h1) * 1.0 / np.cosh(a1)**2
+    a1 = old_div(z * phase, 2.0)
+    return h1 + (h2 - h1) * old_div(1.0, np.cosh(a1)**2)
 
 
 def u(x, t):
@@ -77,33 +76,50 @@ def bathymetry_function(X):
     return x * 0.0  # just 0
 
 
+###################################
+#    FOR ANALYTICAL SOLUTIONS     #
+###################################
+class Zero(object):
+    def uOfXT(self, x, t):
+        return 0.0
+
+
+class water_height_at_tfinal(object):
+    def uOfXT(self, X, t):
+        return soliton(X[0], opts.final_time)
+
+
 ##############################
-##### INITIAL CONDITIONS #####
+#    INITIAL CONDITIONS      #
 ##############################
 
 
 class water_height_at_t0(object):
     def uOfXT(self, X, t):
-        return soliton(X[0], t)
+        return soliton(X[0], 0.0)
 
 
 class x_mom_at_t0(object):
     def uOfXT(self, X, t):
-        return soliton(X[0], t) * u(X[0], t)
+        return soliton(X[0], 0.0) * u(X[0], 0.0)
 
 
 class y_mom_at_t0(object):
     def uOfXT(self, X, t):
         h = water_height_at_t0().uOfXT(X, t)
         return 0.*h
+
+
 """
 heta and hw are needed for the dispersive modified green naghdi equations
 source is 'ROBUST EXPLICIT RELAXATION TECHNIQUE FOR SOLVING
 THE GREEN NAGHDI EQUATIONS' by Guermond, Kees, Popov, Tovar
 """
+
+
 class heta_at_t0(object):
     def uOfXT(self, X, t):
-        h = water_height_at_t0().uOfXT(X, t)
+        h = water_height_at_t0().uOfXT(X, 0.0)
         return h**2
 
 
@@ -114,14 +130,15 @@ class hw_at_t0(object):
         D = np.sqrt(g * h2)
         z = np.sqrt(old_div(3.0 * (h2 - h1), h2 * h1**2))
         phase = x - D * t - x0
-        a1 = z * phase / 2.0
-        sechSqd = 1.0 / np.cosh(a1)**2
+        a1 = old_div(z * phase, 2.0)
+        sechSqd = old_div(1.0, np.cosh(a1)**2)
         hprime = -2.0 * (h2 - h1) * z * sechSqd * np.tanh(phase)
         hw = -D * h1 * hprime
         return hw
 
+
 ###############################
-##### BOUNDARY CONDITIONS #####
+#     BOUNDARY CONDITIONS     #
 ###############################
 
 
@@ -157,7 +174,6 @@ def hw_DBC(X, flag):
         return lambda x, t: hw_at_t0().uOfXT(X, 0.0)
 
 
-
 # ********************************** #
 # ***** Create mySWFlowProblem ***** #
 # ********************************** #
@@ -175,6 +191,16 @@ boundaryConditions = {'water_height': water_height_DBC,
                       'y_mom': lambda x, flag: lambda x, t: 0.0,
                       'h_times_eta': heta_DBC,
                       'h_times_w': hw_DBC}
+if (opts.sw_model == 1):
+    analyticalSolution = {0: water_height_at_tfinal(),
+                          1: Zero(),
+                          2: Zero(),
+                          3: Zero(),
+                          4: Zero()}
+if (opts.sw_model == 0):
+    analyticalSolution = {0: water_height_at_tfinal(),
+                          1: Zero(),
+                          2: Zero()}
 mySWFlowProblem = SWFlowProblem.SWFlowProblem(sw_model=opts.sw_model,
                                               cfl=0.1,
                                               outputStepping=outputStepping,
@@ -185,6 +211,7 @@ mySWFlowProblem = SWFlowProblem.SWFlowProblem(sw_model=opts.sw_model,
                                               domain=domain,
                                               initialConditions=initialConditions,
                                               boundaryConditions=boundaryConditions,
-                                              bathymetry=bathymetry_function)
+                                              bathymetry=bathymetry_function,
+                                              analyticalSolution=analyticalSolution)
 mySWFlowProblem.physical_parameters['LINEAR_FRICTION'] = 0
 mySWFlowProblem.physical_parameters['mannings'] = 0.0
