918655cac0cdc4e08aaa6b85248438f9602e880b
Tristan de Lataillade <delataillade.tristan@gmail.com>
2019-02-15 01:58:02 +0000

    MBD: replace cython headers by links to SWIG Chrono pyEngine API (#845)
    * MBD: headers and SWIG links with dev version of Chrono
    
    * MBD: clean up of ChRigidBody.pyx
    
    * MBD: cleaner(?) way of handling SWIG objects
    
    * MBD: removed cppMesh; test updated; cleaning
    
    * MBD: drag/am bug fixed; link to pychrono nodes/elements
    
    * MBD: modified Aij for manual ext force handling
    
    * Update stack (#898)
    
    * update petsc functions for 3.10.2
    
    * fix 3D pcd error
    
    * update petsc 3.10 tracking branch for latest master
    
    * remove duplicate code
    
    * optimize pcd constructors
    
    * Adds a nonlinear iteration tracker for PCD
    
    * update stack and fix compilation error in ProtChMoorings.h
    
    * updating stack submodule commit
    
    * fix gmsh and added mass tests
    
    * fixed cache paths in makefile; updated amg test to petsc 3.10 api
    
    * update gdbm
    
    * fixed petsc API for getSubMatrix->CreateSubMatrix + stack+rdls
    
    * fix another petsc api change
    
    * updated elliptic redist test comparisons
    
    * updating virtual environment to 16.04
    
    * update swig and gmsh in stack
    
    * disabling MeshAdaptPUMI
    
    * turn off warnings in develop mode
    
    * strip duplicate and verbose default options--use PROTEUS_OPT
    
    * fix ChRigidBody syntax error
    
    * MBD: ChRigidBody -> CouplingFSI (mv)
    
    * setup.py modifications added

--- proteus/tests/test_mbd_chrono.py
+++ proteus/tests/test_mbd_chrono.py
@@ -1,26 +1,32 @@
 import numpy as np
 import numpy.testing as npt
 import unittest
-from proteus.mbd import ChRigidBody as crb
+from proteus.mbd import CouplingFSI as fsi
 import pytest
+import pychrono as chrono
 
 class TestCable(unittest.TestCase):
-    @pytest.mark.skip(reason="returning nans from getTensionBack")
     def testHangingCableANCF(self):
         g = np.array([0.,0.,-9.81])
-        system = crb.ProtChSystem(gravity=g)
+        system = fsi.ProtChSystem()
+        system.ChSystem.Set_G_acc(chrono.ChVectorD(g[0], g[1], g[2]))
         system.setTimeStep(1e-1)
-        mesh = crb.Mesh(system)
+        system.ChSystem.SetSolverType(chrono.ChSolver.Type_MINRES)
+        system.ChSystem.SetSolverWarmStarting(True)
+        system.ChSystem.SetMaxItersSolverSpeed(100)
+        system.ChSystem.SetMaxItersSolverStab(100)
+        system.ChSystem.SetTolForce(1e-10)
+        mesh = fsi.ProtChMesh(system)
         L = np.array([5.])
         nb_elems = np.array([3])
         d = np.array([1e-3])
         rho = np.array([1000.])
         E = np.array([1e10])
         cable_type = "CableANCF"
-        mooring = crb.ProtChMoorings(system=system, mesh=mesh, length=L, nb_elems=nb_elems, d=d, rho=rho, E=E, beam_type=cable_type)
+        mooring = fsi.ProtChMoorings(system=system, mesh=mesh, length=L, nb_elems=nb_elems, d=d, rho=rho, E=E, beam_type=cable_type)
         mooring.external_forces_manual = True # tri: should work without this line
         # vertical cable
-        fairlead_body = crb.ProtChBody(system)
+        fairlead_body = fsi.ProtChBody(system)
         fairlead_body.ChBody.SetBodyFixed(True)
         mooring.setNodesPositionFunction(lambda s: np.array([0., 0., s]), lambda s: np.array([0., 0., 1.]))
         mooring.setNodesPosition()
@@ -36,19 +42,19 @@ class TestCable(unittest.TestCase):
     # CURRENTLY NOT FULLY WORKING
     # def testHangingCableEuler(self):
     #     g = np.array([0.,0.,-9.81])
-    #     system = crb.ProtChSystem(gravity=g)
+    #     system = fsi.ProtChSystem(gravity=g)
     #     system.setTimeStep(1e-1)
-    #     mesh = crb.Mesh(system)
+    #     mesh = fsi.Mesh(system)
     #     L = np.array([5.])
     #     nb_elems = np.array([3])
     #     d = np.array([1e-3])
     #     rho = np.array([1000.])
     #     E = np.array([1e10])
     #     cable_type = "BeamEuler"
-    #     mooring = crb.ProtChMoorings(system=system, mesh=mesh, length=L, nb_elems=nb_elems, d=d, rho=rho, E=E, beam_type=cable_type)
+    #     mooring = fsi.ProtChMoorings(system=system, mesh=mesh, length=L, nb_elems=nb_elems, d=d, rho=rho, E=E, beam_type=cable_type)
     #     mooring.external_forces_manual = True # tri: should work without this line
     #     # vertical cable
-    #     fairlead_body = crb.ProtChBody(system)
+    #     fairlead_body = fsi.ProtChBody(system)
     #     fairlead_body.ChBody.SetBodyFixed(True)
     #     mooring.setNodesPositionFunction(lambda s: np.array([0., 0., s]), lambda s: np.array([0., 0., 1.]))
     #     mooring.setNodesPosition()
