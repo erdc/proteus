b907429dca611685f982fce19be4881b98df60d4
Tristan de Lataillade <delataillade.tristan@gmail.com>
2019-03-21 13:22:51 -0500

    Revert "remove import of forceTerms"
    This reverts commit bdb2f930982eec86c02e0640ad5e0fef3b38cc6b.

--- proteus/TwoPhaseFlow/models/rans3p_n.py
+++ proteus/TwoPhaseFlow/models/rans3p_n.py
@@ -8,6 +8,8 @@ from proteus import (StepControl,
 from proteus.mprans import RANS3PF
 import rans3p_p as physics
 
+forceTerms = physics.forceTerms
+
 # *********************************************** #
 # ********** Read from myTpFlowProblem ********** #
 # *********************************************** #
