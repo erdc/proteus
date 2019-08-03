d801f441e69d790a7129c4635cffca56eb6432f5
Chris Kees <cekees@gmail.com>
2019-08-03 18:32:33 -0500

    enabled meshadapt and a few hacks to test TwoPhaseFlowApp

--- proteus/tests/TwoPhaseFlow/test_TwoPhaseFlow.py
+++ proteus/tests/TwoPhaseFlow/test_TwoPhaseFlow.py
@@ -5,20 +5,21 @@ Test module for TwoPhaseFlow
 import pytest
 import tables
 import numpy as np
-import proteus.defaults
-from proteus import Context
-from proteus import default_so
-from proteus.iproteus import *
+#import proteus.defaults
+#from proteus import Context
+#from proteus import default_so
+#from proteus.iproteus import *
 import os
 import sys
-Profiling.logLevel=1
-Profiling.verbose=True
+#Profiling.logLevel=1
+#Profiling.verbose=True
 
 class TestTwoPhaseFlow(object):
 
     def setup_method(self,method):
         self._scriptdir = os.path.dirname(__file__)
-        self.path = proteus.__path__[0]+"/tests/TwoPhaseFlow/"
+        self.path = self._scriptdir
+        #self.path = proteus.__path__[0]+"/tests/TwoPhaseFlow/"
 
     def compare_vs_saved_files(self,name):
         expected_path = 'comparison_files/' + name + '.h5'
@@ -31,39 +32,39 @@ class TestTwoPhaseFlow(object):
 
     # *** 2D tests *** #
     def test_risingBubble(self):
-        os.system("parun --TwoPhaseFlow --path " + self.path + " "
+        os.system("aprun -n 1 parun --TwoPhaseFlow --path " + self.path + " "
                   "risingBubble.py -l5 -v -C 'final_time=0.1 dt_output=0.1 refinement=1'")
         self.compare_vs_saved_files("risingBubble")
 
     def test_damBreak(self):
-        os.system("parun --TwoPhaseFlow --path " + self.path + " "
+        os.system("aprun -n 1 parun --TwoPhaseFlow --path " + self.path + " "
                   "damBreak.py -l5 -v -C 'final_time=0.1 dt_output=0.1 he=0.1'")
         self.compare_vs_saved_files("damBreak")
 
     def test_damBreak_solver_options(self):
-        os.system("parun --TwoPhaseFlow --path " + self.path + " "
+        os.system("aprun -n 1 parun --TwoPhaseFlow --path " + self.path + " "
                   "damBreak_solver_options.py -l5 -v -C 'final_time=0.1 dt_output=0.1 he=0.05'")
         self.compare_vs_saved_files("damBreak_solver_options")
 
 #    @pytest.mark.skip(reason="long test")
     def test_TwoDimBucklingFlow(self):
-        os.system("parun --TwoPhaseFlow --path " + self.path + " "
+        os.system("aprun -n 1 parun --TwoPhaseFlow --path " + self.path + " "
                   "TwoDimBucklingFlow.py -l5 -v -C 'final_time=0.1 dt_output=0.1 he=0.05'")
         self.compare_vs_saved_files("TwoDimBucklingFlow")
 
 #    @pytest.mark.skip(reason="long test")
     def test_fillingTank(self):
-        os.system("parun --TwoPhaseFlow --path " + self.path + " "
+        os.system("aprun -n 1 parun --TwoPhaseFlow --path " + self.path + " "
                   "fillingTank.py -l5 -v -C 'final_time=0.1 dt_output=0.1 he=0.01'")
         self.compare_vs_saved_files("fillingTank")
 
     # *** 3D tests *** #
     def test_marin(self):
-        os.system("parun --TwoPhaseFlow --path " + self.path + " "
+        os.system("aprun -n 1 parun --TwoPhaseFlow --path " + self.path + " "
                   "marin.py -l5 -v -C 'final_time=0.1 dt_output=0.1 he=0.5'")
         self.compare_vs_saved_files("marin")
 
     def test_moses(self):
-        os.system("parun --TwoPhaseFlow --path " + self.path + " "
+        os.system("aprun -n 1 parun --TwoPhaseFlow --path " + self.path + " "
                   "moses.py -l5 -v -C 'final_time=0.1 dt_output=0.1 he=0.5'")
         self.compare_vs_saved_files("moses")
