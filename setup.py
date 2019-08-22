5d94d4307bc9ff5e960fc7f7ce96d9374630b112
Chris Kees <cekees@gmail.com>
2019-08-22 17:21:08 -0500

    fixed conflict

--- setup.py
+++ setup.py
@@ -550,8 +550,6 @@ EXTENSIONS_TO_BUILD = [
                   "proteus/equivalent_polynomials_utils.h",
                   "proteus/equivalent_polynomials_coefficients.h"],
               extra_compile_args=PROTEUS_OPT+["-std=c++11"],
-<<<<<<< HEAD
-=======
               language="c++", include_dirs=[numpy.get_include(), 'proteus']),
     Extension("mprans.cRANS2P_IB",["proteus/mprans/cRANS2P_IB.pyx"],
               depends=["proteus/mprans/RANS2P_IB.h"] + ["proteus/MixedModelFactory.h","proteus/CompKernel.h"] + [
@@ -559,7 +557,6 @@ EXTENSIONS_TO_BUILD = [
                   "proteus/equivalent_polynomials_utils.h",
                   "proteus/equivalent_polynomials_coefficients.h"],
               extra_compile_args=PROTEUS_OPT+["-std=c++11"],
->>>>>>> 1.7.x
               language="c++", include_dirs=[numpy.get_include(), 'proteus']),
     Extension("mprans.cRANS2P2D",["proteus/mprans/cRANS2P2D.pyx"],
               depends=["proteus/mprans/RANS2P2D.h"] + ["proteus/MixedModelFactory.h","proteus/CompKernel.h"]+ [
