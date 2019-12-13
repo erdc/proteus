c4e99622037ff0d3665a189a7b80190b450058ba
eric <ejtovar@math.tamu.edu>
2019-12-13 15:54:28 -0800

    forgot to remove some things in the test_SWFlows.py file

--- proteus/tests/SWFlow/test_SWFlows.py
+++ proteus/tests/SWFlow/test_SWFlows.py
@@ -33,41 +33,17 @@ class TestSWFlow(object):
         os.system("parun --SWEs -l1 -v solitary_wave.py -C 'refinement=4 final_time=0.1'")
         self.compare_vs_saved_files("solitary_wave")
 
-        # COMPARE VS SAVED FILES #
-        expected_path = 'comparison_files/solitary.h5'
-        expected = tables.open_file(os.path.join(self._scriptdir,expected_path))
-        actual = tables.open_file('solitary.h5','r')
-        assert np.allclose(expected.root.h_t3,actual.root.h_t3,atol=1e-10)
-        expected.close()
-        actual.close()
-
     def test_parab1D(self):
         # Call runSWEs
         os.system("parun --SWEs parab1D.py".format(self._scriptdir))
         os.system("parun --SWEs -l1 -v parab1D.py -C 'refinement=4 final_time=0.1'")
         self.compare_vs_saved_files("parab1D")
 
-        # COMPARE VS SAVED FILES #
-        expected_path = 'comparison_files/parab1D.h5'
-        expected = tables.open_file(os.path.join(self._scriptdir,expected_path))
-        actual = tables.open_file('parab1D.h5','r')
-        assert np.allclose(expected.root.h_t11,actual.root.h_t11,atol=1e-10)
-        expected.close()
-        actual.close()
-
     def test_dam3Bumps(self):
         # Call runSWEs
         os.system("PYTHONPATH={0} parun -v  --SWEs dam3Bumps.py".format(self._scriptdir))
         os.system("parun --SWEs -l1 -v dam3Bumps.py -C 'refinement=4 final_time=0.1'")
         self.compare_vs_saved_files("dam3Bumps")
-
-        # COMPARE VS SAVED FILES #
-        expected_path = 'comparison_files/dam3Bumps.h5'
-        expected = tables.open_file(os.path.join(self._scriptdir,expected_path))
-        actual = tables.open_file('dam3Bumps.h5','r')
-        assert np.allclose(expected.root.h_t4,actual.root.h_t4,atol=1e-10)
-        expected.close()
-        actual.close()
 
     def test_GN_steady(self):
         os.system("parun --SWEs -l1 -v GN_steady.py -C 'refinement=4 final_time=0.1'")
