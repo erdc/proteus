97ed1a8f357c7e4b69187dba1179ee04793240f7
eric <ejtovar@math.tamu.edu>
2019-12-18 21:51:33 -0600

    FOrgot to add path to tests, I think this should do it?

--- proteus/tests/SWFlow/test_SWFlows.py
+++ proteus/tests/SWFlow/test_SWFlows.py
@@ -30,27 +30,33 @@ class TestSWFlow(object):
 
     def test_solitary_wave(self):
         # call runSWEs
-        os.system("parun --SWEs -l1 -v solitary_wave.py -C 'refinement=4 final_time=0.1'")
+        os.system("parun --SWEs --path " + self.path + " "
+                  "-l1 -v solitary_wave.py -C 'refinement=4 final_time=0.1'")
         self.compare_vs_saved_files("solitary_wave")
 
     def test_parab1D(self):
         # Call runSWEs
-        os.system("parun --SWEs -l1 -v parab1D.py -C 'refinement=4 final_time=10.0'")
+        os.system("parun --SWEs --path " + self.path + " "
+                  "-l1 -v parab1D.py -C 'refinement=4 final_time=10.0'")
         self.compare_vs_saved_files("parab1D")
 
     def test_dam3Bumps(self):
         # Call runSWEs
-        os.system("parun --SWEs -l1 -v dam3Bumps.py -C 'refinement=4 final_time=0.1'")
+        os.system("parun --SWEs --path " + self.path + " "
+                  "-l1 -v dam3Bumps.py -C 'refinement=4 final_time=0.1'")
         self.compare_vs_saved_files("dam3Bumps")
 
     def test_GN_steady(self):
-        os.system("parun --SWEs -l1 -v GN_steady.py -C 'refinement=4 final_time=0.1'")
+        os.system("parun --SWEs --path " + self.path + " "
+                  "-l1 -v GN_steady.py -C 'refinement=4 final_time=0.1'")
         self.compare_vs_saved_files("GN_steady")
 
     def test_solitary_reef(self):
-        os.system("parun --SWEs -l1 -v solitary_reef.py -C 'refinement=4 final_time=0.1'")
+        os.system("parun --SWEs --path " + self.path + " "
+                  "-l1 -v solitary_reef.py -C 'refinement=4 final_time=0.1'")
         self.compare_vs_saved_files("solitary_reef")
 
     def test_seawall(self):
-        os.system("parun --SWEs -l1 -v seawall.py -C 'refinement=4 final_time=0.1'")
+        os.system("parun --SWEs --path " + self.path + " "
+                  "-l1 -v seawall.py -C 'refinement=4 final_time=0.1'")
         self.compare_vs_saved_files("seawall")
