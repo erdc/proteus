6737460563482b2b27ddae16e4258011d9928d99
David Brochart <david.brochart@gmail.com>
2020-01-16 11:51:16 +0100

    Travis build for conda and hashdist

---
+++ install-hashdist.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+git lfs pull
+./stack/hit/bin/hit init-home
+./stack/hit/bin/hit remote add http://levant.hrwallingford.com/hashdist_src --objects="source"
+./stack/hit/bin/hit remote add http://levant.hrwallingford.com/hashdist_ubuntu_16_04 --objects="build"
+make stack/default.yaml
+pushd $HOME
+mkdir -p hashdist_src
+mkdir -p hashdist_bld
+rm -rf .hashdist/src .hashdist/bld
+ln -s $HOME/hashdist_src .hashdist/src
+ln -s $HOME/hashdist_bld .hashdist/bld
+popd
+pushd stack
+echo $PWD
+ls -l
+./hit/bin/hit build -j 2 -v default.yaml
+popd
+export PATHSAVE=$PATH
+export PATH=$PWD/linux/bin:$PATH
+export LD_LIBRARY_PATH=$PWD/linux/lib:$LD_LIBRARY_PATH
+PROTEUS_OPT="-w -O2 -UNDEBUG" FC=gfortran CC=mpicc CXX=mpicxx make develop N=2
+export SSL_CERT_DIR=/etc/ssl/certs
+#./linux/bin/pip3 install matplotlib
