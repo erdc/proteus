e217e84cff6d592cd858b7746d940bb43a75c682
David Brochart <david.brochart@gmail.com>
2020-01-27 18:04:39 +0100

    SW2DCV

--- proteus/mprans/SW2DCV.py
+++ proteus/mprans/SW2DCV.py
@@ -5,6 +5,10 @@ from builtins import range
 from past.utils import old_div
 import proteus
 from proteus.mprans.cSW2DCV import *
+import numpy
+from proteus import *
+from proteus.Transport import *
+from proteus.Transport import OneLevelTransport
 
 class NumericalFlux(proteus.NumericalFlux.ShallowWater_2D):
     hasInterior = False
@@ -1387,7 +1391,7 @@ class LevelModel(proteus.Transport.OneLevelTransport):
             self.csrRowIndeces[(2, 0)], self.csrColumnOffsets[(2, 0)],
             self.csrRowIndeces[(2, 1)], self.csrColumnOffsets[(2, 1)],
             self.csrRowIndeces[(2, 2)], self.csrColumnOffsets[(2, 2)],
-            jacobian,
+            jacobian.getCSRrepresentation()[2],
             self.mesh.nExteriorElementBoundaries_global,
             self.mesh.exteriorElementBoundariesArray,
             self.mesh.elementBoundaryElementsArray,
