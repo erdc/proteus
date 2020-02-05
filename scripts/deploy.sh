a77e468583de3d4ed036781555330a6472bec530
Tristan de Lataillade <delataillade.tristan@gmail.com>
2020-02-05 16:40:57 -0500

    Test Travis deploy to external repo

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ "$1" == "docs" ]; then
+    make docs
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis CI"
+    git clone https://erdc:${GH_TOKEN}@github.com/proteus-docs.git ./proteus-docs > /dev/null 2>&1
+    cd ./proteus-docs
+    git checkout gh-pages-test
+    cp -r ./docs/build .
+    git add . *
+    git commit -m "Travis automatic docs build: $TRAVIS_BUILD_NUMBER"
+    git push origin gh-pages-test > /dev/null 2>&1
+    cd ..
+fi
