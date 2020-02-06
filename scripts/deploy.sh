a43f677669f6a803e7f554fc3d769f224f53844a
Tristan de Lataillade <delataillade.tristan@gmail.com>
2020-02-05 23:41:28 -0500

    Add some safety check

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 if [ "$1" == "docs" ]; then
-    make docs
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
-    git clone https://${GH_TOKEN}@github.com/erdc/proteus-docs.git ./proteus-docs > /dev/null 2>&1
-    cd ./proteus-docs
-    git checkout gh-pages-test
-    cp -r ../docs/build/* .
-    git add . *
-    git commit -m "Travis automatic docs build: $TRAVIS_BUILD_NUMBER"
-    git push origin gh-pages-test > /dev/null 2>&1
-    cd ..
+    git clone --branch gh-pages-test https://${GH_TOKEN}@github.com/erdc/proteus-docs.git ./docs/build > /dev/null 2>&1
+    if [[ -d ./docs/build ]]
+    then
+      make docs
+      cd ./docs/build
+      git add . *
+      git commit -m "Travis automatic docs build: $TRAVIS_BUILD_NUMBER"
+      git push origin gh-pages-test > /dev/null 2>&1
+      cd ../..
+    else
+      echo "Error: Travis could not clone docs repository"
+      exit 1
+    fi
 fi
