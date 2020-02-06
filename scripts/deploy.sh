26cde3b9e17859f9c00a509f1140b1880acabfe9
Travis CI <travis@travis-ci.org>
2020-02-05 20:23:14 -0500

    Test Travis after_success 3 [WIP]

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ if [ "$1" == "docs" ]; then
     git clone https://${GH_TOKEN}@github.com/erdc/proteus-docs.git ./proteus-docs > /dev/null 2>&1
     cd ./proteus-docs
     git checkout gh-pages-test
-    cp -r ./docs/build .
+    cp -r ../docs/build .
     git add . *
     git commit -m "Travis automatic docs build: $TRAVIS_BUILD_NUMBER"
     git push origin gh-pages-test > /dev/null 2>&1
