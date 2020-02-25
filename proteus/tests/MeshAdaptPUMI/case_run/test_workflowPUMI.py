9d54d326dc14d74a49c13f19e6c38693e639d3b8
zhang-alvin <alv.zhang@gmail.com>
2020-02-24 23:05:45 -0500

    TST/FIX: add absolute path to test to fix conda test failure

--- proteus/tests/MeshAdaptPUMI/case_run/test_workflowPUMI.py
+++ proteus/tests/MeshAdaptPUMI/case_run/test_workflowPUMI.py
@@ -7,7 +7,9 @@ def test_workflowPUMI(verbose=0):
     """Test serial workflow: load model and mesh, solve within proteus 
     estimate error, adapt, solve again. It's not so important if the
     problem isn't setup properly"""
-    subprocess.check_call("parun -l5 couette_so.py",shell=True )
+    currentPath = os.path.dirname(os.path.abspath(__file__))
+    runCommand = "cd "+currentPath+"; parun -l5 couette_so.py;"
+    subprocess.check_call(runCommand,shell=True )
 if __name__ == '__main__':
     import nose
     nose.main(defaultTest='test_workflowPUMI:test_workflowPUMI')
