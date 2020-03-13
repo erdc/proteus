0022c444003cc535773d9e2e7440e5119cc8922b
Tristan de Lataillade <delataillade.tristan@gmail.com>
2020-03-13 14:00:38 -0400

    Fix MoveMesh for 3D cases + add 3D FSI test (#1113)
    * MoveMesh: import cMoveMesh_base for 3D
    
    * TST: add FSI test for floating cuboid

--- proteus/tests/FSI/floatingCube.py
+++ proteus/tests/FSI/floatingCube.py
@@ -13,8 +13,8 @@ nu_0 = 1.004e-6
 rho_1 = 1.205
 nu_1 = 1.5e-5
 sigma_01 = 0.
-he = 0.05
-tank_dim = [1., 1.]
+he = 0.2
+tank_dim = [1., 1., 1.]
 water_level = 0.5
 genMesh = False
 rhor = 0.5
@@ -28,25 +28,20 @@ rhor = 0.5
 # Domain
 # All geometrical options go here (but not mesh options)
 
-domain = Domain.PlanarStraightLineGraphDomain()
+domain = Domain.PiecewiseLinearComplexDomain()
 
 # ----- SHAPES ----- #
 
 # TANK
-tank = st.Tank2D(domain, tank_dim)
+tank = st.Tank3D(domain, tank_dim)
 
 # CAISSON
 radius = 0.1
-#caisson = st.Circle(domain,
-#                    radius=radius,
-#                    coords=(tank_dim[0]/2., water_level+radius-2.*radius*rhor+radius/100.),
-#                    barycenter=(tank_dim[0]/2., water_level+radius-2.*radius*rhor+radius/100.),
-#                    nPoints=int(np.pi*radius/he))
-caisson = st.Rectangle(domain,
-                       dim=[2*radius, 2*radius],
-                       coords=(tank_dim[0]/2., water_level+radius/10.),
-                       barycenter=(tank_dim[0]/2., water_level+radius/10.))
-caisson.setHoles([caisson.barycenter[:2]])
+caisson = st.Cuboid(domain,
+                    dim=[2*radius, 2*radius, 2*radius],
+                    coords=(tank_dim[0]/2., tank_dim[1]/2., water_level+radius/10.),
+                    barycenter=(tank_dim[0]/2., tank_dim[1]/2., water_level+radius/10.))
+caisson.setHoles([caisson.barycenter])
 caisson.holes_ind = np.array([0])
 # let gmsh know that the caisson is IN the tank
 tank.setChildShape(caisson, 0)
@@ -60,20 +55,19 @@ tank.setChildShape(caisson, 0)
 #                                           |___/
 # Boundary Conditions
 
-tank.BC['y+'].setAtmosphere()
+tank.BC['z+'].setAtmosphere()
+tank.BC['z-'].setFreeSlip()
+tank.BC['y+'].setFreeSlip()
 tank.BC['y-'].setFreeSlip()
 tank.BC['x+'].setFreeSlip()
 tank.BC['x-'].setFreeSlip()
 tank.BC['sponge'].setNonMaterial()
 
-tank.BC['x-'].setFixedNodes()
-tank.BC['x+'].setFixedNodes()
-tank.BC['sponge'].setFixedNodes()
-tank.BC['y+'].setFixedNodes()  # sliding mesh nodes
-tank.BC['y-'].setFixedNodes()  #sliding mesh nodes
-
-for bc in caisson.BC_list:
+for tag, bc in caisson.BC.items():
     bc.setNoSlip()
+
+for tag, bc in tank.BC.items():
+    bc.setFixedNodes()
 
 
 #  ___       _ _   _       _    ____                _ _ _   _
@@ -130,7 +124,7 @@ initialConditions = {'pressure': P_IC(),
 # Chrono
 
 # System
-g = np.array([0., -9.81, 0.])
+g = np.array([0., 0., -9.81])
 system = fsi.ProtChSystem()
 system.ChSystem.Set_G_acc(pychrono.ChVectorD(g[0], g[1], g[2]))
 system.setTimeStep(1e-5)
@@ -142,13 +136,13 @@ body.attachShape(caisson)
 chbod = body.ChBody
 x, y, z = caisson.barycenter
 pos = pychrono.ChVectorD(x, y, z)
-mass = (2.*radius)**2*rho_0*rhor
+mass = (2.*radius)**3*rho_0*rhor
 inertia = pychrono.ChVectorD(1., 1., 1.)
 chbod.SetPos(pos)
 chbod.SetMass(mass)
 chbod.SetInertiaXX(inertia)
 #chbod.SetBodyFixed(True)
-body.setConstraints(free_x=np.array([0.,1.,0.]), free_r=np.array([0.,0.,0.]))
+body.setConstraints(free_x=np.array([1.,1.,1.]), free_r=np.array([1.,1.,1.]))
 
 # body.setInitialRot(rotation_init)
 # body.rotation_init=np.array([np.cos(ang/2.), 0., 0., np.sin(ang/2.)*1.])
@@ -167,12 +161,11 @@ domain.MeshOptions.genMesh = genMesh
 he = he
 domain.MeshOptions.he = he
 modulepath = os.path.dirname(os.path.abspath(__file__))
-mesh_fileprefix=modulepath+'/meshFloatingCylinder'
+mesh_fileprefix=modulepath+'/meshFloatingCube'
 domain.MeshOptions.setOutputFiles(mesh_fileprefix)
 st.assembleDomain(domain)
 domain.use_gmsh = False
 domain.geofile = mesh_fileprefix
-
 
 
 #  _   _                           _
