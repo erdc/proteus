709e44977b1213c84c87120ade8c135d3df4bfb7
ejtovar <36754207+ejtovar@users.noreply.github.com>
2020-03-16 17:29:30 -0500

    Minor cleanup changes. (#1114)
    * Saving the setup file with the pychrono dependencies commented out
    
    * Added the solitary island test, some description udpates to other tests. Added the new rescaling for the EV but need to keep checking that it is indeed robust. Need to add the rescaling to SW2DCV.h
    
    * Cleaned and added SWFlow tests. Deleted /proteus/SWFlows/ directory and updated README.md in /proteus/SWFlow/
    
    * Saving the setup file with the pychrono dependencies commented out
    
    * Added the solitary island test, some description udpates to other tests. Added the new rescaling for the EV but need to keep checking that it is indeed robust. Need to add the rescaling to SW2DCV.h
    
    * Cleaned and added SWFlow tests. Deleted /proteus/SWFlows/ directory and updated README.md in /proteus/SWFlow/
    
    * Changed setup.py back to master setup
    
    * Cleaned up test files with modified descriptions and re-wrote some functions to look nicer. Added solitary_island.py test and associated comparison files. Also removed /proteus/SWFlow/tests directory because it was not needed
    
    * Changed version 1.7.3 to 1.7.3.dev0

--- proteus/tests/SWFlow/solitary_island.py
+++ proteus/tests/SWFlow/solitary_island.py
@@ -1,22 +1,23 @@
 from __future__ import division
 from builtins import object
 from past.utils import old_div
-from proteus import *
-from proteus.default_p import *
-from proteus.mprans import SW2DCV
-from proteus.mprans import GN_SW2DCV
+from proteus.mprans import (SW2DCV, GN_SW2DCV)
 from proteus.Domain import RectangularDomain
-import numpy as np
 from proteus import (Domain, Context,
                      MeshTools as mt)
 from proteus.Profiling import logEvent
 from proteus.Gauges import PointGauges
-import proteus.SWFlows.SWFlowProblem as SWFlowProblem
+import numpy as np
+import proteus.SWFlow.SWFlowProblem as SWFlowProblem
+
 
 """
-This is the problem of a solitary wave overtopping a conical island.
-This experiment was done at (insert ref here). There were several gauges
-places in the tank measuring the water height (location can be see in bottom)
+We consider the 1995 laboratory experiments conducted by
+[Briggs, 1995] at the US Army Waterways Experiment Station in
+Vicksburg, Mississippi. The laboratory experiments were motivated by
+several tsunami events in the 1990s where large unexpected run-up
+heights were observed on the back (or lee) side of small
+islands.
 """
 
 # *************************** #
@@ -34,9 +35,9 @@ opts = Context.Options([
 ###################
 # DOMAIN AND MESH #
 ###################
-L = (48.8, 26.5)  # this is length in x direction and y direction
+L = (25.0, 30.0)  # this is length in x direction and y direction
 refinement = opts.refinement
-domain = RectangularDomain(L=L, x=[0, -13.25, 0])
+domain = RectangularDomain(L=L)
 
 # CREATE REFINEMENT #
 nnx0 = 6
@@ -51,100 +52,40 @@ triangleOptions = "pAq30Dena%f" % (0.5 * he**2,)
 g = 9.81
 
 # stuff for solitary wave
-h0 = 0.78
-alpha = 0.4  # 0.5 * h0
-xs = 5.0
+h0 = 0.32
+alpha = 0.181 * h0
+xs = 7.56
 r = np.sqrt(old_div(3. * alpha, (4. * h0**2 * (h0 + alpha))))
 c = np.sqrt(g * (h0 + alpha))
 
-# stuff for bathymetry, including shelf and cone
-rcone = 3.
-hcone = 0.45
-yc = 13.25
+# stuff for cone bathymetry
+htop = 0.625
+rcone = 3.6
+scone = 4.0
+hcone = 0.9
 
 
 #####################################
 #   Some functions defined here    #
 ####################################
-
 def solitary_wave(x, t):
-    sechSqd = (1.00 / np.cosh(r * (x - xs - c * t)))**2.0
+    sechSqd = (1.0 / np.cosh(r * (x - xs)))**2
     return alpha * sechSqd
 
 
 def bathymetry_function(X):
     x = X[0]
-    y = X[1] + yc
+    y = X[1]
+    radius = np.sqrt((x - 12.96)**2 + (y - 13.80)**2)
 
-    # first define cone topography
-    cone = np.maximum(
-        hcone - np.sqrt(((x - 17.0)**2 + (y - yc)**2) / (rcone / hcone)**2), 0.0)
+    conds = [radius <= rcone, radius > rcone]
+    bath = [lambda radius: np.minimum(
+        htop, hcone - radius / scone), lambda radius: 0.0]
+    return np.piecewise(radius, conds, bath)
 
-    # initialize base with shape of x array
-    base = 0. * x
-
-    # silly hack because X switches from list to array of
-    # length 3 (x,y,z) when called in initial conditions
-    if (isinstance(X, list)):
-        for i, value in enumerate(X[0]):
-            if value < 10.2:
-                base[i] = 0.
-            if ((10.2 <= value) & (value  <= 17.5)):
-                base[i] = (0.5 - 0.0) / (17.5 - 10.20) * (value - 10.2)
-            if ((17.5 <= value) & (value <= 32.5)):
-                base[i] = 1.0 + (1.0- 0.5) / \
-                    (32.5 - 17.5) * (value - 32.5)
-            if (32.5 < value):
-                base[i] = 1.
-    else:
-            if x < 10.2:
-                base = 0.0
-            if ((10.2 < x) & (x <= 17.5)):
-                base = (0.5 - 0.0) / (17.5 - 10.20) * (x - 10.2)
-            if ((17.5 <= x) & (x <= 32.5)):
-                base = 1.0 + (1.0 - 0.5) / \
-                    (32.5 - 17.5) * (x - 32.5)
-            if (32.5 < x):
-                base = 1.
-
-    # define stuff for shelf
-    shelf = 0. * x
-    dist = 1.0 - np.minimum(1.0, np.abs(y - yc) / yc)
-    aux_x = 12.50 + 12.4999 * (1.0 - dist)
-    aux_z = 0.70 + 0.050 * (1.0 - dist)
-
-    if (isinstance(X, list)):
-        for i, value in enumerate(X[0]):
-            if value < 10.2:
-                shelf[i] = 0.
-            if ((10.2 <= value) & (value  <= aux_x[i])):
-                shelf[i] = aux_z[i] / (aux_x[i] - 10.20) * (value - 10.2)
-            if ((aux_x[i] <= value) & (value <= 25.)):
-                shelf[i] = 0.75 + (aux_z[i] - 0.75) / \
-                    (aux_x[i] - 25.) * (value - 25.)
-            if ((25. < value) & (value <= 32.5)):
-                shelf[i] = 1. + (1. - 0.5) / (32.5 - 17.5) * (value - 32.5)
-            if (32.5 < value):
-                shelf[i] = 1.
-    else:
-            if x < 10.2:
-                shelf = 0.0
-            if ((10.2 < x) & (x <= aux_x)):
-                shelf = aux_z / (aux_x - 10.20) * (x - 10.2)
-            if ((aux_x <= x) & (x <= 25.)):
-                shelf = 0.75 + (aux_z - 0.75) / \
-                    (aux_x - 25.) * (x - 25.)
-            if ((25. < x) & (x <= 32.5)):
-                shelf = 1. + (1. - 0.5) / (32.5 - 17.5) * (x - 32.5)
-            if (32.5 < x):
-                shelf = 1.
-
-    bath = np.maximum(base, shelf) + cone
-    return bath
-
-######################
-# INITIAL CONDITIONS #
-######################
+##############################
+##### INITIAL CONDITIONS #####
+##############################
 
 
 class water_height_at_t0(object):
@@ -158,7 +99,7 @@ class x_mom_at_t0(object):
     def uOfXT(self, X, t):
         hTilde = h0 + solitary_wave(X[0], 0)
         h = max(hTilde - bathymetry_function(X), 0.)
-        return h * c * old_div(hTilde-h0, hTilde)
+        return h * c * old_div(hTilde - h0, hTilde)
 
 
 class y_mom_at_t0(object):
@@ -177,15 +118,16 @@ class hw_at_t0(object):
         sechSqd = (1.0 / np.cosh(r * (X[0] - xs)))**2.0
         hTilde = h0 + solitary_wave(X[0], 0)
         h = max(hTilde - bathymetry_function(X), 0.)
-        hTildePrime = -2.0 * alpha * r * np.tanh(r*(X[0]-xs)) * sechSqd
+        hTildePrime = -2.0 * alpha * r * np.tanh(r * (X[0] - xs)) * sechSqd
         hw = -h**2 * old_div(c * h0 * hTildePrime, hTilde**2)
         return hw
+
 
 ###############################
 ##### BOUNDARY CONDITIONS #####
 ###############################
-X_coords = (0.0, 48.8)  # this is x domain, used in BCs
-Y_coords = (-13.25, 13.25)  # this is y domain, used in BCs
+X_coords = (0.0, L[0])  # this is x domain, used in BCs
+Y_coords = (0.0, L[1])  # this is y domain, used in BCs
 
 
 def x_mom_DBC(X, flag):
@@ -216,19 +158,17 @@ boundaryConditions = {'water_height': lambda x, flag: None,
 # **************************** #
 # ********** GAUGES ********** #
 # **************************** #
-want_gauges = True
-heightPointGauges = PointGauges(gauges=((('h'), ((7.5, 0.0,  0),
-                                 (13.0, 0.0, 0),
-                                 (21.0, 0.0, 0),
-                                 (7.5, 5.0, 0),
-                                 (13.0, 5.0, 0),
-                                 (21.0, 5.0, 0),
-                                 (25.0, 0.0, 0),
-                                 (25.0, 5.0, 0),
-                                 (25.0, 10.0, 0))),),
-                activeTime=(0., opts.final_time),
-                fileName='wave_gauges.csv')
-
+want_gauges = False
+heightPointGauges = PointGauges(gauges=((('h',), ((7.56, 16.05,  0),
+                                                 (7.56, 14.55, 0),
+                                                 (7.56, 13.05, 0),
+                                                 (7.56, 11.55, 0),
+                                                 (9.36, 13.80, 0),
+                                                 (10.36, 13.80, 0),
+                                                 (12.96, 11.22, 0),
+                                                 (15.56, 13.80, 0))),),
+                                activeTime=(0., opts.final_time),
+                                fileName='island_wave_gauges.csv')
 # ********************************************* #
 # ********** Create my SWFlowProblem ********** #
 # ********************************************* #
