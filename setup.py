2eb6ce152f1451c1062966d9faa20f407cf16965
Chris Kees <cekees@gmail.com>
2020-05-29 13:42:33 -0500

    Release 1.7.4 (#1200)

--- setup.py
+++ setup.py
@@ -742,7 +742,7 @@ EXTENSIONS_TO_BUILD = [
 
 def setup_given_extensions(extensions):
     setup(name='proteus',
-          version='1.7.3.dev0',
+          version='1.7.4',
           classifiers=[
               'Development Status :: 4 - Beta',
               'Environment :: Console',
