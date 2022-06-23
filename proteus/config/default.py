ad6576507a6e632208ffcc89e018040c26080f55
cekees <613654+cekees@users.noreply.github.com>
2022-06-23 07:58:39 -0500

    try to get darwin build to use conda-installed openblas

--- proteus/config/default.py
+++ proteus/config/default.py
@@ -23,11 +23,10 @@ platform_blas_h = None
 platform_lapack_h = None
 platform_lapack_integer = None
 if sys.platform == 'darwin':
-    platform_extra_link_args = ['-framework', 'Accelerate']
-    platform_lapack_integer = '__CLPK_integer'
-    platform_blas_h = r'<Accelerate/Accelerate.h>'
-    platform_lapack_h = r'<Accelerate/Accelerate.h>'
-    platform_extra_compile_args = ['-flax-vector-conversions','-DPETSC_INCLUDE_AS_C','-DPETSC_SKIP_COMPLEX']
+    platform_extra_compile_args = ['-DPETSC_INCLUDE_AS_C', '-DPETSC_SKIP_COMPLEX']
+    platform_extra_link_args = ['-L'+PROTEUS_LIB_DIR,'-Wl,-rpath,' + PROTEUS_LIB_DIR]
+    platform_blas_h = r'"proteus_blas.h"'
+    platform_lapack_h = r'"proteus_lapack.h"'
     major,minor = platform.mac_ver()[0].split('.')[0:2]
     os.environ["MACOSX_DEPLOYMENT_TARGET"]= major+'.'+minor
 elif sys.platform.startswith('linux'):
