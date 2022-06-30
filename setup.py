bf2bf666000b9c4ae16d6f1d1191e9766bb200ad
cekees <613654+cekees@users.noreply.github.com>
2022-06-30 09:53:27 -0600

    Set up 1.8.1 release

--- setup.py
+++ setup.py
@@ -760,7 +760,7 @@ EXTENSIONS_TO_BUILD = [
 
 def setup_given_extensions(extensions):
     setup(name='proteus',
-          version='1.8.0.dev0',
+          version='1.8.1',
           classifiers=[
               'Development Status :: 4 - Beta',
               'Environment :: Console',
