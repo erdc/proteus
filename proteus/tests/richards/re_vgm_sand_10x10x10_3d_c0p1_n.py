993e8b38a3f850da643ee1b92977214a83441906
cekees <613654+cekees@users.noreply.github.com>
2023-04-13 17:15:08 -0500

    add sand examples with fct

--- proteus/tests/richards/re_vgm_sand_10x10x10_3d_c0p1_n.py
+++ proteus/tests/richards/re_vgm_sand_10x10x10_3d_c0p1_n.py
@@ -2,36 +2,47 @@ from proteus import *
 from proteus.default_n import *
 from re_vgm_sand_10x10x10_3d_p import *
 
+
 #timeIntegration = BackwardEuler
+timeIntegrator = ForwardIntegrator
 #timeIntegration = FLCBDF
-#stepController = FLCBDF_controller
+#stepController  = FLCBDF_controller
 #systemStepControllerType = SplitOperator.Sequential_MinFLCBDFModelStep
 rtol_u[0] = 1.0e-3
 atol_u[0] = 1.0e-3
 timeIntegration = BackwardEuler
 stepController = HeuristicNL_dt_controller#FixedStep
+#stepController = FixedStep
 #nDTout = 1#int(T/DT)#int(T/DT) #100#int(T/DT)
 #for controlling time stepping
-nonlinearIterationsFloor = 4
-nonlinearIterationsCeil  = 8
+if not galerkin:
+    timeIntegration = Richards.ThetaScheme
+timeOrder = 1
+#stepController = Min_dt_controller
+nonlinearIterationsFloor = 6
+nonlinearIterationsCeil  = 12
 dtNLgrowFactor = 2
 dtNLreduceFactor = 0.5
 dtNLfailureReduceFactor = 0.5
 useInitialGuessPredictor= True
 stepExact = True
-nDTout = 100
+nDTout = 200
 DT = T/nDTout 
-tnList = [0.0,min(1.0e-5,DT/2.0)]+[i*DT for i  in range(1,nDTout+1)]
+tnList = [0.0,min(1.0e-6,DT/2.0)]+[i*DT for i  in range(1,nDTout+1)]
+
+#DT = None#0.025#1.0e-1/timeScale
+#nDTout = 100#int(T/DT)
 
 femSpaces = {0:C0_AffineLinearOnSimplexWithNodalBasis}
+#femSpaces = {0:C0_AffineQuadraticOnSimplexWithNodalBasis}
 
-#elementQuadrature = SimplexGaussQuadrature(nd,3)
+elementQuadrature = SimplexGaussQuadrature(nd,3)
 
-#elementBoundaryQuadrature = SimplexGaussQuadrature(nd-1,3)
+elementBoundaryQuadrature = SimplexGaussQuadrature(nd-1,3)
 
-elementQuadrature = SimplexLobattoQuadrature(nd,1)
+#elementQuadrature = SimplexLobattoQuadrature(nd,1)
 #pwl-bdm requires more accurate quadrature on element boundaries
-elementBoundaryQuadrature = SimplexLobattoQuadrature(nd-1,1)
+#elementBoundaryQuadrature = SimplexLobattoQuadrature(nd-1,1)
 #pwl-bdm requires more accurate quadrature on element boundaries
 #nnx=61
 #nny=4
@@ -50,7 +61,7 @@ numericalFluxType = None
 numericalFluxType = Advection_DiagonalUpwind_Diffusion_IIPG_exterior #need weak for parallel and global conservation
 
 shockCapturing = None
-#shockCapturing = ResGradQuadDelayLag_SC(coefficients,nd,shockCapturingFactor=0.75,lag=True,nStepsToDelay=1)
+#shockCapturing = ResGradQuadDelayLag_SC(coefficients,nd,shockCapturingFactor=0.75,lag=True,nStepsToDelay=5)
 
 #multilevelNonlinearSolver  = NLStarILU
 #multilevelNonlinearSolver  = NLGaussSeidel
@@ -62,6 +73,9 @@ multilevelNonlinearSolver = Newton
 #levelNonlinearSolver = NLStarILU
 #levelNonlinearSolver = FAS
 levelNonlinearSolver = Newton
+#levelNonlinearSolver = ExplicitLumpedMassMatrixForRichards
+#levelNonlinearSolver = ExplicitConsistentMassMatrixForRichards
+
 #levelNonlinearSolver = NLGaussSeidel
 #levelNonlinearSolver = NLJacobi
 
@@ -73,10 +87,11 @@ fullNewtonFlag = True
 
 tolFac = 0.0
 
-nl_atol_res = 1.0e-6
+nl_atol_res = 1.0e-8
 
-maxNonlinearIts = 10
-maxLineSearches = 25
+maxNonlinearIts = 20#1001
+maxLineSearches =0
+
 matrix = SparseMatrix
 
 multilevelLinearSolver = LU
