1b0a9a04561839b31a78829a9fcbf2b4708ed2cb
cekees <613654+cekees@users.noreply.github.com>
2024-04-24 19:38:10 +0000

    working build on koehr

--- proteus/partitioning.cpp
+++ proteus/partitioning.cpp
@@ -3178,7 +3178,7 @@ int partitionNodesFromTriangleFiles(const MPI_Comm& PROTEUS_COMM_WORLD, const ch
                          weights_subdomain,
                          &petscAdjacency);CHKERRABORT(PROTEUS_COMM_WORLD, ierr);
   //const double max_rss_gb(0.75*3.25);//half max mem per  core  on topaz
-  const double max_rss_gb(3.75);
+  const double max_rss_gb(5.0);
   ierr = enforceMemoryLimit(PROTEUS_COMM_WORLD, rank, max_rss_gb,"Done allocating MPIAdj");CHKERRABORT(PROTEUS_COMM_WORLD, ierr);
   MatPartitioning petscPartition;
   ierr = MatPartitioningCreate(PROTEUS_COMM_WORLD,&petscPartition);CHKERRABORT(PROTEUS_COMM_WORLD, ierr);
